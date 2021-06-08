4d10919627158952bca241c19ec96e7a0d01f363
florian.enoh <florian.enoh@octo.com>
2021-06-08 15:34:26 +0200

    Ajout du design system

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+declare module '@gouvfr/dsfr/dist/js/dsfr.module.min.js';
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
+import '@gouvfr/dsfr/dist/css/dsfr.min.css';
 import type { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
+import React from 'react';
+
+const DynamicComponentWithNoSSR = dynamic(
+  () =>
+    import('@gouvfr/dsfr/dist/js/dsfr.module.min.js').then((m) => m.toString()),
+  { ssr: false }
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DynamicComponentWithNoSSR />
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
