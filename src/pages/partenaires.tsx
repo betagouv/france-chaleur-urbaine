76f1df725a347aac6252154154cb26f3dfeffc15
damienschallergouv <damien.schaller@beta.gouv.fr>
2021-07-06 10:00:21 +0200

    correction pages partner and resources

--- src/pages/partenaires.tsx
+++ src/pages/partenaires.tsx
@@ -1,116 +1,33 @@
-import ResourceSection from '@components/ResourceSection';
-import PartnerCard from '@pages/partnerCard';
+import PartnerCard from '@components/partenaires/partnerCard';
+import Data from '@components/partenaires/partnerData.json';
+import PartnerSection from '@components/partenaires/partnerSection';
 import React from 'react';
 
 function Partner() {
   return (
     <>
-      <div className="fr-container fr-container--fluid fr-mt-2w">
+      <div className="fr-container fr-mt-2w">
         <div className="fr-grid-row">
-          <div className="fr-col-4">
-            <h2>Partenaires</h2>
-            <p className="fr-m-0">
-              Vous trouverez ci-dessous un ensemble ressoures concernant le
-              chauffage urbain : raccordement, aide public, relation avec les
-              exploitants des réseaux, réglementation.
-            </p>
+          <div className="fr-col-lg-4 fr-col-sm-12">
+            <h2>{Data.title}</h2>
+            <p className="fr-m-0">{Data.description}</p>
           </div>
         </div>
       </div>
-      <ResourceSection
-        title="Partenaires principaux"
-        columnNumberDesktop="3"
-        columnNumberResponsive="6"
-      >
-        <PartnerCard
-          imageUrl="https://resize2.prod.docfr.doc-media.fr/rcrop/650,380,center-middle/img/var/doctissimo/storage/images/fr/www/animaux/chat/races-chat/sphynx/790409-2-fre-FR/sphynx.jpg"
-          imageTitle="image"
-          sourceTitle="Titre"
-          sourceDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, amet, animi atque consequuntur cum dolorum
-  eveniet itaque nisi odio omnis quasi, quidem rerum sequi tenetur ut vel veniam vitae voluptatem?"
-          link="#"
-        />
-        <PartnerCard
-          imageUrl="https://resize2.prod.docfr.doc-media.fr/rcrop/650,380,center-middle/img/var/doctissimo/storage/images/fr/www/animaux/chat/races-chat/sphynx/790409-2-fre-FR/sphynx.jpg"
-          imageTitle="image"
-          sourceTitle="Titre"
-          sourceDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, amet, animi atque consequuntur cum dolorum
-  eveniet itaque nisi odio omnis quasi, quidem rerum sequi tenetur ut vel veniam vitae voluptatem?"
-          link="#"
-        />
-        <PartnerCard
-          imageUrl="https://resize2.prod.docfr.doc-media.fr/rcrop/650,380,center-middle/img/var/doctissimo/storage/images/fr/www/animaux/chat/races-chat/sphynx/790409-2-fre-FR/sphynx.jpg"
-          imageTitle="image"
-          sourceTitle="Titre"
-          sourceDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, amet, animi atque consequuntur cum dolorum
-  eveniet itaque nisi odio omnis quasi, quidem rerum sequi tenetur ut vel veniam vitae voluptatem?"
-          link="#"
-        />
-        <PartnerCard
-          imageUrl="https://resize2.prod.docfr.doc-media.fr/rcrop/650,380,center-middle/img/var/doctissimo/storage/images/fr/www/animaux/chat/races-chat/sphynx/790409-2-fre-FR/sphynx.jpg"
-          imageTitle="image"
-          sourceTitle="Titre"
-          sourceDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, amet, animi atque consequuntur cum dolorum
-  eveniet itaque nisi odio omnis quasi, quidem rerum sequi tenetur ut vel veniam vitae voluptatem?"
-          link="#"
-        />
-        <PartnerCard
-          imageUrl="https://resize2.prod.docfr.doc-media.fr/rcrop/650,380,center-middle/img/var/doctissimo/storage/images/fr/www/animaux/chat/races-chat/sphynx/790409-2-fre-FR/sphynx.jpg"
-          imageTitle="image"
-          sourceTitle="Titre"
-          sourceDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, amet, animi atque consequuntur cum dolorum
-  eveniet itaque nisi odio omnis quasi, quidem rerum sequi tenetur ut vel veniam vitae voluptatem?"
-          link="#"
-        />
-        <PartnerCard
-          imageUrl="https://resize2.prod.docfr.doc-media.fr/rcrop/650,380,center-middle/img/var/doctissimo/storage/images/fr/www/animaux/chat/races-chat/sphynx/790409-2-fre-FR/sphynx.jpg"
-          imageTitle="image"
-          sourceTitle="Titre"
-          sourceDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, amet, animi atque consequuntur cum dolorum
-  eveniet itaque nisi odio omnis quasi, quidem rerum sequi tenetur ut vel veniam vitae voluptatem?"
-          link="#"
-        />
-      </ResourceSection>
-      <ResourceSection
-        title="Partenaires principaux"
-        columnNumberDesktop="3"
-        columnNumberResponsive="6"
-      >
-        <PartnerCard
-          imageUrl="https://resize2.prod.docfr.doc-media.fr/rcrop/650,380,center-middle/img/var/doctissimo/storage/images/fr/www/animaux/chat/races-chat/sphynx/790409-2-fre-FR/sphynx.jpg"
-          imageTitle="image"
-          sourceTitle="Titre"
-          sourceDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, amet, animi atque consequuntur cum dolorum
-  eveniet itaque nisi odio omnis quasi, quidem rerum sequi tenetur ut vel veniam vitae voluptatem?"
-          link="#"
-        />
-        <PartnerCard
-          imageUrl="https://resize2.prod.docfr.doc-media.fr/rcrop/650,380,center-middle/img/var/doctissimo/storage/images/fr/www/animaux/chat/races-chat/sphynx/790409-2-fre-FR/sphynx.jpg"
-          imageTitle="image"
-          sourceTitle="Titre"
-          sourceDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, amet, animi atque consequuntur cum dolorum
-  eveniet itaque nisi odio omnis quasi, quidem rerum sequi tenetur ut vel veniam vitae voluptatem?"
-          link="#"
-        />
-        <PartnerCard
-          imageUrl="https://resize2.prod.docfr.doc-media.fr/rcrop/650,380,center-middle/img/var/doctissimo/storage/images/fr/www/animaux/chat/races-chat/sphynx/790409-2-fre-FR/sphynx.jpg"
-          imageTitle="image"
-          sourceTitle="Titre"
-          sourceDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, amet, animi atque consequuntur cum dolorum
-  eveniet itaque nisi odio omnis quasi, quidem rerum sequi tenetur ut vel veniam vitae voluptatem?"
-          link="#"
-        />
-        <PartnerCard
-          imageUrl="https://resize2.prod.docfr.doc-media.fr/rcrop/650,380,center-middle/img/var/doctissimo/storage/images/fr/www/animaux/chat/races-chat/sphynx/790409-2-fre-FR/sphynx.jpg"
-          imageTitle="image"
-          sourceTitle="Titre"
-          sourceDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, amet, animi atque consequuntur cum dolorum
-  eveniet itaque nisi odio omnis quasi, quidem rerum sequi tenetur ut vel veniam vitae voluptatem?"
-          link="#"
-        />
-      </ResourceSection>
+      {Data.partners.map((partner, index) => (
+        <PartnerSection key={index} title={partner.title}>
+          {partner.items.map((item, index) => (
+            <PartnerCard
+              key={index}
+              image={item.image}
+              title={item.title}
+              description={item.description}
+              link={item.link}
+            />
+          ))}
+        </PartnerSection>
+      ))}
     </>
   );
 }
-
 export default Partner;
