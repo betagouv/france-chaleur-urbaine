707615cdf6e0c444f7351e3f5b06e9202e7d19a1
florian.enoh <florian.enoh@octo.com>
2021-07-19 11:12:04 +0200

    Add matomo tags

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -10,13 +10,13 @@ function Banner() {
   return (
     <BannerContainer className="fr-container--fluid fr-py-6w">
       <div className="fr-grid-row fr-grid-row--center">
-        <div className="fr-col-lg-8">
+        <div className="fr-col-lg-12">
           <div className="fr-container">
             <div className="fr-grid-row fr-grid-row--center">
               <ImageContainer className="fr-col-lg-4">
                 <img src="./pictoBuilding.png" alt="" />
               </ImageContainer>
-              <Container className="fr-col-lg-7 fr-col-md-12 fr-ml-6w fr-mt-2w">
+              <Container className="fr-col-lg-7 fr-col-md-12 fr-ml-1w fr-mt-2w">
                 <CheckEligibilityForm />
               </Container>
             </div>
