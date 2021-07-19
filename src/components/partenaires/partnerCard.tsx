0b6fd6016d406a76b4a4b390811d2b172fce4108
damienschallergouv <damien.schaller@beta.gouv.fr>
2021-07-19 18:17:33 +0200

    wip show text

--- src/components/partenaires/partnerCard.tsx
+++ src/components/partenaires/partnerCard.tsx
@@ -1,4 +1,7 @@
-import { TextCard } from '@components/partenaires/partnerCard.style';
+import {
+  FullTextCard,
+  TextCard,
+} from '@components/partenaires/partnerCard.style';
 import React from 'react';
 
 type PartnerCard = {
@@ -9,21 +12,45 @@ type PartnerCard = {
 };
 
 function PartnerCard({ image, title, description, link }: PartnerCard) {
+  let displayFullCard = false;
+  const setDisplayFullCard = () => {
+    displayFullCard = !displayFullCard;
+  };
   return (
-    <div className="fr-card fr-enlarge-link">
-      <TextCard className="fr-card__body">
-        <h4 className="fr-card__title">
-          <a
-            href={link}
-            target="_blank"
-            className="fr-card__link"
-            rel="noreferrer"
-          >
-            {title}
-          </a>
-        </h4>
-        <p className="fr-card__desc">{description}</p>
-      </TextCard>
+    <div className="fr-card">
+      {displayFullCard ? (
+        <FullTextCard className="fr-card__body">
+          <h4 className="fr-card__title">
+            <a
+              href={link}
+              target="_blank"
+              className="fr-card__link"
+              rel="noreferrer"
+            >
+              {title}
+            </a>
+          </h4>
+          <p className="fr-card__desc" onClick={setDisplayFullCard}>
+            {description}
+          </p>
+        </FullTextCard>
+      ) : (
+        <TextCard className="fr-card__body">
+          <h4 className="fr-card__title">
+            <a
+              href={link}
+              target="_blank"
+              className="fr-card__link"
+              rel="noreferrer"
+            >
+              {title}
+            </a>
+          </h4>
+          <p className="fr-card__desc" onClick={setDisplayFullCard}>
+            {description}
+          </p>
+        </TextCard>
+      )}
       <div className="fr-card__img">
         <img src={image.url} className="fr-responsive-img" alt={image.title} />
       </div>
