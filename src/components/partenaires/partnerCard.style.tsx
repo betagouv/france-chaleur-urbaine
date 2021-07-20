a3c969c1cfefd827e1b6cd8063209c3484937768
damienschallergouv <damien.schaller@beta.gouv.fr>
2021-07-20 11:41:34 +0200

    change text partners

--- src/components/partenaires/partnerCard.style.tsx
+++ src/components/partenaires/partnerCard.style.tsx
@@ -1,11 +1,5 @@
 import styled from 'styled-components';
 
 export const TextCard = styled.div`
-  height: 280px;
-  > p {
-    display: -webkit-box;
-    -webkit-line-clamp: 6;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-  }
+  height: max-content;
 `;
