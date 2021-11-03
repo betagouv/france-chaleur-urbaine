b22ae563d21349d39a74990fc100f4c965046f29
Nicolas KOKLA <n.kokla@koaji.fr>
2021-11-03 13:45:08 +0100

    ✨ New component: HighlightList

---
+++ src/components/HighlightList/HighlightList.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Highlight, { HighlightType } from './Highlight';
+import { PageTitle } from './HighlightList.style';
+
+type HighlightListProps = {
+  title: string;
+  data: HighlightType[];
+};
+
+function HighlightList({ title, data = [] }: HighlightListProps) {
+  return (
+    <div className="fr-my-10w">
+      <PageTitle>{title}</PageTitle>
+      <div className="fr-container--fluid fr-my-6w">
+        <div className="fr-grid-row fr-grid-row--gutters">
+          {data?.map(
+            ({
+              title,
+              subTitle,
+              description,
+              icon,
+              altIcon,
+            }: HighlightType) => (
+              <div key={icon} className="XXX--fr-col-lg-4 fr-col-sm">
+                <Highlight
+                  title={title}
+                  subTitle={subTitle}
+                  description={description}
+                  icon={icon}
+                  altIcon={altIcon}
+                />
+              </div>
+            )
+          ) || null}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HighlightList;
