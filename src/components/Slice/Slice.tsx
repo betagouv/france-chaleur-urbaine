e0ac3760c228152ab1ba356db952288ed186fcef
Nicolas KOKLA <n.kokla@koaji.fr>
2021-11-05 15:16:02 +0100

    🩹 Fix: Sometimes, Slice for mobile view is broken

--- src/components/Slice/Slice.tsx
+++ src/components/Slice/Slice.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BannerContainer } from './Slice.style';
+import { SliceContainer, SliceSection } from './Slice.style';
 
 const Slice: React.FC<{ theme?: string }> = ({ children, theme }) => {
   return (
-    <BannerContainer className="fr-container--fluid fr-py-2w" theme={theme}>
+    <SliceSection className="fr-container--fluid fr-py-2w" theme={theme}>
       <div className="fr-grid-row fr-grid-row--center">
-        <div className="fr-col-lg-12">
+        <SliceContainer className="fr-col-lg-12">
           <div className="fr-container">{children}</div>
-        </div>
+        </SliceContainer>
       </div>
-    </BannerContainer>
+    </SliceSection>
   );
 };
 
