7f8e30044a1afc17b333ad2aea35348e322303dd
Nicolas KOKLA <n.kokla@koaji.fr>
2021-11-12 16:52:05 +0100

    💄  Update Accordions to support Markdown

--- src/components/accordions/faq-rcu.tsx
+++ src/components/accordions/faq-rcu.tsx
@@ -2,12 +2,14 @@ const faqRcu = [
   {
     label: 'Quelles énergies alimentent les réseaux de chaleur ?',
     body: `Les réseaux de chaleur sont alimentés en moyenne à 60 % par des énergies renouvelables et de récupération disponibles localement (ENR&R). Cette chaleur "verte" est principalement issue :
-    - de la récupération de la chaleur issue de l’incinération de nos déchets, première source d'ENR&R dans les réseaux de chaleur ;
-    - de la combustion de la biomasse (bois déchet, bois issu de l’élagage urbain d’hiver, produits forestiers issus de l’exploitation ou de l’entretien des forêts, résidus agricoles…) ;
-    - de la géothermie (chaleur extraite du sous-sol).
-    Les 40 % restants correspondent majoritairement à du gaz naturel.
-    Il est à noter que le mix énergétique est variable d’un réseau à l’autre et que le gaz est généralement utilisé en appoint/secours pour assurer la continuité du service.
-    `,
+
+- de la récupération de la chaleur issue de l’incinération de nos déchets, première source d'ENR&R dans les réseaux de chaleur ;
+- de la combustion de la biomasse (bois déchet, bois issu de l’élagage urbain d’hiver, produits forestiers issus de l’exploitation ou de l’entretien des forêts, résidus agricoles…) ;
+- de la géothermie (chaleur extraite du sous-sol).
+
+Les 40 % restants correspondent majoritairement à du gaz naturel.  
+Il est à noter que le mix énergétique est variable d’un réseau à l’autre et que le gaz est généralement utilisé en appoint/secours pour assurer la continuité du service.
+`,
   },
   {
     label:
@@ -17,10 +19,12 @@ const faqRcu = [
   {
     label: 'Quelles sont les aides financières disponibles ?',
     body: `Les aides financières suivantes sont disponibles pour le raccordement à un réseau de chaleur :
-    MaPrimeRenov' Copropriétés, aide versée aux syndicats de copropriétés pour des travaux effectués sur les parties communes. Elle concerne notamment les équipements de raccordement à un réseau de chaleur lorsque celui-ci est majoritairement alimenté par des énergies renouvelables ou de récupération, ou encore la dépose des cuves à fioul. Des aides individuelles sont cumulables avec MaPrimeRenov' Copropriétés, au titre du dispositif MaPrimeRenov'.
-    Primes "Coups de pouce chauffage", dont peuvent bénéficier l’ensemble des ménages (avec des montants fonction de leurs revenus) pour des travaux visant à substituer un chauffage fossile par un raccordement à un réseau de chaleur alimenté majoritairement par des énergies renouvelables et de récupération (voir fiche CEE BAR-TH-137). Les travaux doivent être réalisés par une entreprise qualifiée RGE.
-    Par ailleurs, pour les réseaux de chaleur alimentés à plus de 50 % par des énergies renouvelables et de récupération, un taux de TVA réduit à 5.5 % s’applique sur l’ensemble de la facture de chauffage.
-    `,
+
+- [MaPrimeRenov' Copropriétés](https://www.maprimerenov.gouv.fr/prweb/PRAuth/app/AIDES_/BPNVwCpLW8TKW49zoQZpAw*/!STANDARD), aide versée aux syndicats de copropriétés pour des travaux effectués sur les parties communes. Elle concerne notamment les équipements de raccordement à un réseau de chaleur lorsque celui-ci est majoritairement alimenté par des énergies renouvelables ou de récupération, ou encore la dépose des cuves à fioul. Des aides individuelles sont cumulables avec MaPrimeRenov' Copropriétés, au titre du dispositif MaPrimeRenov'.
+- [Primes "Coups de pouce chauffage"](https://www.ecologie.gouv.fr/coup-pouce-chauffage-et-isolation), dont peuvent bénéficier l’ensemble des ménages (avec des montants fonction de leurs revenus) pour des travaux visant à substituer un chauffage fossile par un raccordement à un réseau de chaleur alimenté majoritairement par des énergies renouvelables et de récupération (voir [fiche CEE BAR-TH-137](https://www.ecologie.gouv.fr/sites/default/files/Fiche%20BAR-TH-137.pdf)). Les travaux doivent être réalisés par une entreprise qualifiée RGE.
+
+Par ailleurs, pour les réseaux de chaleur alimentés à plus de 50 % par des énergies renouvelables et de récupération, un taux de TVA réduit à 5.5 % s’applique sur l’ensemble de la facture de chauffage.
+`,
   },
   {
     label:
@@ -30,41 +34,42 @@ const faqRcu = [
   {
     label: 'Quels sont les acteurs d’un réseau de chaleur ?',
     body: `Dans le secteur des réseaux de chaleur, les notions d’abonnés et d’usagers ne recouvrent pas tout à fait la même chose que dans les domaines de distribution et fourniture d’électricité ou de gaz, en alimentation individuelle. Ces deux entités sont en effet bien distinctes : on retrouve d’un côté les abonnés (syndic de copropriété,…) et de l’autre les usagers (propriétaires, locataires…).
-    Au niveau contractuel, les relations entre les différents acteurs sont régies par un certain nombre de contrats :
-    - la collectivité travaille en régie ou délègue et contrôle la production et la distribution de la chaleur sur le réseau primaire à un exploitant, via un contrat de concession de service public ;
-    - l’exploitant conclut un contrat d’abonnement (règlement de service + police d’abonnement), avec les abonnés (clients) auxquels il fournit de la chaleur ;
-    - les abonnés refacturent par la suite le service aux usagers, conformément au règlement de copropriété.
-    `,
+Au niveau contractuel, les relations entre les différents acteurs sont régies par un certain nombre de contrats :
+- la collectivité travaille en régie ou délègue et contrôle la production et la distribution de la chaleur sur le réseau primaire à un exploitant, via un contrat de concession de service public ;
+- l’exploitant conclut un contrat d’abonnement (règlement de service + police d’abonnement), avec les abonnés (clients) auxquels il fournit de la chaleur ;
+- les abonnés re-facturent par la suite le service aux usagers, conformément au règlement de copropriété.
+`,
   },
   {
     label:
       'Quelles sont les étapes à suivre pour raccorder ma copropriété à un réseau de chaleur ?',
     body: `France Chaleur Urbaine, tiers neutre et indépendant, vous accompagne dans la mise en place de votre projet, dont les grandes étapes seront les suivantes :
-    - déterminer la compatibilité du raccordement :
-    -- savoir si un réseau de chaleur passe à proximité de ma copropriété ou dans le cas contraire si des travaux d’extension/création sont prévus prochainement ;
-    -- connaître les travaux à envisager selon le mode de chauffage initial du bâtiment ;
-    - vérifier la faisabilité technico-économique du projet auprès du gestionnaire du réseau de chaleur et obtenir une estimation du coût de raccordement ainsi que du tarif de la chaleur ;
-    - préciser les contours du projet (mode de gestion, plan de financement,…) ;
-    
-    Le projet peut alors être soumis au vote de l’assemblée générale de la copropriété. S’ensuivent la contractualisation et la réalisation des travaux de raccordement par le gestionnaire du réseau.
-    `,
+- déterminer la compatibilité du raccordement :
+  - savoir si un réseau de chaleur passe à proximité de ma copropriété ou dans le cas contraire si des travaux d’extension/création sont prévus prochainement ;
+  - connaître les travaux à envisager selon le mode de chauffage initial du bâtiment ;
+- vérifier la faisabilité technico-économique du projet auprès du gestionnaire du réseau de chaleur et obtenir une estimation du coût de raccordement ainsi que du tarif de la chaleur ;
+- préciser les contours du projet (mode de gestion, plan de financement,…) ;
+
+Le projet peut alors être soumis au vote de l’assemblée générale de la copropriété. S’ensuivent la contractualisation et la réalisation des travaux de raccordement par le gestionnaire du réseau.
+`,
   },
   {
     label: 'Quels travaux impliquent un raccordement ?',
     body: `Trois types de cas peuvent se présenter :
 
-    - le bâtiment est équipé d’un système de chauffage collectif → il possède un réseau de distribution interne et des équipements adaptés au sein des logements ;
-    - le bâtiment est équipé d’un système de chauffage individuel avec un système de distribution de la chaleur à eau → il possède les équipements adaptés au sein des logements ;
-    - le bâtiment est équipé d’un système de chauffage individuel électrique → les équipements nécessaires à la desserte de la chaleur au sein de la copropriété devront être mis en place, ce qui peut s’avérer coûteux et techniquement compliqué. 
-    C’est le gestionnaire du réseau de chaleur auquel la copropriété se raccorde qui est en charge de la réalisation des travaux sur le réseau dit “primaire”, qui s’arrête au pied de l’immeuble. Ces travaux sont suivis par la copropriété, qui est associée aux réunions de chantier. Sur le réseau “secondaire”, interne à l’immeuble, les travaux sont gérés par le syndic de copropriété.
-    `,
+- le bâtiment est équipé d’un système de chauffage collectif → il possède un réseau de distribution interne et des équipements adaptés au sein des logements ;
+- le bâtiment est équipé d’un système de chauffage individuel avec un système de distribution de la chaleur à eau → il possède les équipements adaptés au sein des logements ;
+- le bâtiment est équipé d’un système de chauffage individuel électrique → les équipements nécessaires à la desserte de la chaleur au sein de la copropriété devront être mis en place, ce qui peut s’avérer coûteux et techniquement compliqué. 
+
+C’est le gestionnaire du réseau de chaleur auquel la copropriété se raccorde qui est en charge de la réalisation des travaux sur le réseau dit “primaire”, qui s’arrête au pied de l’immeuble. Ces travaux sont suivis par la copropriété, qui est associée aux réunions de chantier. Sur le réseau “secondaire”, interne à l’immeuble, les travaux sont gérés par le syndic de copropriété.
+`,
   },
   {
     label: 'Comment sont assurés l’entretien et la maintenance du réseau ?',
     body: `On distingue :
-    - le réseau primaire, qui achemine la chaleur depuis les centrales de production de chaleur jusqu’aux sous-stations situées au pied des bâtiments. L’entretien, la maintenance, la continuité et la garantie de performance du réseau primaire est satisfaite par le gestionnaire du réseau de chaleur ;
-    - le réseau secondaire, qui prend le relais au niveau de la sous-station et alimente les différents logements. Pour ce réseau, il appartient à la copropriété de conclure un contrat d’entretien et de maintenance avec un chauffagiste afin de s’assurer du bon fonctionnement des installations.
-    `,
+- le réseau primaire, qui achemine la chaleur depuis les centrales de production de chaleur jusqu’aux sous-stations situées au pied des bâtiments. L’entretien, la maintenance, la continuité et la garantie de performance du réseau primaire est satisfaite par le gestionnaire du réseau de chaleur ;
+- le réseau secondaire, qui prend le relais au niveau de la sous-station et alimente les différents logements. Pour ce réseau, il appartient à la copropriété de conclure un contrat d’entretien et de maintenance avec un chauffagiste afin de s’assurer du bon fonctionnement des installations.
+`,
   },
 ];
 
