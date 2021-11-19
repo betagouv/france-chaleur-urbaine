6d96e16ad2a4ac3e5539a864067205de1bb570b0
Nicolas KOKLA <n.kokla@koaji.fr>
2021-11-19 17:35:05 +0100

    💬 Update 'Accessibilite' text page

--- src/pages/accessibilite.tsx
+++ src/pages/accessibilite.tsx
@@ -14,20 +14,28 @@ function Accessibilite() {
             <div className="fr-col-12">
               <h2>Déclaration d’accessibilité France Chaleur Urbaine</h2>
               <p>
-                Nom de l’entité s’engage à rendre ses sites internet, intranet,
-                extranet et ses progiciels accessibles (et ses applications
-                mobiles et mobilier urbain numérique) conformément à l’article
-                47 de la loi n°2005-102 du 11 février 2005.
-                <br />
-                Cette déclaration d’accessibilité s’applique à
-                france-chaleur-urbaine.beta.gouv.fr.
+                France Chaleur Urbaine s’engage à rendre ses sites internet,
+                intranet, extranet et ses progiciels accessibles (et ses
+                applications mobiles et mobilier urbain numérique) conformément
+                à l’article 47 de la loi n°2005-102 du 11 février 2005.
+              </p>
+              <p>
+                Cette déclaration d’accessibilité s’applique à{' '}
+                <a href="https://france-chaleur-urbaine.beta.gouv.fr/">
+                  france-chaleur-urbaine.beta.gouv.fr
+                </a>
+                .
               </p>
               <h3>État de conformité</h3>
               <p>
-                Nom du site, url est non conforme avec le référentiel général
-                d’amélioration de l’accessibilité (RGAA), version 4 en raison
-                d’absence d’audit achevé, ce qui ne permet pas aujourd’hui de
-                lister les non-conformités du site.
+                France Chaleur Urbaine,{' '}
+                <a href="https://france-chaleur-urbaine.beta.gouv.fr/">
+                  france-chaleur-urbaine.beta.gouv.fr
+                </a>{' '}
+                est non conforme avec le référentiel général d’amélioration de
+                l’accessibilité (RGAA), version 4 en raison d’absence d’audit
+                achevé, ce qui ne permet pas aujourd’hui de lister les
+                non-conformités du site.
               </p>
               <p>
                 Cependant, France Chaleur Urbaine est conçu en utilisant le
@@ -36,27 +44,41 @@ function Accessibilite() {
                 l’audit RGAA.
               </p>
               <h3>Établissement de cette déclaration d’accessibilité</h3>
-              <p>Cette déclaration a été établie le 12/07/2021.</p>
+              <p>Cette déclaration a été établie le 19/11/2021.</p>
               <strong>
-                Technologies utilisées pour la réalisation de nom, url du site
+                Technologies utilisées pour la réalisation de France Chaleur
+                Urbaine,{' '}
+                <a href="https://france-chaleur-urbaine.beta.gouv.fr/">
+                  france-chaleur-urbaine.beta.gouv.fr
+                </a>
               </strong>
               <ul className="fr-mb-4w">
                 <li>HTML5</li>
                 <li>CSS</li>
-                <li>NEXT</li>
+                <li>JavaScript</li>
+                <li>Node.js</li>
+                <li>NEXT.js</li>
                 <li>REACT</li>
-                <li>Styled component</li>
+                <li>Styled Component</li>
+                <li>Markdown</li>
               </ul>
               <p>
                 Si vous n’arrivez pas à accéder à un contenu ou à un service,
-                vous pouvez contacter le responsable de nom, url du site pour
-                être orienté vers une alternative accessible ou obtenir le
+                vous pouvez contacter le responsable de France Chaleur Urbaine,{' '}
+                <a href="https://france-chaleur-urbaine.beta.gouv.fr/">
+                  france-chaleur-urbaine.beta.gouv.fr
+                </a>{' '}
+                pour être orienté vers une alternative accessible ou obtenir le
                 contenu sous une autre forme.
               </p>
               <ul className="fr-mb-4w">
                 <li>
                   Envoyer un message à{' '}
-                  <a href="mailto:france-chaleur-urbaine@beta.gouv.fr">
+                  <a
+                    href="mailto:france-chaleur-urbaine@beta.gouv.fr"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     france-chaleur-urbaine@beta.gouv.fr
                   </a>
                 </li>
