2792bc95dba13025d66730c815a42fac2b12eb3b
Nicolas KOKLA <n.kokla@koaji.fr>
2021-12-07 16:30:48 +0100

    ✨ Add Facebook Markup feature

--- src/components/Markup/index.ts
+++ src/components/Markup/index.ts
@@ -1,3 +1,4 @@
+export { default as FacebookMarkup, facebookEvent } from './FacebookMarkup';
 export { default as GoogleAdsMarkup, googleAdsEvent } from './GoogleAdsMarkup';
 export { default as LinkedInMarkup, linkedInEvent } from './LinkedInMarkup';
 export { default } from './markup-data.json';
