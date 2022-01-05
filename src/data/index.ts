9c468c8e331d774055361bb8b441bcc3430991e9
Nicolas KOKLA <n.kokla@koaji.fr>
2022-01-05 23:17:45 +0100

    🗃️ Add new datas

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,5 @@
 export { default as accompagnementRcu } from './home/accompagnement-rcu.json';
+export { default as dataNumberFcu } from './home/data-number-fcu.json';
 export { default as dataNumberRcu } from './home/data-number-rcu.json';
 export { default as fcuSolutionForFutur } from './home/fcu-solution-for-futur.json';
 export { default as testimonies } from './home/testimonies.json';
