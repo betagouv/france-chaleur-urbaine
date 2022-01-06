96707614e70fe1224a285fe37abb0d2f7ba85a39
Nicolas KOKLA <n.kokla@koaji.fr>
2022-01-06 22:41:17 +0100

    ✏️Update Privacy Policy page

---
+++ src/data/home/fcu-privacy-policy.js
@@ -0,0 +1,98 @@
+const privacyPolicy = `
+## Politique de protection des données personnelles
+
+### Le traitement de données
+
+La nature des opérations réalisées sur les données est :
+- Lors de l’utilisation du service, les informations personnelles que vous acceptez de communiquer sont enregistrées dans France Chaleur Urbaine.
+- L’affectation d’un utilisateur à une entité privée de type entreprise est supervisée par un administrateur de la plateforme France Chaleur Urbaine.
+- La mise à jour et l’effacement des données personnelles liées au compte utilisateur France Chaleur Urbaine et possible par mail à l’adresse [france-chaleur-urbaine@beta.gouv.fr](mailto:france-chaleur-urbaine@beta.gouv.fr).
+- Les données de connexion sont utilisées pour établir un journal d’ajout et modification des données de France Chaleur Urbaine et de consultation des données non publiques ou à caractère confidentiel.
+- Les données personnelles contenues dans les dossiers de demandes suivent un cycle de vie contraint par les procédures d’instruction et d’archivage légaux.
+
+### Responsable du traitement
+
+Le responsable du traitement de vos données à caractère personnel est la Direction des Affaires Juridiques du Ministère de la Transition Ecologique 1 place Carpeaux 92800 Puteaux.
+
+### Données à caractère personnel traitées
+
+Les données à caractère personnel traitées sont relatives à :
+- Nom, Prénom
+- Adresse géographie
+- Adresse mail, téléphone
+- Statut dans la copropriété
+
+### Finalités
+- Transfert de vos informations à l’opérateur de réseau de chaleur de votre quartier pour prise de contact commercial ;
+- communication par mail d’informations ponctuelles sur l’évolution du service France Chaleur Urbaine ;
+- demande ponctuelle par mail d’entretien téléphonique par l’équipe France Chaleur Urbaine en vue de connaître votre retour d’expérience et suggestions pour améliorer le service France Chaleur Urbaine ;
+- agrégation anonymisées des données en vue d’analyses statistiques ;
+- d’enrichissement des bases de données de l’administration publique.
+
+### Base légale du traitement de données
+
+Le traitement de données est rendu licite parce qu’il est nécessaire à l’exécution d’une mission d’intérêt public ou relevant de l’exercice de l’autorité publique dont est investi le responsable de traitement, telle qu’entendue par l’article 6-e du règlement (UE) n°2016/679 du Parlement européen et du Conseil relatif à la protection des personnes physiques à l’égard du traitement des données à caractère personnel et à la libre circulation de ces données.
+
+La base légale du traitement pour certains cookies est le consentement des personnes.
+
+La base légale du traitement des données d’hébergeur est une obligation légale.
+
+### Conservation
+
+Les données sont conservées pour une durée de :
+- 12 mois pour les données de connexion et les cookies. 
+- 24 mois pour toutes les autres données.
+
+### Sécurité et confidentialité
+
+Le responsable de traitement s’engage à prendre toutes les mesures techniques et organisationnelles de sécurité nécessaires pour assurer la confidentialité, l’intégrité et pour protéger les données.
+
+En cas de violation de données, seront informés les référents métier, la CNIL et les personnes concernées par la violation, dans le respect de notre procédure interne de violation de données. Seront également mises en place le plus rapidement possible toutes les mesures de sécurité nécessaires pour identifier et réparer la faille.
+
+### Droits des personnes concernées
+
+En vertu de l’article 13 du règlement (UE) n°2016/679 du Parlement européen et du Conseil du 27 avril 2016 relatif à la protection des personnes physiques à l’égard du traitement des données à caractère personnel et à la libre circulation de ces données, il est rappelé à chaque utilisateur dont les données sont collectées dans le cadre de l’utilisation ou de la connexion à la plateforme France Chaleur Urbaine qu’il dispose des droits suivants concernant ses données à caractère personnel :
+- Droit d’information ;
+- Droit d’accès aux données ;
+- Droit de rectification et droit d’effacement des données ;
+- Droit de limitation du traitement relatif à la personne concernée ;
+- Droit au retrait du consentement en matière de cookies.
+
+Vous pouvez exercer ces droits en contactant par :
+- Par courriel : [france-chaleur-urbaine@beta.gouv.fr](mailto:france-chaleur-urbaine@beta.gouv.fr)
+- Par voie postale :  
+_France Chaleur Urbaine - Fabrique Numérique  
+Ministère de la Transition écologique et solidaire Direction Générale de l'Aménagement, du Logement et de la Nature,  
+Bureau de la politique des ressources minérales non énergétiques,  
+1 place Carpeaux,  
+92800 Puteaux_
+
+En raison de l’obligation de sécurité et de confidentialité dans le traitement des données à caractère personnel qui incombe au responsable de traitement, votre demande ne sera traitée que si vous rapportez la preuve de votre identité. Délais de réponse : la responsable de traitement s’engage à répondre à votre demande d’accès, de rectification ou d’opposition ou toute autre demande complémentaire d’informations dans un délai raisonnable qui ne saurait dépasser 1 mois à compter de la réception de votre demande.
+
+### Sous-traitants
+
+Certaines des données sont envoyées à des sous-traitants pour réaliser certaines missions. Le responsable de traitement s’est assuré de la mise en œuvre par ses sous-traitants de garanties adéquates et du respect de conditions strictes de confidentialité, d’usage et de protection des données.
+- Hébergement du site : [scalingo.com](https://scalingo.com/)
+- Hébergement et traitement du formulaire : [formspark.io](https://formspark.io/)
+
+### Cookies
+
+Nous déposons des cookies de mesure d’audience (nombre de visites, pages consultées), respectant les conditions d’exemption du consentement de l’internaute définies par la recommandation « Cookies » de la Commission nationale informatique et libertés (CNIL). Cela signifie, notamment, que ces cookies ne servent qu’à la production de statistiques anonymes et ne permettent pas de suivre votre navigation sur d’autres sites. Nous déposons également des cookies de navigation, aux fins strictement techniques, qui ne sont pas conservés (mémorisation, pendant la durée de la session, des paramètres de filtrage et de préférence de fond cartographique). La consultation de la plateforme n’est pas affectée lorsque les utilisateurs utilisent des navigateurs désactivant les cookies. Les adresses IP sont collectées et conservées pour une durée de douze (12) mois.
+
+::consent-link[Configuration des cookies et consentements]{ForceBlock='p'}  \n
+
+### Évolution du service
+
+Nous pouvons faire évoluer France Chaleur Urbaine sans information préalable ou préavis. Nous ajoutons et corrigeons régulièrement des données, améliorons l’interface et modifions les formulations sur la base de vos retours et des évolutions réglementaires et législatives.
+
+### Disponibilité du service
+
+Nous pouvons suspendre l’accès à France Chaleur Urbaine sans information préalable ni préavis, notamment pour des raisons de maintenance. Nous mettons l’application à jour régulièrement. L’indisponibilité ne dépasse généralement pas une dizaine de secondes. Nous mettons France Chaleur Urbaine à disposition sans garantie sur sa disponibilité. Même si nous nous efforçons de maintenir le service toujours opérationnel, cela signifie que d’éventuelles indisponibilités n’ouvriront pas droit à compensation financière. Nous nous réservons également le droit de bloquer, sans information préalable ni compensation financière, les usages mettant en péril l’utilisation du logiciel par d’autres usagers. Cela nous permet d’anticiper d’éventuelles attaques par déni de service.
+
+### Modification de la politique de confidentialité
+
+Notre politique de confidentialité peut être mise à jour à tout moment. En cas de modification significative des principes de confidentialité appliqués, nous publierons la politique révisée sur cette page. Si vous continuez à utiliser le service après que ces modifications ont pris effet, vous acceptez la politique révisée.
+
+`;
+
+export default privacyPolicy;
