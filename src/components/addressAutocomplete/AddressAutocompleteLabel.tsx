19ffcdaed8a2426902f1f55cdb561167b9e71063
Nicolas KOKLA <n.kokla@koaji.fr>
2022-02-14 15:23:16 +0100

    💚 Fix: New Formating after prettier update

--- src/components/addressAutocomplete/AddressAutocompleteLabel.tsx
+++ src/components/addressAutocomplete/AddressAutocompleteLabel.tsx
@@ -4,14 +4,15 @@ type AddressAutocompleteLabelProps = {
   name?: string;
   centred?: boolean;
 };
-export const AddressAutocompleteLabel: React.FC<AddressAutocompleteLabelProps> =
-  ({ label, name = 'address', centred }) => {
-    const className = `fr-text--lg fr-my-2w fr-grid-row ${
-      centred ? 'fr-grid-row--center' : 'fr-grid-row--left'
-    }`;
-    return (
-      <label className={className} htmlFor={name}>
-        <span>{label}</span>
-      </label>
-    );
-  };
+export const AddressAutocompleteLabel: React.FC<
+  AddressAutocompleteLabelProps
+> = ({ label, name = 'address', centred }) => {
+  const className = `fr-text--lg fr-my-2w fr-grid-row ${
+    centred ? 'fr-grid-row--center' : 'fr-grid-row--left'
+  }`;
+  return (
+    <label className={className} htmlFor={name}>
+      <span>{label}</span>
+    </label>
+  );
+};
