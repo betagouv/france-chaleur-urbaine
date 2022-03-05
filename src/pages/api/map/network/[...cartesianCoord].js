effd25144e6f4728f02c733cff9bf5df30f0704b
Nicolas KOKLA <n.kokla@koaji.fr>
2022-03-05 18:19:57 +0100

    ♻️ Refactor API map/network for cartography - Add ssupport for options - Load multipart text data - Config minZoom per Layer - Support Filter data per Layer

--- src/pages/api/map/network/[...cartesianCoord].js
+++ src/pages/api/map/network/[...cartesianCoord].js
@@ -1,14 +1,21 @@
 import geojsonvt from 'geojson-vt';
 import vtpbf from 'vt-pbf';
-import readJsonFile from '../helper/readJsonFile';
+import { readFileAsync, readSplitFileAsync } from '../helper';
 
+const path = './public/geojson/';
 const filepaths = {
-  outline:
-    './public/geojson/dataset-1641576913364-[Trace-des-reseaux-de-chaleur-en-Ile-de-France].geojson',
-  substation:
-    './public/geojson/dataset-1642417907427-[Sous-stations-des-reseaux-de-chaleur-Ile-de-France].geojson',
-  boilerRoom:
-    './public/geojson/dataset-1642417995651-[Chaufferies-des-reseaux-de-chaleur-en-Ile-de-France].geojson',
+  outline: {
+    filename:
+      'dataset-1641576913364-[Trace-des-reseaux-de-chaleur-en-Ile-de-France].geojson',
+  },
+  substation: {
+    filename:
+      'dataset-1642417907427-[Sous-stations-des-reseaux-de-chaleur-Ile-de-France].geojson',
+  },
+  boilerRoom: {
+    filename:
+      'dataset-1642417995651-[Chaufferies-des-reseaux-de-chaleur-en-Ile-de-France].geojson',
+  },
 };
 
 const tileOptions = {
@@ -16,24 +23,61 @@ const tileOptions = {
   tolerance: 20,
 };
 
-const objTileIndex = Object.entries(filepaths).reduce((acc, [key, path]) => {
-  const geoJSON = readJsonFile(path);
-  const tileIndex = geojsonvt(geoJSON, tileOptions);
-  return {
-    ...acc,
-    [key]: tileIndex,
-  };
-}, {});
+const getObjectIndex = async () => {
+  const tileIndexPromises = Object.entries(filepaths).map(
+    ([key, { filename, featuresFilter, multipart }]) => {
+      return (
+        multipart
+          ? readSplitFileAsync(path, filename, true)
+          : readFileAsync(`${path}${filename}`, true)
+      ).then((rawdata) => {
+        console.info(
+          'Convert string to Json ...',
+          typeof rawdata,
+          rawdata.length
+        );
+        const geoJSON = JSON.parse(rawdata);
+        console.info('geoJson-ized', key);
+
+        if (featuresFilter) {
+          geoJSON.features = geoJSON.features.filter(featuresFilter);
+        }
+
+        const tileIndex = geojsonvt(geoJSON, tileOptions);
+        console.info('tileIndex-ized', typeof tileIndex);
+        return [key, tileIndex];
+      });
+    }
+  );
+  return Promise.all(tileIndexPromises).then((tileIndexPromise) =>
+    tileIndexPromise.reduce(
+      (acc, [key, tileIndex]) => ({
+        ...acc,
+        [key]: tileIndex,
+      }),
+      {}
+    )
+  );
+};
+
+let objTileIndex = {};
+const objTileIndexPromise = getObjectIndex().then(
+  (tileIndex) => (objTileIndex = tileIndex)
+);
 
 export default async function handleRequest(req, res) {
+  await objTileIndexPromise;
   const { cartesianCoord } = req.query;
   const [z, x, y] = cartesianCoord;
   const tiles = Object.entries(objTileIndex).reduce((acc, [key, tileIndex]) => {
-    const tile = tileIndex.getTile(+z, +x, +y);
+    const tile =
+      filepaths[key]?.minZoom && filepaths[key]?.minZoom > +z
+        ? null
+        : tileIndex.getTile(+z, +x, +y);
     return tile
       ? {
           ...(acc || {}),
-          [key]: tileIndex.getTile(+z, +x, +y),
+          [key]: tile,
         }
       : acc;
   }, null);
