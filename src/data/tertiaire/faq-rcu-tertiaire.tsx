29dacb753a91de8ecdad6dd99ba237246294a851
Nicolas KOKLA <n.kokla@koaji.fr>
2022-04-27 18:44:01 +0200

    ✨ new page 'tertiaire'

---
+++ src/data/tertiaire/faq-rcu-tertiaire.tsx
@@ -0,0 +1,30 @@
+const faqRcuTertiaire = [
+  {
+    label: 'Quelles énergies alimentent les réseaux de chaleur ?',
+    body: `Les réseaux de chaleur sont alimentés en moyenne à 60 % par des énergies renouvelables et de récupération disponibles localement (ENR&R). Cette chaleur "verte" est principalement issue :
+
+- de la récupération de la chaleur issue de l’incinération de nos déchets, première source d'ENR&R dans les réseaux de chaleur ;
+- de la combustion de la biomasse (bois déchet, bois issu de l’élagage urbain d’hiver, produits forestiers issus de l’exploitation ou de l’entretien des forêts, résidus agricoles…) ;
+- de la géothermie (chaleur extraite du sous-sol).
+
+Les 40 % restants correspondent majoritairement à du gaz naturel.  
+Il est à noter que le mix énergétique est variable d’un réseau à l’autre et que le gaz est généralement utilisé en appoint/secours pour assurer la continuité du service.
+`,
+  },
+  {
+    label:
+      'Quel est le tarif de la chaleur fournie par les réseaux ? La stabilité des prix est-elle garantie ?',
+    body: `La tarification de la chaleur diffère d’un réseau à l’autre, en fonction des caractéristiques du réseau (taux d’énergies renouvelables, nombre d’abonnés,...). Le prix de vente comprend une part fixe (abonnement) et une part variable relative à la consommation. Pour les réseaux de chaleur alimentés à plus de 50 % par des énergies renouvelables et de récupération, un taux de TVA réduit à 5.5 % s’applique sur l’ensemble de la facture. Ce taux réduit s’applique uniquement sur la part fixe (abonnement) pour les autres réseaux. Le prix de la chaleur peut évoluer d’une année sur l’autre (inflation, prix des énergies, coût de l'ingénierie...), mais ces fluctuations restent largement inférieures à celles subies par les prix du gaz, du fioul ou de l’électricité. Cela est d’autant plus vrai que le taux d’énergies renouvelables et de récupération du réseau est élevé.`,
+  },
+  {
+    label: 'Quels sont les acteurs d’un réseau de chaleur ?',
+    body: `Dans le secteur des réseaux de chaleur, les notions d’abonnés et d’usagers ne recouvrent pas tout à fait la même chose que dans les domaines de distribution et fourniture d’électricité ou de gaz, en alimentation individuelle. Ces deux entités sont en effet bien distinctes : on retrouve d’un côté les abonnés (syndic de copropriété,…) et de l’autre les usagers (propriétaires, locataires…).
+Au niveau contractuel, les relations entre les différents acteurs sont régies par un certain nombre de contrats :
+- la collectivité travaille en régie ou délègue et contrôle la production et la distribution de la chaleur sur le réseau primaire à un exploitant, via un contrat de concession de service public ;
+- l’exploitant conclut un contrat d’abonnement (règlement de service + police d’abonnement), avec les abonnés (clients) auxquels il fournit de la chaleur ;
+- les abonnés re-facturent par la suite le service aux usagers, conformément au règlement de copropriété.
+`,
+  },
+];
+
+export default faqRcuTertiaire;
