017efe75512d810411aafa81a5c0ce0cda2cadf0
Nicolas KOKLA <n.kokla@koaji.fr>
2022-06-02 18:31:48 +0200

    ♻️ new react Hooks : useContactFormFCU

--- src/components/HeadSliceForm/HeadSliceForm.tsx
+++ src/components/HeadSliceForm/HeadSliceForm.tsx
@@ -5,15 +5,9 @@ import {
   EnergyInputsLabelsType,
 } from '@components/EligibilityForm';
 import MarkdownWrapper from '@components/MarkdownWrapper';
-import markupData, {
-  facebookEvent,
-  googleAdsEvent,
-  linkedInEvent,
-  matomoEvent,
-} from '@components/Markup';
 import Slice from '@components/Slice';
-import React, { useCallback, useMemo, useRef, useState } from 'react';
-import { useBackEndFCU } from 'src/hooks';
+import React, { useMemo } from 'react';
+import { useContactFormFCU } from 'src/hooks';
 import {
   Container,
   FormLabel,
@@ -40,44 +34,6 @@ type HeadBannerType = {
   needGradient?: boolean;
 };
 
-const callMarkup__handleOnFetchAddress = (address: string) => {
-  matomoEvent(markupData.eligibilityTest.matomoEvent, [address]);
-  linkedInEvent(markupData.eligibilityTest.linkedInEvent);
-  facebookEvent(markupData.eligibilityTest.facebookEvent);
-  googleAdsEvent('10794036298', markupData.eligibilityTest.googleAdsEvent);
-};
-const callMarkup__handleOnSuccessAddress = ({
-  eligibility,
-  address,
-}: {
-  eligibility: boolean;
-  address?: string;
-}) => {
-  if (eligibility) {
-    matomoEvent(markupData.eligibilityTestOK.matomoEvent, [
-      address || 'Adresse indefini',
-    ]);
-    linkedInEvent(markupData.eligibilityTestOK.linkedInEvent);
-    googleAdsEvent('10794036298', markupData.eligibilityTestOK.googleAdsEvent);
-  } else {
-    matomoEvent(markupData.eligibilityTestKO.matomoEvent, [
-      address || 'Adresse indefini',
-    ]);
-    linkedInEvent(markupData.eligibilityTestKO.linkedInEvent);
-    googleAdsEvent('10794036298', markupData.eligibilityTestKO.googleAdsEvent);
-  }
-};
-const callMarkup__handleOnSubmitContact = (data: Record<string, any>) => {
-  const { estEligible: eligibility, address } = data;
-  const markupEligibilityKey = eligibility
-    ? 'contactFormEligible'
-    : 'contactFormIneligible';
-  matomoEvent(markupData[markupEligibilityKey].matomoEvent, [address]);
-  facebookEvent(markupData[markupEligibilityKey].facebookEvent);
-};
-
-const warningMessage = "N'oubliez pas d'indiquer votre type de chauffage.";
-
 const HeadSlice = ({
   bg,
   bgPos,
@@ -90,58 +46,19 @@ const HeadSlice = ({
   children,
   needGradient,
 }: HeadBannerType) => {
-  const [addressData, setAddressData] = useState({});
-  const [contactReady, setContactReady] = useState(false);
-  const [showWarning, setShowWarning] = useState(false);
-  const [messageSent, setMessageSent] = useState(false);
-  const [loadingStatus, setLoadingStatus] = useState('idle');
-  const [submitToFCU] = useBackEndFCU();
-
-  const EligibilityFormContactRef = useRef(null);
-
-  const handleOnChangeAddress = useCallback((data) => {
-    const { address, heatingType } = data;
-    setAddressData(data);
-    setShowWarning(address && !heatingType);
-  }, []);
-  const handleOnFetchAddress = useCallback(
-    ({ address }) => {
-      const { heatingType }: any = addressData;
-      setLoadingStatus('loading');
-      setMessageSent(false);
-      callMarkup__handleOnFetchAddress(address);
-      setShowWarning(address && !heatingType);
-    },
-    [addressData]
-  );
-  const handleOnSuccessAddress = useCallback((data: any) => {
-    const { address, heatingType, eligibility } = data;
-    callMarkup__handleOnSuccessAddress({ eligibility, address });
-    // TODO: Prefer context ?
-    setAddressData(data);
-    if (address && heatingType) {
-      setContactReady(true);
-      const scrollTimer = window.setTimeout(() => {
-        const { current }: any = EligibilityFormContactRef;
-        current?.scrollIntoView({
-          behavior: 'smooth',
-        });
-        setLoadingStatus('loaded');
-      }, 500);
-
-      return () => window.clearTimeout(scrollTimer);
-    }
-  }, []);
-
-  const handleOnSubmitContact = useCallback(
-    async (data: Record<string, any>) => {
-      callMarkup__handleOnSubmitContact(data);
-      await submitToFCU(data);
-      setAddressData({ ...addressData, ...data });
-      setMessageSent(true);
-    },
-    [addressData, submitToFCU]
-  );
+  const {
+    EligibilityFormContactRef,
+    addressData,
+    contactReady,
+    showWarning,
+    messageSent,
+    loadingStatus,
+    warningMessage,
+    handleOnChangeAddress,
+    handleOnFetchAddress,
+    handleOnSuccessAddress,
+    handleOnSubmitContact,
+  } = useContactFormFCU();
 
   const Child = useMemo(
     () =>
@@ -201,6 +118,7 @@ const HeadSlice = ({
       handleOnSuccessAddress,
       loadingStatus,
       showWarning,
+      warningMessage,
     ]
   );
 
--- src/components/SliceForm/SliceForm.tsx
+++ src/components/SliceForm/SliceForm.tsx
@@ -3,15 +3,9 @@ import {
   EligibilityFormContact,
   EligibilityFormMessageConfirmation,
 } from '@components/EligibilityForm';
-import markupData, {
-  facebookEvent,
-  googleAdsEvent,
-  linkedInEvent,
-  matomoEvent,
-} from '@components/Markup';
 import Slice from '@components/Slice';
-import React, { useCallback, useRef, useState } from 'react';
-import { useBackEndFCU } from 'src/hooks';
+import React from 'react';
+import { useContactFormFCU } from 'src/hooks';
 import {
   Container,
   FormWarningMessage,
@@ -20,97 +14,20 @@ import {
   SliceContactFormStyle,
 } from './SliceForm.style';
 
-const callMarkup__handleOnFetchAddress = (address: string) => {
-  matomoEvent(markupData.eligibilityTest.matomoEvent, [address]);
-  linkedInEvent(markupData.eligibilityTest.linkedInEvent);
-  facebookEvent(markupData.eligibilityTest.facebookEvent);
-  googleAdsEvent('10794036298', markupData.eligibilityTest.googleAdsEvent);
-};
-const callMarkup__handleOnSuccessAddress = ({
-  eligibility,
-  address,
-}: {
-  eligibility: boolean;
-  address?: string;
-}) => {
-  if (eligibility) {
-    matomoEvent(markupData.eligibilityTestOK.matomoEvent, [
-      address || 'Adresse indefini',
-    ]);
-    linkedInEvent(markupData.eligibilityTestOK.linkedInEvent);
-    googleAdsEvent('10794036298', markupData.eligibilityTestOK.googleAdsEvent);
-  } else {
-    matomoEvent(markupData.eligibilityTestKO.matomoEvent, [
-      address || 'Adresse indefini',
-    ]);
-    linkedInEvent(markupData.eligibilityTestKO.linkedInEvent);
-    googleAdsEvent('10794036298', markupData.eligibilityTestKO.googleAdsEvent);
-  }
-};
-const callMarkup__handleOnSubmitContact = (data: Record<string, any>) => {
-  const { estEligible: eligibility, address } = data;
-  const markupEligibilityKey = eligibility
-    ? 'contactFormEligible'
-    : 'contactFormIneligible';
-  matomoEvent(markupData[markupEligibilityKey].matomoEvent, [address]);
-  facebookEvent(markupData[markupEligibilityKey].facebookEvent);
-};
-
-const warningMessage = "N'oubliez pas d'indiquer votre type de chauffage.";
-
 const HeadSlice: React.FC = () => {
-  const [addressData, setAddressData] = useState({});
-  const [contactReady, setContactReady] = useState(false);
-  const [showWarning, setShowWarning] = useState(false);
-  const [messageSent, setMessageSent] = useState(false);
-  const [loadingStatus, setLoadingStatus] = useState('idle');
-  const [submitToFCU] = useBackEndFCU();
-
-  const EligibilityFormContactRef = useRef(null);
-
-  const handleOnChangeAddress = useCallback((data) => {
-    const { address, heatingType } = data;
-    setAddressData(data);
-    setShowWarning(address && !heatingType);
-  }, []);
-  const handleOnFetchAddress = useCallback(
-    ({ address }) => {
-      const { heatingType }: any = addressData;
-      setLoadingStatus('loading');
-      setMessageSent(false);
-      callMarkup__handleOnFetchAddress(address);
-      setShowWarning(address && !heatingType);
-    },
-    [addressData]
-  );
-  const handleOnSuccessAddress = useCallback((data: any) => {
-    const { address, heatingType, eligibility } = data;
-    callMarkup__handleOnSuccessAddress({ eligibility, address });
-    // TODO: Prefer context ?
-    setAddressData(data);
-    if (address && heatingType) {
-      setContactReady(true);
-      const scrollTimer = window.setTimeout(() => {
-        const { current }: any = EligibilityFormContactRef;
-        current?.scrollIntoView({
-          behavior: 'smooth',
-        });
-        setLoadingStatus('loaded');
-      }, 500);
-
-      return () => window.clearTimeout(scrollTimer);
-    }
-  }, []);
-
-  const handleOnSubmitContact = useCallback(
-    async (data: Record<string, any>) => {
-      callMarkup__handleOnSubmitContact(data);
-      await submitToFCU(data);
-      setAddressData({ ...addressData, ...data });
-      setMessageSent(true);
-    },
-    [addressData, submitToFCU]
-  );
+  const {
+    EligibilityFormContactRef,
+    addressData,
+    contactReady,
+    showWarning,
+    messageSent,
+    loadingStatus,
+    warningMessage,
+    handleOnChangeAddress,
+    handleOnFetchAddress,
+    handleOnSuccessAddress,
+    handleOnSubmitContact,
+  } = useContactFormFCU();
 
   return (
     <>
