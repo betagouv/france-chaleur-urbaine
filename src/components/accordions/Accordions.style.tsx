657e3c136e472bed41d7ace8e443fea3525573a6
Desoindx <xavier.desoindre@hotmail.fr>
2022-07-05 14:38:12 +0200

    Use dsfr accordion

--- src/components/accordions/Accordions.style.tsx
+++ src/components/accordions/Accordions.style.tsx
@@ -5,24 +5,3 @@ export const SectionTitle = styled.h2`
   margin-bottom: 32px;
   text-align: center;
 `;
-
-export const BodyAccordion = styled.div`
-  white-space: pre-wrap;
-`;
-
-type BodyAccordionWrapperProps = {
-  activeMenu: boolean;
-};
-export const BodyAccordionWrapper = styled.div.attrs<BodyAccordionWrapperProps>(
-  ({ activeMenu }) => ({
-    className: `fr-collapse ${activeMenu ? 'fr-collapse--expanded' : ''}`,
-    style: activeMenu
-      ? {
-          '--collapse': '0px',
-          maxHeight: 'none',
-        }
-      : {
-          '--collapse': '-40px',
-        },
-  })
-)<BodyAccordionWrapperProps>``;
