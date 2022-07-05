a0b0a5f38926e64b74361d6198be876569c58a51
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2022-07-05 09:39:39 +0100

    Fix(contact): Make structure mandatory (#196)
    * Fix(contact): Make structure mandatory
    
    * Add a default structure related to the current page

--- src/components/EligibilityForm/EligibilityFormContact.tsx
+++ src/components/EligibilityForm/EligibilityFormContact.tsx
@@ -222,7 +222,7 @@ const EligibilityFormContact = ({
     distance !== null &&
     distance !== undefined &&
     (distance <= 200
-      ? `Un reseau de chaleur se trouve à ${distance}m de ce batiment`
+      ? `Un reseau de chaleur se trouve à ${distance}m de ce bâtiment`
       : '');
   const linkToMap =
     addressCoords &&
