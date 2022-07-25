17a6c8a2fddb9cca787da2757dd8be1006107714
Desoindx <xavier.desoindre@hotmail.fr>
2022-07-25 10:08:57 +0200

    Fix build

--- src/core/infrastructure/repository/distance.ts
+++ src/core/infrastructure/repository/distance.ts
@@ -3,9 +3,7 @@ import db from 'src/db';
 const DEBUG = !!(process.env.API_DEBUG_MODE || null);
 
 const distanceReseau = async (lat: number, lon: number): Promise<number> => {
-  const { distance } = await db(
-    'potentiel_rcu — l_traces_rdch_l_r11_2022_05_new'
-  )
+  const { distance } = await db('reseaux_de_chaleur_new')
     .select(
       db.raw(`ST_Distance(
     ST_Transform('SRID=4326;POINT(${lon} ${lat})'::geometry, 2154),
