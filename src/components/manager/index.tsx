161e1a4e7dab43df9a1a8d006597aa365c8acf0a
Desoindx <xavier.desoindre@hotmail.fr>
2022-08-24 16:10:21 +0200

    feat(gestionnaire): Add filters

--- src/components/manager/index.tsx
+++ src/components/manager/index.tsx
@@ -1,13 +1,15 @@
-import { Table } from '@dataesr/react-dsfr';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { Select, Table, TextInput } from '@dataesr/react-dsfr';
 import { useEffect, useState } from 'react';
 import { useServices } from 'src/services';
-import { DemandSummary } from 'src/types/Summary/Demand';
+import { Demand } from 'src/types/Summary/Demand';
+import { Filters, NoResult } from './manager.style';
 
 const columns = [
   {
     name: 'Nom',
     label: 'Nom',
-    render: (demands: DemandSummary) =>
+    render: (demands: Demand) =>
       `${demands.Nom}${demands.Prénom ? ` ${demands.Prénom}` : ''}`,
   },
   { name: 'Structure', label: 'Structure' },
@@ -18,25 +20,137 @@ const columns = [
   { name: 'Type de chauffage', label: 'Type de chauffage' },
   { name: 'Mode de chauffage', label: 'Mode de chauffage' },
 ];
+
+const searchKeys: (keyof Demand)[] = ['Nom', 'Prénom', 'Mail'];
+const matchFilter = (filter: string, value: string | undefined) => {
+  return (
+    value &&
+    value
+      .toString()
+      .toLowerCase()
+      .replace(/é/g, 'e')
+      .includes(filter.toLowerCase())
+  );
+};
+
+const typeDeChauffageOptions = [
+  {
+    label: 'Tous',
+    value: '',
+  },
+  {
+    label: 'Collectif',
+    value: 'collectif',
+  },
+  {
+    label: 'Individuel',
+    value: 'individuel',
+  },
+];
+
+const modeDeChauffageOptions = [
+  {
+    label: 'Tous',
+    value: '',
+  },
+  {
+    label: 'Gaz',
+    value: 'gaz',
+  },
+  {
+    label: 'Fioul',
+    value: 'fioul',
+  },
+  {
+    label: 'Électricité',
+    value: 'électricité',
+  },
+];
+
 const Manager = () => {
   const { demandsService } = useServices();
-  const [demands, setDemands] = useState<DemandSummary[]>([]);
+  const [page, setPage] = useState(1);
+  const [demands, setDemands] = useState<Demand[]>([]);
+  const [filteredDemands, setFilteredDemands] = useState<Demand[]>([]);
+  const [filter, setFilter] = useState('');
+  const [filterModeChauffage, setFilterModeChauffage] = useState('');
+  const [filterTypeChauffage, setFilterTypeChauffage] = useState('');
 
   useEffect(() => {
     demandsService.fetchDemands().then(setDemands);
   }, [demandsService]);
+
+  useEffect(() => {
+    let tempDemands = demands;
+    if (filter) {
+      tempDemands = tempDemands.filter((demand) =>
+        searchKeys.some((key) => matchFilter(filter, demand[key]))
+      );
+    }
+
+    if (filterModeChauffage) {
+      tempDemands = tempDemands.filter(
+        (demand) =>
+          demand['Mode de chauffage'] &&
+          demand['Mode de chauffage'].toLowerCase() === filterModeChauffage
+      );
+    }
+
+    if (filterTypeChauffage) {
+      tempDemands = tempDemands.filter(
+        (demand) =>
+          demand['Type de chauffage'] &&
+          demand['Type de chauffage'].toLowerCase() === filterTypeChauffage
+      );
+    }
+
+    setFilteredDemands(tempDemands);
+    setPage(1);
+  }, [filter, filterModeChauffage, filterTypeChauffage, demands]);
+
   return (
     <>
       <h2>Mes demandes - {demands.length || 'Chargement...'}</h2>
       {demands.length > 0 && (
-        <Table
-          fixedHeader
-          columns={columns}
-          data={demands}
-          rowKey="id"
-          pagination
-          paginationPosition="center"
-        />
+        <>
+          <Filters>
+            <TextInput
+              label="Rechercher par nom ou par mail:"
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
+            />
+            <Select
+              // @ts-ignore: to fix in react-dsfr
+              label="Mode de chauffage:"
+              selected={filterModeChauffage}
+              // @ts-ignore: to fix in react-dsfr
+              onChange={(e) => setFilterModeChauffage(e.target.value)}
+              options={modeDeChauffageOptions}
+            />
+            <Select
+              // @ts-ignore: to fix in react-dsfr
+              label="Type de chauffage:"
+              selected={filterTypeChauffage}
+              // @ts-ignore: to fix in react-dsfr
+              onChange={(e) => setFilterTypeChauffage(e.target.value)}
+              options={typeDeChauffageOptions}
+            />
+          </Filters>
+          {filteredDemands.length > 0 ? (
+            <Table
+              fixedHeader
+              columns={columns}
+              data={filteredDemands}
+              rowKey="N° de dossier"
+              pagination
+              paginationPosition="center"
+              page={page}
+              setPage={setPage}
+            />
+          ) : (
+            <NoResult>Aucun résultats</NoResult>
+          )}
+        </>
       )}
     </>
   );
