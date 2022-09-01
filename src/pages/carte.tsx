eb628b30ce923390b965b910ead8d818b55c219b
Desoindx <xavier.desoindre@hotmail.fr>
2022-09-01 10:32:09 +0200

    feat(carto): Improve mobile view

--- src/pages/carte.tsx
+++ src/pages/carte.tsx
@@ -12,19 +12,6 @@ const MapWrapper = styled.div`
   @media (min-width: 992px) {
     height: calc(100vh - ${fullscreenHeaderHeight});
   }
-
-  @media (max-width: 1225px) {
-    display: none;
-  }
-`;
-
-const NotAvailable = styled.h2`
-  display: none;
-  margin: auto;
-  padding: 32px;
-  @media (max-width: 1225px) {
-    display: block;
-  }
 `;
 
 function LegalMentions() {
@@ -34,9 +21,6 @@ function LegalMentions() {
         <title>Carte des réseaux : France Chaleur Urbaine</title>
       </Head>
       <MainContainer currentMenu="/carte" fullscreen>
-        <NotAvailable>
-          Notre cartographie n'est pas encore disponible en version mobile
-        </NotAvailable>
         <MapWrapper>
           <Map />
         </MapWrapper>
