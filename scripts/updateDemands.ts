387af5d4e54f214a6526db787f39097cafa408b8
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2022-09-13 17:43:54 +0200

    feat(carto): Manage multiple BNB regions (#256)
    * query by region
    
    * improve filltiles
    
    * Add reims on city managed by DB
    
    * Add an update demands script
    
    * ignore 0 regions tiles
    
    * add new data

---
+++ scripts/updateDemands.ts
@@ -0,0 +1,72 @@
+import {
+  computeDistance,
+  getConso,
+  getConsoById,
+  getNbLogement,
+  getNbLogementById,
+} from '../src/core/infrastructure/repository/addresseInformation';
+import base from '../src/db/airtable';
+
+const updateDemands = async () => {
+  try {
+    const demands = await base('FCU - Utilisateurs').select().all();
+    console.log(
+      demands.filter(
+        (demand) => demand.get('Latitude') && demand.get('Longitude')
+      ).length,
+      'demandes a analyser'
+    );
+    let i = 1;
+    for (const demand of demands.filter(
+      (demand) => demand.get('Latitude') && demand.get('Longitude')
+    )) {
+      console.log('Demande', i++);
+      if (i < 237) {
+        continue;
+      }
+      const latitude = demand.get('Latitude') as number;
+      const longitude = demand.get('Longitude') as number;
+
+      const newValue: any = {};
+      let shouldUpdate = false;
+      if (!demand.get('Distance au réseau')) {
+        const distance = Math.round(await computeDistance(latitude, longitude));
+        newValue['Distance au réseau'] = distance;
+        shouldUpdate = true;
+      }
+      if (!demand.get('Conso')) {
+        const conso = await (demand.get('ID Conso')
+          ? getConsoById(demand.get('Id Conso') as string)
+          : getConso(latitude, longitude));
+        if (conso) {
+          newValue.Conso = conso.conso_nb;
+          newValue['ID Conso'] = conso.rownum;
+          shouldUpdate = true;
+        }
+      }
+      if (!demand.get('Logement')) {
+        const nbLogement = await (demand.get('ID BNB')
+          ? getNbLogementById(
+              demand.get('Id BNB') as string,
+              latitude,
+              longitude
+            )
+          : getNbLogement(latitude, longitude));
+
+        if (nbLogement) {
+          newValue.Logement = nbLogement.nb_logements;
+          newValue['ID BNB'] = nbLogement.fid;
+          shouldUpdate = true;
+        }
+      }
+      if (shouldUpdate) {
+        await base('FCU - Utilisateurs').update(demand.getId(), newValue);
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  }
+  process.exit(0);
+};
+
+updateDemands();
---
+++ src/core/infrastructure/repository/addresseInformation.ts
@@ -0,0 +1,124 @@
+import db from 'src/db';
+
+const DEBUG = !!(process.env.API_DEBUG_MODE || null);
+
+export const computeDistance = async (
+  lat: number,
+  lon: number
+): Promise<number> => {
+  const { distance } = await db('reseaux_de_chaleur')
+    .select(
+      db.raw(`ST_Distance(
+    ST_Transform('SRID=4326;POINT(${lon} ${lat})'::geometry, 2154),
+    ST_Transform(geom, 2154)
+  ) as distance`)
+    )
+    .orderBy('distance')
+    .first();
+
+  DEBUG && console.info(`Minimum distance is ${distance} meters.`);
+  return distance;
+};
+
+export const getConso = async (
+  lat: number,
+  lon: number
+): Promise<{ conso_nb: number; rownum: string } | null> => {
+  const result = await db('Donnees_de_conso_et_pdl_gaz_nat_2020')
+    .select('rownum', 'conso_nb')
+    .where(
+      db.raw(`
+        ST_INTERSECTS(
+          ST_Transform('SRID=4326;POINT(${lon} ${lat})'::geometry, 2154),
+          ST_BUFFER(ST_Transform(geom, 2154), 0.1)
+        )
+      `)
+    )
+    .first();
+  return result;
+};
+
+export const getConsoById = async (
+  id: string
+): Promise<{ conso_nb: number; rownum: string } | null> => {
+  const result = await db('Donnees_de_conso_et_pdl_gaz_nat_2020')
+    .select('rownum', 'conso_nb')
+    .where('rownum', id)
+    .first();
+  return result;
+};
+
+export const getNbLogement = async (
+  lat: number,
+  lon: number
+): Promise<{ nb_logements: number; fid: string } | null> => {
+  const region = await db('regions')
+    .select('bnb_nom')
+    .where(
+      db.raw(`
+      ST_Intersects(
+        ST_Transform(geom, 2154),
+        ST_Transform('SRID=4326;POINT(${lon} ${lat})'::geometry, 2154)
+        )
+    `)
+    )
+    .first();
+  const result = await db(region.bnb_nom)
+    .select(
+      'fid',
+      db.raw(`
+        CASE
+          WHEN cerffo2020_nb_log ISNULL 
+            THEN anarnc202012_nb_log
+          WHEN cerffo2020_nb_log < 1 
+            THEN anarnc202012_nb_log
+          ELSE cerffo2020_nb_log
+        END as nb_logements
+      `)
+    )
+    .where(
+      db.raw(`
+        ST_INTERSECTS(
+          ST_Transform('SRID=4326;POINT(${lon} ${lat})'::geometry, 2154),
+          ST_BUFFER(ST_Transform(geom_adresse, 2154), 0.1)
+        )
+      `)
+    )
+    .first();
+  return result;
+};
+
+export const getNbLogementById = async (
+  id: string,
+  lat: number,
+  lon: number
+): Promise<{ nb_logements: number; fid: string } | null> => {
+  const region = await db('regions')
+    .select('bnb_nom')
+    .where(
+      db.raw(`
+      ST_Intersects(
+        ST_Transform(geom, 2154),
+        ST_Transform('SRID=4326;POINT(${lon} ${lat})'::geometry, 2154)
+        )
+    `)
+    )
+    .first();
+
+  const result = await db(region.bnb_nom)
+    .select(
+      'fid',
+      db.raw(`
+        CASE
+          WHEN cerffo2020_nb_log ISNULL 
+            THEN anarnc202012_nb_log
+          WHEN cerffo2020_nb_log < 1 
+            THEN anarnc202012_nb_log
+          ELSE cerffo2020_nb_log
+        END as nb_logements
+      `)
+    )
+    .where('fid', id)
+    .first();
+  return result;
+};
