4f8b65528b17721a68ab4f66992aa76e19eeb66e
Desoindx <xavier.desoindre@hotmail.fr>
2022-11-15 13:37:16 +0100

    feat(eligibility): Add a bulk endpoint

---
+++ src/pages/api/map/bulkEligibilityStatus/index.ts
@@ -0,0 +1,133 @@
+import { getElibilityStatus } from '@core/infrastructure/repository/addresseInformation';
+import axios from 'axios';
+import crypto from 'crypto';
+import FormData from 'form-data';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import db from 'src/db';
+import { withCors } from 'src/services/api/cors';
+import { v4 as uuidv4 } from 'uuid';
+import * as yup from 'yup';
+
+const version = 1;
+
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  addresses: yup.array().of(yup.string()).min(1).required(),
+});
+
+const bulkEligibilitygibilityStatus = async (
+  req: NextApiRequest,
+  res: NextApiResponse<
+    { id: string; progress: number; result?: string; error?: boolean } | string
+  >
+) => {
+  if (req.method !== 'POST') {
+    return res.status(501);
+  }
+
+  if (!(await schema.isValid(req.body))) {
+    return res.status(400).send('Error');
+  }
+
+  const { addresses, email } = req.body;
+
+  const addressesCSV = [''].concat(addresses).join('\n');
+  const hash = crypto.createHash('sha1').update(addressesCSV).digest('hex');
+
+  const existingValue = await db('eligibility_tests')
+    .where('hash', hash)
+    .andWhere('version', version)
+    .first();
+
+  if (existingValue) {
+    return res.status(200).json({
+      id: existingValue.id,
+      error: existingValue.in_error,
+      progress: existingValue.progress / addresses.length,
+      result: existingValue.result,
+    });
+  }
+
+  const id = uuidv4();
+  res.status(200).json({
+    id: id,
+    progress: 0,
+  });
+
+  try {
+    await db('eligibility_tests').insert({
+      id,
+      version,
+      email,
+      hash,
+      addresses_count: addresses.length,
+    });
+
+    const form = new FormData();
+    form.append('data', addressesCSV, 'file.csv');
+    form.append('result_columns', 'latitude');
+    form.append('result_columns', 'longitude');
+    form.append('result_columns', 'result_label');
+    form.append('result_columns', 'result_score');
+
+    const addressesCoords = await axios.post(
+      'https://api-adresse.data.gouv.fr/search/csv/',
+      form,
+      {
+        headers: form.getHeaders(),
+      }
+    );
+
+    const addressesInformation = addressesCoords.data.split('\n');
+    const results = [
+      "Adresse,Adresse testée,Score,Eligibilité,Distance,Basé sur l'iris,PDP,Réseau en construction",
+    ];
+    let errorCount = 0;
+    let eligibileCount = 0;
+
+    for (let i = 1; i < addressesInformation.length - 1; i++) {
+      const informations: string[] = addressesInformation[i].split(',');
+      const score = informations[informations.length - 1];
+      const label = informations[informations.length - 2];
+      const lon = informations[informations.length - 3];
+      const lat = informations[informations.length - 4];
+
+      const result = await getElibilityStatus(Number(lat), Number(lon));
+      results.push(
+        `"${informations
+          .slice(0, informations.length - 4)
+          .join(',')}","${label}",${score},${result.isEligible},${
+          result.distance || ''
+        },${result.isEligible && result.isBasedOnIris},${result.inZDP},${
+          result.futurNetwork
+        }`
+      );
+      if (result.isEligible) {
+        eligibileCount++;
+      }
+      if (!lat || !lon) {
+        errorCount++;
+      }
+      await db('eligibility_tests')
+        .update({
+          progress: i,
+        })
+        .where('id', id);
+    }
+
+    const result = results.join('\n');
+    await db('eligibility_tests')
+      .update({
+        progress: addresses.length,
+        result,
+        error_count: errorCount,
+        eligibile_count: eligibileCount,
+      })
+      .where('id', id);
+  } catch (e) {
+    console.error(e);
+    await db('eligibility_tests').update({ in_error: true }).where('id', id);
+  }
+};
+
+export default withCors(bulkEligibilitygibilityStatus);
--- src/pages/api/map/eligibilityStatus.ts
+++ src/pages/api/map/eligibilityStatus.ts
@@ -1,14 +1,9 @@
-import {
-  closestNetwork,
-  isOnAnIRISNetwork,
-} from '@core/infrastructure/repository/addresseInformation';
-import inZDP from '@core/infrastructure/repository/zdp';
+import { getElibilityStatus } from '@core/infrastructure/repository/addresseInformation';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { withCors } from 'src/services/api/cors';
 import { ErrorResponse } from 'src/types/ErrorResponse';
 import { HeatNetworksResponse } from 'src/types/HeatNetworksResponse';
 
-const THRESHOLD = parseInt(process.env.NEXT_THRESHOLD || '0', 10);
 const eligibilitygibilityStatus = async (
   req: NextApiRequest,
   res: NextApiResponse<HeatNetworksResponse | ErrorResponse>
@@ -26,28 +21,7 @@ const eligibilitygibilityStatus = async (
       });
       return;
     }
-    const coords = { lat: Number(lat), lon: Number(lon) };
-    const zdpPromise = inZDP(coords.lat, coords.lon);
-    const irisNetwork = isOnAnIRISNetwork(coords.lat, coords.lon);
-
-    const network = await closestNetwork(coords.lat, coords.lon);
-    if (network.distance !== null && Number(network.distance) < 1000) {
-      const result = {
-        isEligible: Number(network.distance) <= THRESHOLD,
-        distance: Math.round(network.distance),
-        inZDP: await zdpPromise,
-        isBasedOnIris: false,
-        futurNetwork: network.date !== null,
-      };
-      return res.status(200).json(result);
-    }
-    const result = {
-      isEligible: await irisNetwork,
-      distance: null,
-      inZDP: await zdpPromise,
-      isBasedOnIris: true,
-      futurNetwork: false,
-    };
+    const result = await getElibilityStatus(Number(lat), Number(lon));
     return res.status(200).json(result);
   } catch (error) {
     // eslint-disable-next-line no-console
