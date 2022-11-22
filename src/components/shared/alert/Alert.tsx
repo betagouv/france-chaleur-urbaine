3dc1f5082a4d3a9cc13682c5ccaeb3289c5ee4ea
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2022-11-22 16:35:06 +0100

    chore: clean useless components (#397)

--- src/components/shared/alert/Alert.tsx
+++
@@ -1,9 +0,0 @@
-const Alert: React.FC = (props) => {
-  return (
-    <div className="fr-callout">
-      <header className="fr-callout__title" {...props} />
-    </div>
-  );
-};
-
-export default Alert;
