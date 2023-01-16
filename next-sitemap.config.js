a37897b675c93ad3d655a9daf3786af00fbf1033
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2023-01-16 18:03:34 +0100

    fix(sitemap): Add ressources (#429)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next-sitemap').IConfig} */
+
+const ressources = [
+  'reseau',
+  'energie-vertes',
+  'atouts',
+  'livraisons',
+  'role',
+  'faisabilite',
+  'avantages',
+  'aides',
+  'facture',
+  'prioritaire',
+  'acteurs',
+  'etat',
+];
+
 module.exports = {
   siteUrl: process.env.NEXTAUTH_URL || 'https://example.com',
   generateRobotsTxt: true, // (optional)
+  additionalPaths: () => {
+    return ressources.map((key) => ({ loc: `/ressource/${key}` }));
+  },
   // ...other options
 };
