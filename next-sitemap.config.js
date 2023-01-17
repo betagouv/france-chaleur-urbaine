3720371fd1538723ec0169ae47cecca5175839d3
Desoindx <xavier.desoindre@hotmail.fr>
2023-01-17 10:55:54 +0100

    fix(sitemap): Change root priority

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -19,7 +19,20 @@ module.exports = {
   siteUrl: process.env.NEXTAUTH_URL || 'https://example.com',
   generateRobotsTxt: true, // (optional)
   additionalPaths: () => {
-    return ressources.map((key) => ({ loc: `/ressources/${key}` }));
+    return ressources.map((key) => ({
+      loc: `/ressources/${key}`,
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+      changefreq: 'daily',
+    }));
+  },
+  transform: (config, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: path === '/' ? 1 : config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    };
   },
   // ...other options
 };
