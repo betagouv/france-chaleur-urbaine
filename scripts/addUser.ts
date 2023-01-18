cb11383a4c9194a33926a497d7d0bb95c5411f00
Desoindx <xavier.desoindre@hotmail.fr>
2023-01-18 14:58:28 +0100

    fix(admin): Fix add admin user

--- scripts/addUser.ts
+++ scripts/addUser.ts
@@ -26,7 +26,7 @@ const addUser = async (
     email,
     password: bcrypt.hashSync(password, salt),
     role,
-    gestionnaires: [gestionnaire],
+    gestionnaires: gestionnaire ? [gestionnaire] : undefined,
   });
   console.info('User created');
   process.exit(0);
