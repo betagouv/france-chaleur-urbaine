d8618c1885f6b5a58b1f19e0c9584e00a35781d0
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2023-02-02 09:14:07 +0000

    feat(carto): Add id and rates on networks (#447)

--- src/types/Summary/Network.d.ts
+++ src/types/Summary/Network.d.ts
@@ -3,5 +3,8 @@ export interface NetworkSummary {
   'Taux EnR&R': number;
   Gestionnaire: string;
   commentaires: string;
+  'Identifiant reseau': string;
+  'reseaux classes': string;
+  'contenu CO2 ACV': number;
   date: Date;
 }
