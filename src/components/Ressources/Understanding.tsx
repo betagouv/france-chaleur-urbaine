3796e58dcd138d6ae3070ddaffa3e1dbcb9f9806
Desoindx <xavier.desoindre@hotmail.fr>
2023-02-17 08:41:08 +0100

    feat(pages): Add ressources link on bottom

--- src/components/Ressources/Understanding.tsx
+++ src/components/Ressources/Understanding.tsx
@@ -1,42 +1,33 @@
 import { CardDescription, CardTitle } from '@dataesr/react-dsfr';
 import Link from 'next/link';
-import { growths, issues, understandings } from './config';
+import { Document, understandings } from './config';
 import {
   BottomLink,
   UnderstandingCard,
   UnderstandingCards,
 } from './Understanding.styles';
 
-const tertiaireCards = {
-  'energies-verte': issues['energies-verte'],
-  aides: understandings.aides,
-  avantages: understandings.avantages,
-  acteurs: growths.acteurs,
-};
-
-const Understanding = ({ tertiaire }: { tertiaire?: boolean }) => {
+const Understanding = ({ cards }: { cards?: Record<string, Document> }) => {
   return (
     <UnderstandingCards>
-      {Object.entries(tertiaire ? tertiaireCards : understandings).map(
-        ([key, understanding]) => (
-          <UnderstandingCard
-            key={key}
-            asLink={<Link href={`/ressources/${key}`} />}
-          >
-            <CardTitle>
-              {tertiaire && understanding.altTitle
-                ? understanding.altTitle
-                : understanding.title}
-            </CardTitle>
-            <CardDescription>
-              {understanding.description}
-              <br />
-              <br />
-              <BottomLink>Lire l'article</BottomLink>
-            </CardDescription>
-          </UnderstandingCard>
-        )
-      )}
+      {Object.entries(cards || understandings).map(([key, understanding]) => (
+        <UnderstandingCard
+          key={key}
+          asLink={<Link href={`/ressources/${key}`} />}
+        >
+          <CardTitle>
+            {cards && understanding.altTitle
+              ? understanding.altTitle
+              : understanding.title}
+          </CardTitle>
+          <CardDescription>
+            {understanding.description}
+            <br />
+            <br />
+            <BottomLink>Lire l'article</BottomLink>
+          </CardDescription>
+        </UnderstandingCard>
+      ))}
     </UnderstandingCards>
   );
 };
--- src/pages/conseiller.tsx
+++ src/pages/conseiller.tsx
@@ -1,7 +1,10 @@
 import HeadSliceForm from '@components/HeadSliceForm';
 import IFrame from '@components/IFrame/IFrame';
 import MarkdownWrapper from '@components/MarkdownWrapper';
+import { issues, understandings } from '@components/Ressources/config';
 import Simulator from '@components/Ressources/Contents/Simulator';
+
+import Understanding from '@components/Ressources/Understanding';
 import MainContainer from '@components/shared/layout';
 import { GlobalStyle } from '@components/shared/layout/Global.style';
 import SimulateurCO2 from '@components/SimulatorCO2';
@@ -10,6 +13,12 @@ import WrappedText from '@components/WrappedText';
 import Head from 'next/head';
 
 const currentPage = 'conseiller';
+const conseillerCards = {
+  'energies-verte': issues['energies-verte'],
+  avantages: understandings.avantages,
+  aides: understandings.aides,
+  prioritaire: understandings.prioritaire,
+};
 
 const Conseiller = () => {
   return (
@@ -26,39 +35,38 @@ const Conseiller = () => {
       </Head>
 
       <MainContainer currentMenu={`/${currentPage}`}>
-        <div>
-          <GlobalStyle />
+        <GlobalStyle />
 
-          <HeadSliceForm
-            bg="/img/head-slice-bg-professionnels.svg"
-            pageBody={`
+        <HeadSliceForm
+          bg="/img/head-slice-bg-professionnels.svg"
+          pageBody={`
 Bailleurs sociaux, bureaux d’étude, syndics, conseillers France Renov...
 # Avec le chauffage urbain, faites un choix d’avenir, écologique et économique`}
-            formLabel="Le bâtiment pourrait-il être raccordé à un réseau de chaleur&nbsp;?"
-            checkEligibility
-            needGradient
-            alwaysDisplayBulkForm
-            withBulkEligibility
-          />
-          <Slice
-            theme="white"
-            padding={8}
-            header={`## Estimer les émissions de CO2 évitées par le raccordement d’un bâtiment à un réseau de chaleur*`}
-          >
-            <SimulateurCO2 />
-          </Slice>
-          <Simulator withRedirection />
-          <Slice
-            padding={8}
-            header={`
+          formLabel="Le bâtiment pourrait-il être raccordé à un réseau de chaleur&nbsp;?"
+          checkEligibility
+          needGradient
+          alwaysDisplayBulkForm
+          withBulkEligibility
+        />
+        <Slice
+          theme="white"
+          padding={8}
+          header={`## Estimer les émissions de CO2 évitées par le raccordement d’un bâtiment à un réseau de chaleur*`}
+        >
+          <SimulateurCO2 />
+        </Slice>
+        <Simulator withRedirection />
+        <Slice
+          padding={8}
+          header={`
 ## La cartographie France Chaleur Urbaine
 
 *Un outil incontournable au service de l’accélération des raccordements*
 `}
-          >
-            <WrappedText
-              textClassName="slice-carto-text"
-              body={`
+        >
+          <WrappedText
+            textClassName="slice-carto-text"
+            body={`
 Visualisez sur une même carte :
 
 ::check-item[Les tracés des réseaux de chaleur]
@@ -67,18 +75,18 @@ Visualisez sur une même carte :
 
 :button-link[Voir la cartographie]{href="./carte"}
 `}
-              imgSrc="/img/rcu-carto.jpg"
-              reverse
-            />
-          </Slice>
+            imgSrc="/img/rcu-carto.jpg"
+            reverse
+          />
+        </Slice>
 
-          <IFrame />
+        <IFrame />
 
-          <Slice theme="white" padding={8}>
-            <Slice header={`## Comment se passe un raccordement&nbsp;?`} />
-            <WrappedText
-              center
-              body={`
+        <Slice theme="white" padding={8}>
+          <Slice header={`## Comment se passe un raccordement&nbsp;?`} />
+          <WrappedText
+            center
+            body={`
 *Le 100 rue du Paradis est un immeuble chauffé par une chaudière collective au gaz ayant 20 ans.*
 
 Un conseiller en rénovation ou le gestionnaire de l’immeuble cherche un chauffage *[plus performant et responsable](/ressources/avantages#contenu)* et vérifie sur *France Chaleur Urbaine* si le bâtiment est raccordable. 
@@ -87,12 +95,12 @@ Un conseiller en rénovation ou le gestionnaire de l’immeuble cherche un chauf
 
 Le conseiller en rénovation ou le gestionnaire de l’immeuble demande via France Chaleur Urbaine à être *mis en relation avec le gestionnaire du réseau de chaleur*, qui le recontacte pour l’informer sur les conditions de raccordement.
 `}
-              imgSrc="/img/user-experience-simple-1.svg"
-              reverse
-            />
-            <WrappedText
-              center
-              body={`
+            imgSrc="/img/user-experience-simple-1.svg"
+            reverse
+          />
+          <WrappedText
+            center
+            body={`
 *Les frais de raccordement s’élèvent à 105 000 €.*
 *Le « [Coup de pouce chauffage des bâtiments résidentiels collectifs et tertiaires](/ressources/aides#contenu) » permet de réduire ce coût à 50 000 €, soit 400 € par lot.*
 
@@ -102,16 +110,18 @@ Depuis, l’immeuble bénéficie d’une bonne température de chauffe, d’une 
 
 *Une démarche écologique et économique.*
 `}
-              imgSrc="/img/user-experience-simple-2.svg"
+            imgSrc="/img/user-experience-simple-2.svg"
+          />
+          <Slice>
+            <MarkdownWrapper
+              value={`:button-link[Télécharger notre guide]{href="./guide-france-chaleur-urbaine" tagName="downloadLink" trackEvent="Guide FCU, ${currentPage}" target="_blank"}`}
+              className="fcuSolutionForFuturFooter"
             />
-            <Slice>
-              <MarkdownWrapper
-                value={`:button-link[Télécharger notre guide]{href="./guide-france-chaleur-urbaine" tagName="downloadLink" trackEvent="Guide FCU, ${currentPage}" target="_blank"}`}
-                className="fcuSolutionForFuturFooter"
-              />
-            </Slice>
           </Slice>
-        </div>
+        </Slice>
+        <Slice theme="color">
+          <Understanding cards={conseillerCards} />
+        </Slice>
       </MainContainer>
     </>
   );
