d893e0a4bde9f49335dec1a3313e43d6478b795d
Desoindx <xavier.desoindre@hotmail.fr>
2023-03-13 09:41:38 +0100

    fix(infographie): Use regular image

--- src/components/Infographie/index.tsx
+++ src/components/Infographie/index.tsx
@@ -1,5 +1,4 @@
 import { Icon } from '@dataesr/react-dsfr';
-import Image from 'next/image';
 import { Glass, ImageContainer } from './index.styles';
 
 const Infographie = ({
@@ -13,7 +12,7 @@ const Infographie = ({
 }) => {
   return (
     <ImageContainer href={src} target="_blank">
-      <Image width={216} height={height || 350} src={src} alt={alt} />
+      <img width={216} height={height || 350} src={src} alt={alt} />
       <Glass>
         <Icon name="ri-search-eye-fill" size="2x" />
       </Glass>
