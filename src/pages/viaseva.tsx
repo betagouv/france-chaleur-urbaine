7b8fa0a7be8921742719f1df98a3a7994f9f84f5
Desoindx <xavier.desoindre@hotmail.fr>
2023-05-22 16:30:57 +0200

    feat: Add viaseva iframe

---
+++ src/pages/viaseva.tsx
@@ -0,0 +1,25 @@
+import Map from '@components/Map/Map';
+
+const ViasevaMap = () => {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Map
+        initialLayerDisplay={{
+          outline: true,
+          zoneDP: false,
+          demands: false,
+          raccordements: false,
+          gasUsageGroup: false,
+          buildings: false,
+          gasUsage: [],
+          energy: [],
+          gasUsageValues: [1000, Number.MAX_VALUE],
+          energyGasValues: [50, Number.MAX_VALUE],
+          energyFuelValues: [50, Number.MAX_VALUE],
+        }}
+      />
+    </div>
+  );
+};
+
+export default ViasevaMap;
