a239ff29918736937b1e2ef769442ef9689f10d0
Desoindx <xavier.desoindre@hotmail.fr>
2023-06-05 11:47:02 +0200

    feat: Add other building types to contact form

--- src/components/Manager/Tag.tsx
+++ src/components/Manager/Tag.tsx
@@ -3,6 +3,8 @@ import { Container } from './Tag.styles';
 const colors: Record<string, { color: string; backgroundColor: string }> = {
   ZDP: { color: '#454B58', backgroundColor: '#FFDA8F' },
   Tertiaire: { color: '#DAE6FF', backgroundColor: '#406ED2' },
+  'Maison individuelle': { color: '#264653', backgroundColor: '#2a9d8f' },
+  'Bailleur social': { color: '#006d77', backgroundColor: '#83c5be' },
   Copropriété: { color: '#86BD46', backgroundColor: '#EBF5E0' },
   'Fioul collectif': { color: '#FFFFFF', backgroundColor: '#4550E5' },
   'Gaz individuel': { color: '#EE9817', backgroundColor: '#FBE69C' },
