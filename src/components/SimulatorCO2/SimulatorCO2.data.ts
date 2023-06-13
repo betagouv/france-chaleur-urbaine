6512bcb4288c2e1766095f3754c676b473cf03e1
Desoindx <xavier.desoindre@hotmail.fr>
2023-06-13 09:17:26 +0200

    Change texts

--- src/components/SimulatorCO2/SimulatorCO2.data.ts
+++ src/components/SimulatorCO2/SimulatorCO2.data.ts
@@ -3,13 +3,14 @@ export const dataSimulator = {
     'Estimer les émissions de CO2 évitées par le raccordement de vos locaux à un réseau de chaleur*',
   label: {
     conso: 'Consommation (MWh/an)',
-    surf: 'Surface (m²)',
-    log: 'Nombre de logements**',
+    surf: 'Surface (m²)**',
+    log: 'Nombre de logements***',
     chauffage: 'Chauffage actuel',
   },
   annotation: [
-    '*Bâtiment tertiaire construit entre 2005 et 2012, consommant 78kWhu/m2/an pour son chauffage et se raccordant à un réseau de chaleur alimenté à 60% par des énergies renouvelables',
-    '**Logement moyen de 70 m2 consommant 10MWh/an',
+    '*Raccordement à un réseau de chaleur alimenté à 60% par des énergies renouvelables',
+    '**Bâtiment tertiaire construit entre 2005 et 2012, consommant 78kWhu/m2/an pour son chauffage',
+    '***Logement moyen de 70 m2 consommant 10MWh/an',
   ],
   styleInline: true,
 };
