2e26ccbf038426e32d7c5105de509aac683e0987
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2023-06-13 09:56:14 +0200

    feat: Add logement option on CO2 simularot (#507)
    * feat: Add logement option on CO2 simularot
    
    * Change texts

--- src/components/SimulatorCO2/SimulatorCO2.data.ts
+++ src/components/SimulatorCO2/SimulatorCO2.data.ts
@@ -1,31 +1,16 @@
-export const dataSimulator: { [key: string]: any } = {
-  copropriete: {
-    chapo:
-      'Estimer les émissions de CO2 évitées par le raccordement de votre copropriété à un réseau de chaleur*',
-    label: {
-      conso: 'Consommation (MWh/an)',
-      surf: 'Surface (m²)**',
-      log: 'Nombre de logements***',
-      chauffage: 'Chauffage actuel',
-    },
-    annotation: [
-      '* Raccordement à un réseau de chaleur alimenté à 60% par des énergies renouvelables',
-      '**Logement moyen consommant 140 kWh/m2/an',
-      '*** Logement moyen de 70 m2 consommant 10MWh/an',
-    ],
-    styleInline: false,
+export const dataSimulator = {
+  chapo:
+    'Estimer les émissions de CO2 évitées par le raccordement de vos locaux à un réseau de chaleur*',
+  label: {
+    conso: 'Consommation (MWh/an)',
+    surf: 'Surface (m²)**',
+    log: 'Nombre de logements***',
+    chauffage: 'Chauffage actuel',
   },
-  tertiaire: {
-    chapo:
-      'Estimer les émissions de CO2 évitées par le raccordement de vos locaux à un réseau de chaleur*',
-    label: {
-      conso: 'Consommation (MWh/an)',
-      surf: 'Surface (m²)',
-      chauffage: 'Chauffage actuel',
-    },
-    annotation: [
-      '*Bâtiment tertiaire construit entre 2005 et 2012, consommant 78kWhu/m2/an pour son chauffage et se raccordant à un réseau de chaleur alimenté à 60% par des énergies renouvelables',
-    ],
-    styleInline: true,
-  },
+  annotation: [
+    '*Raccordement à un réseau de chaleur alimenté à 60% par des énergies renouvelables',
+    '**Bâtiment tertiaire construit entre 2005 et 2012, consommant 78kWhu/m2/an pour son chauffage',
+    '***Logement moyen de 70 m2 consommant 10MWh/an',
+  ],
+  styleInline: true,
 };
