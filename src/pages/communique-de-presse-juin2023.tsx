8e16676e0f4be76e6f4ce1a08468ef0e73f5a639
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2023-06-20 11:59:25 +0200

    feat: Add CP 06/2023 (#514)

---
+++ src/pages/communique-de-presse-juin2023.tsx
@@ -0,0 +1,12 @@
+import PressJune23 from '@components/Press/PressJune23';
+import MainContainer from '@components/shared/layout/MainContainer';
+
+const QuiSommesNous = () => {
+  return (
+    <MainContainer currentMenu={'/communique-de-presse-juin2023'}>
+      <PressJune23 />
+    </MainContainer>
+  );
+};
+
+export default QuiSommesNous;
