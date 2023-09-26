0b390df5eeb012528a219e85205370b6faf1a80d
Clemence <technique@clem-dev.com>
2023-09-26 19:01:12 +0200

    fix: CoproGuide link alignment

--- src/components/Coproprietaire/CoproGuide.tsx
+++ src/components/Coproprietaire/CoproGuide.tsx
@@ -5,13 +5,15 @@ const CoproGuide = ({ guideClassName }: { guideClassName?: string }) => {
   return (
     <Guide className={guideClassName}>
       <img src="/img/copro_guide.png" alt="Guide de raccordement" />
-      <div className="fr-btn fr-mt-2w fr-ml-4w">
-        <Link
-          href="/documentation/guide-france-chaleur-urbaine.pdf"
-          target="_blank"
-        >
-          Télécharger notre guide
-        </Link>
+      <div>
+        <div className="fr-btn fr-mt-2w fr-ml-4w">
+          <Link
+            href="/documentation/guide-france-chaleur-urbaine.pdf"
+            target="_blank"
+          >
+            Télécharger notre guide
+          </Link>
+        </div>
       </div>
     </Guide>
   );
