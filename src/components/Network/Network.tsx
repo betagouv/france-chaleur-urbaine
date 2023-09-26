8c37b391def074c99475b7f3c816943cd0094033
Desoindx <xavier.desoindre@hotmail.fr>
2023-09-26 18:00:43 +0200

    fix: Single cold network visibility

--- src/components/Network/Network.tsx
+++ src/components/Network/Network.tsx
@@ -492,9 +492,9 @@ const Network = ({
                   center={[network.lon, network.lat]}
                   initialZoom={13}
                   initialLayerDisplay={{
-                    outline: !isCold,
+                    outline: true,
                     futurOutline: false,
-                    coldOutline: isCold,
+                    coldOutline: true,
                     zoneDP: false,
                     demands: false,
                     raccordements: false,
