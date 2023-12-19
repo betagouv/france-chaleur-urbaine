2d2cc94f42af66f8a48edbf7a81dcc9829444fee
Maxime Dréau <maxime@dreau.fr>
2023-12-19 12:40:09 +0100

    chore: validate and review tileCoordinates route

---
+++ src/pages/api/map/[type]/[...tileCoordinates].ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import getTiles from 'src/services/tiles';
+import { zDataType } from 'src/services/tiles.config';
+import zod from 'zod';
+
+export default async function handleRequest(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { type, tileCoordinates } = zod
+    .object({
+      type: zDataType,
+      tileCoordinates: zod.array(zod.coerce.number()).length(3),
+    })
+    .parse(req.query);
+  const [z, x, y] = tileCoordinates;
+  const tiles = await getTiles(type, x, y, z);
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  if (!tiles) {
+    res.status(204).end();
+    return;
+  }
+
+  res.setHeader('Content-Type', 'application/protobuf');
+  res.status(200).send(tiles);
+}
