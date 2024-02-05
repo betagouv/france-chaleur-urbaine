0280a367a42866dae0e90caaf0d65e09525bc26b
Maxime Dréau <maxime@dreau.fr>
2024-02-05 18:17:28 +0100

    feat: add requests logging

--- src/pages/api/map/[type]/[...tileCoordinates].ts
+++ src/pages/api/map/[type]/[...tileCoordinates].ts
@@ -11,26 +11,28 @@ export const config = {
   },
 };
 
-export default handleRouteErrors(async function handleRequest(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const {
-    type,
-    tileCoordinates: [z, x, y],
-  } = await validateObjectSchema(req.query, {
-    type: zDataType,
-    tileCoordinates: zod.array(zod.coerce.number()).length(3),
-  });
-  const tiles = await getTiles(type, x, y, z);
+export default handleRouteErrors(
+  async function handleRequest(req: NextApiRequest, res: NextApiResponse) {
+    const {
+      type,
+      tileCoordinates: [z, x, y],
+    } = await validateObjectSchema(req.query, {
+      type: zDataType,
+      tileCoordinates: zod.array(zod.coerce.number()).length(3),
+    });
+    const tiles = await getTiles(type, x, y, z);
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-  if (!tiles) {
-    res.status(204).end();
-    return;
+    if (!tiles) {
+      res.status(204).end();
+      return;
+    }
+
+    res.setHeader('Content-Type', 'application/protobuf');
+    res.status(200).send(tiles);
+  },
+  {
+    logRequest: false,
   }
-
-  res.setHeader('Content-Type', 'application/protobuf');
-  res.status(200).send(tiles);
-});
+);
