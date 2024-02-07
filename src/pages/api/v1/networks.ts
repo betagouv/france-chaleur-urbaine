1802e35abc3caf53207b8a776dc641bb4af46466
Maxime Dréau <maxime@dreau.fr>
2024-02-07 19:03:09 +0100

    fix: harmonisation commentaire code

--- src/pages/api/v1/networks.ts
+++ src/pages/api/v1/networks.ts
@@ -14,7 +14,7 @@ export const config = {
 
 const rateLimitMiddleware = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 20, // Limit each IP to 20 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   handler: (req, res, next) => {
