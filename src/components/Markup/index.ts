d4a5856bd7361f09782d412836ad79aee1fa6953
Maxime Dréau <maxime@dreau.fr>
2024-03-18 10:30:44 +0100

    fix: cleanup old code

--- src/components/Markup/index.ts
+++ src/components/Markup/index.ts
@@ -1,4 +1,3 @@
 export { default as FacebookMarkup } from './FacebookMarkup';
 export { default as GoogleAdsMarkup } from './GoogleAdsMarkup';
 export { default as LinkedInMarkup } from './LinkedInMarkup';
-export { default as MatomoMarkup } from './MatomoMarkup';
