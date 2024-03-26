37266499384b184b78de36698ee210fc9aa07bde
Maxime Dréau <maxime@dreau.fr>
2024-03-26 10:40:40 +0100

    refactor: integrate @codegouvfr/react-dsfr

--- next.config.js
+++ next.config.js
@@ -115,6 +115,8 @@ module.exports = withBundleAnalyzer(
       eslint: {
         ignoreDuringBuilds: true,
       },
+      reactStrictMode: true,
+      swcMinify: true,
       async redirects() {
         return [
           {
@@ -181,13 +183,18 @@ module.exports = withBundleAnalyzer(
           })),
         ];
       },
-      webpack: function (config) {
+      webpack: (config) => {
         config.module.rules.push({
           test: /\.md$/,
           use: 'raw-loader',
         });
+        config.module.rules.push({
+          test: /\.woff2$/,
+          type: 'asset/resource',
+        });
         return config;
       },
+      transpilePackages: ['@codegouvfr/react-dsfr'],
     },
     {
       // For all available options, see:
