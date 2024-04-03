ae04184edff77f4caf166848d8b2ed1d5b2ef471
Maxime Dréau <maxime@dreau.fr>
2024-04-03 12:31:21 +0200

    style: revue page actu

--- src/pages/actus/[slug].tsx
+++ src/pages/actus/[slug].tsx
@@ -1,25 +1,33 @@
 import MarkdownWrapper from '@components/MarkdownWrapper';
 import SimplePage from '@components/shared/page/SimplePage';
-import Slice from '@components/Slice';
+import Box from '@components/ui/Box';
+import Heading from '@components/ui/Heading';
+import Link from '@components/ui/Link';
+import Text from '@components/ui/Text';
 import { getArticle } from '@data/contents';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { createGlobalStyle } from 'styled-components';
+import { Article } from 'src/types/Article';
+import styled from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
-  .markdown-actu {
-    img {
-      max-width: 100%;
-    }
+const ArticleContentWrapper = styled(Box)`
+  img {
+    max-width: 100%;
   }
   a {
     word-break: break-word;
   }
 `;
 
+const ThemeTagItem = styled.li`
+  background-color: var(--blue-france-sun-113-625) !important;
+  color: white !important;
+  line-height: 1.8em !important;
+`;
+
 const Article = () => {
   const router = useRouter();
-  const [content, setContent] = useState('');
+  const [article, setArticle] = useState<Article | null>(null);
 
   useEffect(() => {
     if (!router.query.slug) {
@@ -28,18 +36,67 @@ const Article = () => {
 
     const article = getArticle(router.query.slug as string);
     if (article) {
-      setContent(article.content);
+      setArticle(article);
     } else {
       router.push('/actus');
     }
-  }, [router]);
+  }, [router.query]);
+
+  const lines = article?.content?.split('\n') ?? [];
+  const title = lines.slice(0, 1)?.[0]?.substring(1); // remove the hashtag
+  const content = lines.slice(1).join('\n');
 
   return (
-    <SimplePage currentPage="/ressources">
-      <GlobalStyle />
-      <Slice padding={8}>
-        <MarkdownWrapper value={content} className="markdown-actu" />
-      </Slice>
+    <SimplePage currentPage="/ressources" title={title}>
+      <Box className="fr-container fr-mb-n2w fr-mb-md-n4w">
+        <nav
+          role="navigation"
+          className="fr-breadcrumb"
+          aria-label="Vous êtes ici"
+        >
+          <ol className="fr-breadcrumb__list">
+            <li>
+              <Link className="fr-breadcrumb__link" href="/">
+                Accueil
+              </Link>
+            </li>
+            <li>
+              <Link className="fr-breadcrumb__link" href="/actus">
+                Nos actualités
+              </Link>
+            </li>
+            <li>
+              <a className="fr-breadcrumb__link" aria-current="page">
+                {title}
+              </a>
+            </li>
+          </ol>
+        </nav>
+      </Box>
+
+      <Box backgroundColor="blue-cumulus-950-100">
+        <Box maxWidth="1000px" mx="auto" pt="8w" pb="4w" px="2w">
+          <Heading size="h1" color="blue-france" mb="0">
+            {title}
+          </Heading>
+
+          <Text className="fr-icon-arrow-right-line" my="2w">
+            Publié le {article?.publishedDate.toLocaleDateString('fr-FR')}
+          </Text>
+
+          <ul className="fr-tags-group">
+            {article?.themes.map((theme, index) => (
+              <ThemeTagItem key={index} className="fr-tag">
+                {theme}
+              </ThemeTagItem>
+            ))}
+          </ul>
+        </Box>
+      </Box>
+
+      <ArticleContentWrapper pt="5w" pb="10w" className="fr-container">
+        <MarkdownWrapper value={content} />
+      </ArticleContentWrapper>
     </SimplePage>
   );
 };
