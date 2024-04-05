0f523eb3ccdf016b43aca9275fc9d961c26729fc
Maxime Dréau <maxime@dreau.fr>
2024-04-05 16:25:15 +0200

    fix: bad import

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -67,7 +67,6 @@ import {
 } from 'src/services/Map/map-configuration';
 import {
   LayerId,
-  SourceId,
   applyMapConfigurationToLayers,
   buildMapLayers,
   layerSymbolsImagesURLs,
@@ -77,6 +76,7 @@ import useRouterReady from '@hooks/useRouterReady';
 import MapSearchForm from './components/MapSearchForm';
 import CardSearchDetails from './components/CardSearchDetails';
 import { layersWithDynamicContentPopup } from './components/DynamicMapPopupContent';
+import { SourceId } from 'src/services/tiles.config';
 
 const mapSettings = {
   defaultLongitude: 2.3,
