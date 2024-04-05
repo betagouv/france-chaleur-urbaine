eb9858394d9b622d4270fac758baa859a38b512d
Maxime Dréau <maxime@dreau.fr>
2024-04-05 17:11:24 +0200

    feat: amélioration du script pour avoir du zero downtime

--- scripts/copyLocalTableToRemote.sh
+++ scripts/copyLocalTableToRemote.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -e
 
 usage() {
-  echo "Usage: copyLocalTableToRemote.sh <dev|prod> <table_name>"
+  echo "Usage: copyLocalTableToRemote.sh <dev|prod> <table_name> [--data-only]"
   echo "Used to synchronize a local table with a remote table (dropped and recreated each sync)"
+  echo "Use --data-only to perform a zero downtime update (in a transaction)"
   echo "Use the env variable SCALINGO_TUNNEL_ARGS=\"-i $HOME/.ssh/keys/path_to_keys_ed\" if your SSH key is not ~/.ssh/id_rsa"
 }
 
 env=$1
 table=$2
+options=$3
 if [[ $env != "dev" && $env != "prod" ]]; then
   usage
   exit 1
@@ -18,8 +20,16 @@ if [[ "$table" = "" ]]; then
   exit 1
 fi
 
+if [[ $options == "--data-only" ]] ; then
+  dataonly=true
+fi
+
 # export depuis BDD locale
-pg_dump postgres://postgres:postgres_fcu@localhost:5432 --format=c --no-owner -t ${table} >/tmp/table.dump
+if [[ $dataonly = "true" ]]; then
+  pg_dump postgres://postgres:postgres_fcu@localhost:5432/postgres --data-only -t $table >/tmp/table.dump.sql
+else
+  pg_dump postgres://postgres:postgres_fcu@localhost:5432/postgres --format=c --no-owner -t $table >/tmp/table.dump
+fi
 
 if [[ $env = "prod" ]]; then
   SCALINGO_APP=france-chaleur-urbaine
@@ -38,9 +48,14 @@ POSTGRESQL_URL=$(scalingo -a $SCALINGO_APP env-get SCALINGO_POSTGRESQL_URL)
 export PGUSER=$(expr $POSTGRESQL_URL : '.*/\([^:]*\):.*')
 export PGPASSWORD=$(expr $POSTGRESQL_URL : '.*:\([^@]*\)@.*')
 
-# drop if exists
-psql postgres://localhost:10000 -c "drop table if exists ${table};"
-pg_restore --no-owner -d postgres://localhost:10000 /tmp/table.dump
+# copie des données
+if [[ $dataonly = "true" ]]; then
+  # noter le delete plutôt que truncate pour ne pas locker la table et bloquer les requêtes
+  psql -v ON_ERROR_STOP=1 postgres://localhost:10000 --single-transaction -c "delete from $table;" -f /tmp/table.dump.sql
+else
+  pg_restore --no-owner --clean --if-exists -d postgres://localhost:10000 /tmp/table.dump
+fi
+
 
 # ferme le tunnel
 kill %1
