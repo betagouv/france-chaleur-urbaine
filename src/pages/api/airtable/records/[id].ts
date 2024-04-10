edcf376e7a7687bb26cb7cf992649938db807b74
Maxime Dréau <maxime@dreau.fr>
2024-04-10 18:27:54 +0200

    refactor: validation route /api/airtable/records/:id

--- src/pages/api/airtable/records/[id].ts
+++ src/pages/api/airtable/records/[id].ts
@@ -1,50 +1,34 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import {
+  handleRouteErrors,
+  requirePutMethod,
+  validateObjectSchema,
+} from '@helpers/server';
+import type { NextApiRequest } from 'next';
 import base from 'src/db/airtable';
 import { Airtable } from 'src/types/enum/Airtable';
+import { z } from 'zod';
 
-const creationCallBack =
-  (res: NextApiResponse<any>) => (err: any, records: any) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: err });
-      return;
-    }
-    const jsonResponse = {
-      ids: records,
-    };
-    res.status(200).json(jsonResponse);
-  };
+const zAirtableId = z.string().length(17); // e.g. rec6nCFUO7Nzj6M9n
 
-export default async (req: NextApiRequest, res: NextApiResponse<any>) => {
-  if (req.method !== 'PUT') {
-    return res.status(405).send({ message: 'Only PUT requests allowed' });
-  }
+export default handleRouteErrors(async (req: NextApiRequest) => {
+  requirePutMethod(req);
 
-  const { id } = req.query;
-  const { type, ...values } = req.body;
-  if (process.env.NEXT_PUBLIC_MOCK_USER_CREATION === 'true') {
-    console.log('Updating', id, 'to', type, values);
-    return res.status(200).json({
-      type: type,
-      values: values,
-    });
-  }
+  const { id } = await validateObjectSchema(req.query, {
+    id: zAirtableId,
+  });
+  // would be best with the type in the URL
+  const { type, ...values } = await validateObjectSchema(req.body, {
+    type: z.enum([Airtable.UTILISATEURS]),
+    sondage: z.array(z.string().max(300)), // may contain an unknown string as the last value if Other is included
+  });
 
   switch (type) {
     case Airtable.UTILISATEURS: {
-      base(Airtable.UTILISATEURS).update(
-        id as string,
+      await base(Airtable.UTILISATEURS).update(
+        id,
         { Sondage: values.sondage },
-        { typecast: true },
-        async (error, records) => {
-          creationCallBack(res)(error, records);
-        }
+        { typecast: true }
       );
-      break;
     }
-
-    default:
-      res.status(400).send({ message: 'Type not recognized' });
-      break;
   }
-};
+});
