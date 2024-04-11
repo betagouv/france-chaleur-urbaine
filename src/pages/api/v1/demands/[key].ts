b8df92ca868282acca43966a5099650cd7f64b73
Maxime Dréau <maxime@dreau.fr>
2024-04-11 16:16:55 +0200

    refactor: validation /api/v1/demands/:key

--- src/pages/api/v1/demands/[key].ts
+++ src/pages/api/v1/demands/[key].ts
@@ -1,39 +1,28 @@
 import { getGestionnairesDemands } from '@core/infrastructure/repository/manager';
+import { handleRouteErrors, requireGetMethod } from '@helpers/server';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { apiUser } from 'src/services/api/authentication';
 import { withCors } from 'src/services/api/cors';
 
-const demands = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'GET') {
-    return res.status(501);
-  }
+const demands = handleRouteErrors(
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    requireGetMethod(req);
 
-  try {
     const account = await apiUser(req, res);
     if (!account) {
       return;
     }
 
     const demands = await getGestionnairesDemands(account.gestionnaires);
-    return res.status(200).json(
-      demands.map((demand) => ({
-        id: demand.id,
-        distance: demand['Distance au réseau'],
-        network: demand['Identifiant réseau'],
-        address: demand['Adresse'],
-        buildingType: demand['Structure'],
-        date: demand['Date demandes'],
-      }))
-    );
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error);
-    res.statusCode = 500;
-    return res.json({
-      message: 'internal server error',
-      code: 'Internal Server Error',
-    });
+    return demands.map((demand) => ({
+      id: demand.id,
+      distance: demand['Distance au réseau'],
+      network: demand['Identifiant réseau'],
+      address: demand['Adresse'],
+      buildingType: demand['Structure'],
+      date: demand['Date demandes'],
+    }));
   }
-};
+);
 
 export default withCors(demands);
