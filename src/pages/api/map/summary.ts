c5330f7137e25e100e9680c51a6da1b2b6cb0b6b
Maxime Dréau <maxime@dreau.fr>
2024-04-11 14:55:42 +0200

    refactor: validation routes GET/POST /api/map/summary

--- src/pages/api/map/summary.ts
+++ src/pages/api/map/summary.ts
@@ -3,7 +3,12 @@ import {
   getLineSummary,
   getPolygonSummary,
 } from '@core/infrastructure/repository/dataSummary';
-import { handleRouteErrors } from '@helpers/server';
+import {
+  handleRouteErrors,
+  invalidRouteError,
+  requireGetMethod,
+  validateObjectSchema,
+} from '@helpers/server';
 import turfArea from '@turf/area';
 import { lineString, polygon } from '@turf/helpers';
 import turfLength from '@turf/length';
@@ -27,28 +32,27 @@ const polygonSummary = async (
       );
   }
   if (req.method === 'GET') {
-    const data = await getPolygonSummary(coordinates);
-    return res.json(data);
+    const summary = await getPolygonSummary(coordinates);
+    res.json(summary);
+    return;
   } else if (req.method === 'POST') {
-    const format = req.query.format as EXPORT_FORMAT;
-    if (!Object.values(EXPORT_FORMAT).includes(format)) {
-      return res.status(400).json({
-        message: `Parameter format is required and must be one of "${Object.values(
-          EXPORT_FORMAT
-        ).join()}"`,
-        code: 'Bad Arguments',
-      });
-    }
+    const { format } = await validateObjectSchema(
+      { format: req.query.format },
+      {
+        format: z.nativeEnum(EXPORT_FORMAT),
+      }
+    );
 
-    const data = await exportPolygonSummary(coordinates, format);
+    const file = await exportPolygonSummary(coordinates, format);
 
     res.setHeader('Content-Type', 'application/zip');
-    res.setHeader('Content-Disposition', `attachment; filename=${data.name}`);
+    res.setHeader('Content-Disposition', `attachment; filename=${file.name}`);
 
-    return res.send(data.content);
+    res.send(file.content);
+    return;
   }
 
-  return res.status(501);
+  throw invalidRouteError;
 };
 
 const lineSummary = async (
@@ -56,64 +60,63 @@ const lineSummary = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
-  if (req.method === 'GET') {
-    const size = coordinates.reduce(
-      (acc, value) => acc + turfLength(lineString(value)),
-      0
-    );
-    const summaries = await Promise.all(
-      coordinates.map((coordinate) => getLineSummary(coordinate))
-    );
-    const data = summaries.reduce(
-      (acc, value) => {
-        return {
-          '10': acc['10']
-            .concat(value['10'])
-            .filter(
-              (value, index, array) =>
-                array.findIndex((v) => v.rownum === value.rownum) === index
-            ),
-          '50': acc['50']
-            .concat(value['50'])
-            .filter(
-              (value, index, array) =>
-                array.findIndex((v) => v.rownum === value.rownum) === index
-            ),
-        };
-      },
-      { '10': [], '50': [] } as { '10': any[]; '50': any[] }
-    );
-    return res.json({ size, data });
-  }
-
-  return res.status(501);
+  requireGetMethod(req);
+  const size = coordinates.reduce(
+    (acc, value) => acc + turfLength(lineString(value)),
+    0
+  );
+  const summaries = await Promise.all(
+    coordinates.map((coordinate) => getLineSummary(coordinate))
+  );
+  const data = summaries.reduce(
+    (acc, value) => {
+      return {
+        '10': acc['10']
+          .concat(value['10'])
+          .filter(
+            (value, index, array) =>
+              array.findIndex((v) => v.rownum === value.rownum) === index
+          ),
+        '50': acc['50']
+          .concat(value['50'])
+          .filter(
+            (value, index, array) =>
+              array.findIndex((v) => v.rownum === value.rownum) === index
+          ),
+      };
+    },
+    { '10': [], '50': [] } as { '10': any[]; '50': any[] }
+  );
+  res.json({ size, data });
 };
 
-const summary = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { type, coordinates } = await z
-    .discriminatedUnion('type', [
-      z.object({
-        type: z.literal('line'),
-        coordinates: z.preprocess(
-          (v) => JSON.parse(decodeURIComponent(v as string)),
-          z.array(z.array(z.array(z.number())))
-        ),
-      }),
-      z.object({
-        type: z.literal('polygon'),
-        coordinates: z.preprocess(
-          (v) => JSON.parse(decodeURIComponent(v as string)),
-          z.array(z.array(z.number()))
-        ),
-      }),
-    ])
-    .parseAsync(req.query);
+const summary = handleRouteErrors(
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    const { type, coordinates } = await z
+      .discriminatedUnion('type', [
+        z.object({
+          type: z.literal('line'),
+          coordinates: z.preprocess(
+            (v) => JSON.parse(decodeURIComponent(v as string)),
+            z.array(z.array(z.array(z.number())))
+          ),
+        }),
+        z.object({
+          type: z.literal('polygon'),
+          coordinates: z.preprocess(
+            (v) => JSON.parse(decodeURIComponent(v as string)),
+            z.array(z.array(z.number()))
+          ),
+        }),
+      ])
+      .parseAsync(req.query);
 
-  if (type === 'polygon') {
-    await polygonSummary(coordinates, req, res);
-  } else if (type === 'line') {
-    await lineSummary(coordinates, req, res);
+    if (type === 'polygon') {
+      await polygonSummary(coordinates, req, res);
+    } else if (type === 'line') {
+      await lineSummary(coordinates, req, res);
+    }
   }
-};
+);
 
-export default withCors(handleRouteErrors(summary));
+export default withCors(summary);
