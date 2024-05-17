27dc85eb11989456ebc67a934155fbb6859eed97
Maxime Dréau <maxime@dreau.fr>
2024-05-17 16:41:40 +0200

    style: fix height

--- src/components/Network/EligibilityTestBox.tsx
+++ src/components/Network/EligibilityTestBox.tsx
@@ -299,7 +299,7 @@ export const ResultsBox = styled(Box)`
       opacity: 0;
     }
     to {
-      max-height: 1000px; // needs to be greater than the height of the box
+      max-height: 1200px; // needs to be greater than the height of the box
       opacity: 1;
     }
   }
