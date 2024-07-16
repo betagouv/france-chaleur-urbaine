0286d2c4f6c5c98c51f6e5a1e77696390bc5374e
Martin Ratinaud <martinratinaud@gmail.com>
2024-07-16 12:48:00 +0400

    chore: enleve tarteaucitron car on utilise le DSFR

--- src/components/Markup/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as FacebookMarkup } from './FacebookMarkup';
-export { default as GoogleAdsMarkup } from './GoogleAdsMarkup';
-export { default as LinkedInMarkup } from './LinkedInMarkup';
