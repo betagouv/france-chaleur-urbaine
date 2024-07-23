f49d95d3d1f46c874f57352d3ece48dee89543d5
Martin Ratinaud <martinratinaud@gmail.com>
2024-07-23 16:32:43 +0400

    chore: ajoute onChange global sur le useForm
    Initialement pour etre capable de set les champs de l'engine publicodes à chaque changement

--- src/components/form/react-hook-form/useForm.tsx
+++ src/components/form/react-hook-form/useForm.tsx
@@ -1,8 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useDebouncedEffect, usePrevious } from '@react-hookz/web';
+import { getChanges } from '@utils/objects';
 import React from 'react';
 import {
   FormProvider,
   useForm as useFormReactHookForm,
+  useWatch,
+  type DeepPartialSkipArrayKey,
   type FieldValues,
   type SubmitHandler,
   type UseFormProps,
@@ -16,14 +20,27 @@ import Toggle from './Toggle';
 
 export { z } from 'zod';
 
+interface UseFormOnChangeData<T extends FieldValues> {
+  values: DeepPartialSkipArrayKey<T>;
+  changed?: ReturnType<typeof getChanges>;
+  isDirty: boolean;
+  isValid: boolean;
+}
+
+type UseFormOnChange<T extends FieldValues> = (
+  callback: UseFormOnChangeData<T>
+) => any;
+
 export const useForm = <
   TFieldValues extends FieldValues = FieldValues,
   TContext = any,
 >({
   schema,
+  onChange,
   ...props
 }: UseFormProps<TFieldValues, TContext> & {
   schema?: Parameters<typeof zodResolver> | Parameters<typeof zodResolver>[0];
+  onChange?: UseFormOnChange<TFieldValues>;
 }) => {
   if (schema && !props.resolver) {
     if (Array.isArray(schema)) {
@@ -34,6 +51,11 @@ export const useForm = <
   }
 
   const methods = useFormReactHookForm(props);
+
+  const submitValues = useWatch({ control: methods.control });
+  const previousSubmitValues = usePrevious(submitValues);
+  const isDirty = methods.formState.isDirty;
+  const isValid = methods.formState.isValid;
 
   const Form: React.FC<
     Omit<React.FormHTMLAttributes<HTMLFormElement>, 'onSubmit'> & {
@@ -56,6 +78,22 @@ export const useForm = <
     },
     [methods]
   );
+
+  // Send onChange callback on every field change
+  useDebouncedEffect(
+    () =>
+      onChange?.({
+        values: submitValues,
+        changed: !previousSubmitValues
+          ? []
+          : getChanges(submitValues, previousSubmitValues),
+        isDirty,
+        isValid,
+      }),
+    [submitValues, isDirty, isValid],
+    500
+  );
+
   return {
     ...methods,
     Form,
