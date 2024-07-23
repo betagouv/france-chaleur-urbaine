881ff08eea2a4c651f614cc524242906e25c1b98
Martin Ratinaud <martinratinaud@gmail.com>
2024-07-23 16:34:05 +0400

    chore: crée formulaire connecté avec publicode

---
+++ src/components/Simulator/usePublicodesEngine.tsx
@@ -0,0 +1,36 @@
+import Engine from 'publicodes';
+import React from 'react';
+
+type EngineConstructorParameters = ConstructorParameters<typeof Engine>;
+type Rules = EngineConstructorParameters[0];
+type Options = EngineConstructorParameters[1];
+
+const usePublicodesEngine = <DottedName,>(rules: Rules, options?: Options) => {
+  const [, rerender] = React.useState({});
+
+  const engine = React.useMemo(() => new Engine(rules, options), []);
+
+  const setField = (key: DottedName, value: any) => {
+    engine.setSituation({
+      ...engine.getSituation(),
+      [key as any]: value === '' ? null : value,
+    });
+    rerender({});
+  };
+
+  const getField = (key: DottedName) => {
+    const result = engine.evaluate(key as any).nodeValue;
+
+    if (result === null || result === undefined) {
+      console.warn(`${key} cannot be evaluated`);
+      return '';
+    }
+    return `${result}`;
+  };
+  return {
+    getField,
+    setField,
+  };
+};
+
+export default usePublicodesEngine;
