89bcd31a148da7a91c63027860bc85e665d69e8f
Martin Ratinaud <martinratinaud@gmail.com>
2024-07-23 16:29:57 +0400

    chore: ajoute util pour comparer 2 objets profondément
    Initiallement pour utiliser dans useForm et etre capable de dire quels champs ont changé

---
+++ src/utils/objects.ts
@@ -0,0 +1,26 @@
+import { diff } from 'deep-object-diff';
+
+const getChangedFields = (obj: any, prefix = '') => {
+  const changedFields: { path: string; value: any }[] = [];
+  for (const key of Object.keys(obj)) {
+    const value = obj[key];
+    const newPrefix = prefix ? `${prefix}.${key}` : key;
+
+    if (typeof value === 'object' && value !== null) {
+      // If the value is an object, recursively get changed fields
+      changedFields.push(...getChangedFields(value, newPrefix));
+    } else {
+      changedFields.push({ path: newPrefix, value });
+    }
+  }
+  return changedFields;
+};
+
+export const getChanges = <T extends Record<string, any>>(
+  newObject: T,
+  previousObject: T
+) => {
+  const differences = diff(previousObject, newObject);
+
+  return getChangedFields(differences);
+};
