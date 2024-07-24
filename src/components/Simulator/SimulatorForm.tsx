4a9964ed1464b9a7ce6d842ebfe875345b166bba
Martin Ratinaud <martinratinaud@gmail.com>
2024-07-24 18:10:03 +0400

    feat: remplace  departement et commune par Autocomplete

--- src/components/Simulator/SimulatorForm.tsx
+++ src/components/Simulator/SimulatorForm.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
+import Autocomplete from '@components/form/Autocomplete';
 import { useForm } from '@components/form/react-hook-form/useForm';
-import { communes } from 'src/services/simulateur/communes';
-import { departements } from 'src/services/simulateur/departements';
+import { useServices } from 'src/services';
 
 import { SimulatorSchemaType, simulatorSchema } from './validation';
 
@@ -11,7 +11,7 @@ type SimulatorFormProps = Omit<React.HTMLAttributes<HTMLFormElement>, 'onSubmit'
 };
 
 const SimulatorForm: React.FC<SimulatorFormProps> = ({ children, className, onSubmit: onExternalSubmit, ...props }) => {
-  const { watch, Form, resetField, Input, Radio, TextArea, Toggle, Select, Checkboxes, Checkbox } = useForm<SimulatorSchemaType>({
+  const { setValue, Form, Input, Radio, TextArea, Toggle, Select, Checkboxes, Checkbox } = useForm<SimulatorSchemaType>({
     schema: simulatorSchema,
     defaultValues: {
       departement: '59',
@@ -21,15 +21,37 @@ const SimulatorForm: React.FC<SimulatorFormProps> = ({ children, className, onSu
       onExternalSubmit(values as any);
     },
   });
+  const [address, setAddress] = React.useState<Awaited<ReturnType<typeof fetchOptions>>[number]>();
+  const { suggestionService } = useServices();
   const onSubmit: React.ComponentProps<typeof Form>['onSubmit'] = (values) => {
     onExternalSubmit(values);
   };
 
-  const departmentId = watch('departement');
+  const fetchOptions = async (query: string) => {
+    const suggestions = await suggestionService.fetchSuggestions(query, { limit: '10' });
+
+    return suggestions.features;
+  };
 
   return (
     <Form className={className} onSubmit={onSubmit} {...props}>
       <Toggle name="mode_pro" label="Mode pro" labelPosition="left" showCheckedHint={false} />
+
+      <Autocomplete<Awaited<ReturnType<typeof fetchOptions>>[number]>
+        fetchFn={fetchOptions}
+        label={"Autocomplete de l'adresse"}
+        onSelect={(address) => {
+          setAddress(address);
+          setValue('commune', address.properties.postcode);
+          setValue('departement', address.properties.postcode.slice(0, 2));
+        }}
+        getOptionValue={(option) => option.properties.label}
+        onClear={() => setAddress(undefined)}
+        nativeInputProps={{
+          placeholder: 'Tapez ici votre adresse',
+        }}
+      />
+      <pre>{JSON.stringify(address, null, 2)}</pre>
 
       <Radio
         rules={{ required: true }}
@@ -49,26 +71,14 @@ const SimulatorForm: React.FC<SimulatorFormProps> = ({ children, className, onSu
       <Input rules={{ required: true }} name="adresse" label="Entrez votre adresse" hintText="Plus que 5 caractères" />
       <TextArea name="description" label="Entrez une description" />
       <Select
-        options={departements.map((d) => ({
-          label: `${d.nom} (${d.codeDepartement})`,
-          value: d.codeDepartement,
-        }))}
-        onChange={() => {
-          resetField('commune');
-        }}
-        name="departement"
-        label="Département"
-        hintText="Essayez avec 64, 55, 59, 31 pour avoir une commune"
-      />
-      <Select
-        options={communes
-          .filter((c) => c.codeDepartement === departmentId)
-          .map((d) => ({
-            label: d.nom,
-            value: d.codeInsee,
-          }))}
-        name="commune"
-        label="Commune"
+        options={[
+          {
+            label: 'DPE 1',
+            value: 'dpe-1',
+          },
+        ]}
+        name="dpe"
+        label="DPE (ou année de construction ou norme thermique)"
       />
 
       <Select
