77e7addfef56527d53857b0238f7e4f5c98ca8b3
Maxime Dréau <maxime@dreau.fr>
2024-07-25 16:10:27 +0200

    fix: corrige (a priori) le tracking google

--- src/components/ConsentBanner/GoogleTagsScript.tsx
+++ src/components/ConsentBanner/GoogleTagsScript.tsx
@@ -7,20 +7,32 @@ type GoogleTagsScriptProps = React.ComponentProps<typeof Script> & {
 const GoogleTagsScript: React.FC<GoogleTagsScriptProps> = ({ tagIds, ...props }) => {
   return (
     <>
+      <Script id={`google-ads-tags`}>
+        {`
+        "use strict";
+        window.dataLayer = window.dataLayer || [];
+        window.gtag = function gtag() {
+          window.dataLayer.push(arguments);
+        };
+        `}
+      </Script>
       {tagIds.map((tagId) => (
         <Script key={tagId} id={`google-ads-tag-${tagId}`} {...props}>
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${tagId}');
+          {`
+          (() => {
+            const script = document.createElement('script');
+            script.src = 'https://www.googletagmanager.com/gtag/js?id=${tagId}';
+            script.async = true;
+            document.head.appendChild(script);
 
-window.dataLayer = window.dataLayer || [];
-window.dataLayer.push({
-  'gtm.start': new Date().getTime(),
-  event: 'gtm.js',
-  'config': { 'anonymize_ip': true }
-});`}
+            script.onload = () => {
+              gtag('js', new Date());
+              gtag('config', '${tagId}', {
+                'anonymize_ip': true
+              });
+            };
+          })()
+          `}
         </Script>
       ))}
     </>
