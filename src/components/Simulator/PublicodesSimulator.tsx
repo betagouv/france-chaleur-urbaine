54ebee0572e964900faf3372ef44587206acda00
Martin Ratinaud <martinratinaud@gmail.com>
2024-08-05 11:02:56 +0400

    Rationalise l'utilisation du modele publicodes pour faciliter typescript

--- src/components/Simulator/PublicodesSimulator.tsx
+++ src/components/Simulator/PublicodesSimulator.tsx
@@ -4,6 +4,7 @@ import Box from '@components/ui/Box';
 
 import PublicodesSimulatorForm from './PublicodesSimulatorForm';
 import PublicodesSimulatorResults from './PublicodesSimulatorResults';
+import useSimulatorEngine from './useSimulatorEngine';
 import { type PublicodeSimulatorSchemaType } from './validation';
 
 type PublicodesSimulatorProps = React.HTMLAttributes<HTMLDivElement> & {
@@ -12,11 +13,12 @@ type PublicodesSimulatorProps = React.HTMLAttributes<HTMLDivElement> & {
 
 const PublicodesSimulator: React.FC<PublicodesSimulatorProps> = ({ children, className, ...props }) => {
   const [results, setResults] = React.useState<PublicodeSimulatorSchemaType>();
+  const engine = useSimulatorEngine();
 
   return (
     <div className={className} {...props}>
       <Box display="flex" gap="4w" justifyContent="space-between">
-        <PublicodesSimulatorForm onSubmit={(data) => setResults(data)} />
+        <PublicodesSimulatorForm onSubmit={(data) => setResults(data)} engine={engine} />
         <PublicodesSimulatorResults results={results} />
       </Box>
     </div>
