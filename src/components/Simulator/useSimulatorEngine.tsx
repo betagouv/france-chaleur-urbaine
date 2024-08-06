a906296045b6fa5d54ec1fbd9ec1dce1623c7598
Martin Ratinaud <martinratinaud@gmail.com>
2024-08-06 13:07:17 +0400

    Ajoute AddressAutocomplete Input

--- src/components/Simulator/useSimulatorEngine.tsx
+++
@@ -1,9 +0,0 @@
-import rules, { DottedName } from '@betagouv/france-chaleur-urbaine-publicodes';
-
-import usePublicodesEngine from '@helpers/publicodes/usePublicodesEngine';
-
-const useSimulatorEngine = () => {
-  return usePublicodesEngine<DottedName>(rules);
-};
-
-export default useSimulatorEngine;
