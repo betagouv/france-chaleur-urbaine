7afe13ee12fff79d823fd015d30e85d566c79ace
Martin Ratinaud <martinratinaud@gmail.com>
2024-08-06 13:07:17 +0400

    Ajoute AddressAutocomplete Input

---
+++ src/components/form/AddressAutocomplete.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { useServices } from 'src/services';
+
+import Autocomplete from './Autocomplete';
+
+export type AddressAutocompleteProps = Omit<React.ComponentProps<typeof Autocomplete>, 'children' | 'getOptionValue' | 'fetchFn'>;
+
+const AddressAutocomplete = ({ nativeInputProps, onSelect, onClear, ...props }: AddressAutocompleteProps) => {
+  // const [address, setAddress] = React.useState<Awaited<ReturnType<typeof fetchOptions>>[number]>();
+  const { suggestionService } = useServices();
+  const fetchOptions = async (query: string) => {
+    const suggestions = await suggestionService.fetchSuggestions(query, { limit: '10' });
+
+    return suggestions.features;
+  };
+
+  return (
+    <Autocomplete
+      fetchFn={fetchOptions}
+      onSelect={(address) => {
+        // setAddress(address);
+        onSelect(address);
+      }}
+      getOptionValue={(option) => option.properties.label}
+      onClear={() => {
+        // setAddress(undefined);
+        onClear?.();
+      }}
+      nativeInputProps={{
+        ...nativeInputProps,
+        placeholder: nativeInputProps?.placeholder || 'Tapez ici votre adresse',
+      }}
+      {...props}
+    />
+  );
+};
+
+export default AddressAutocomplete;
---
+++ src/components/form/dsfr/AddressAutocompleteInput.tsx
@@ -0,0 +1,45 @@
+import { fr } from '@codegouvfr/react-dsfr';
+
+import Autocomplete, { type AddressAutocompleteProps } from '../AddressAutocomplete';
+import FieldWrapper, { type FieldWrapperProps } from '../dsfr/FieldWrapper';
+
+type AddressAutocompleteInputProps = Omit<FieldWrapperProps, 'onSelect' | 'children'> & AddressAutocompleteProps;
+
+const AddressAutocompleteInput = ({
+  fieldId,
+  label,
+  hintText,
+  state,
+  stateRelatedMessage,
+  className,
+  nativeInputProps,
+  ...props
+}: AddressAutocompleteInputProps) => {
+  return (
+    <FieldWrapper
+      fieldId={fieldId}
+      label={label || 'Adresse'}
+      hintText={hintText}
+      state={state}
+      stateRelatedMessage={stateRelatedMessage}
+      className={className}
+    >
+      <Autocomplete
+        nativeInputProps={{
+          className: fr.cx('fr-input', {
+            'fr-input--error': state === 'error',
+            'fr-input--valid': state === 'success',
+          }),
+          style: {
+            // this is because DSFR uses .fr-label + .fr-input, .fr-label + .fr-input-wrap, .fr-label + .fr-select which can't be used here
+            marginTop: label ? '0.5rem' : 'inherit',
+          },
+          ...nativeInputProps,
+        }}
+        {...props}
+      />
+    </FieldWrapper>
+  );
+};
+
+export default AddressAutocompleteInput;
