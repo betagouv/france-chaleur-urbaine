b277ebc6199a12345a597fe9b7436ee8580fd0af
Martin Ratinaud <martinratinaud@gmail.com>
2024-08-08 10:51:50 +0400

    Ajoute accordéon pour être au plus proche du fichier excel

--- src/components/SimulatorPublicodes/GrandPublicForm.tsx
+++ src/components/SimulatorPublicodes/GrandPublicForm.tsx
@@ -1,3 +1,4 @@
+import Accordion from '@codegouvfr/react-dsfr/Accordion';
 import React from 'react';
 
 import AddressAutocomplete from '@components/form/dsfr/AddressAutocompleteInput';
@@ -14,91 +15,101 @@ type GrandPublicFormProps = React.HTMLAttributes<HTMLDivElement> & {
 const GrandPublicForm: React.FC<GrandPublicFormProps> = ({ children, className, engine, ...props }) => {
   return (
     <div {...props}>
-      <AddressAutocomplete
-        label="Adresse"
-        onSelect={(address) => {
-          // TODO engine.setField('commune', address.properties.postcode);
+      <h2>Paramètres techniques</h2>
+      <Accordion label="Généraux">
+        <AddressAutocomplete
+          label="Adresse"
+          onSelect={(address) => {
+            // TODO engine.setField('commune', address.properties.postcode);
 
-          engine.setStringField('code département', address.properties.context.split(', ')[0]);
-        }}
-      />
-      <RadioInput name="type de bâtiment" small orientation="horizontal" />
-      <Input name="degré jours unifié spécifique chaud" label="degré jours unifié spécifique chaud" iconId="fr-icon-temp-cold-fill" />
-      <Input name="degré jours unifié spécifique froid" label="degré jours unifié spécifique froid" iconId="fr-icon-temp-cold-fill" />
-      <Input name="température de référence chaud" label="température de référence chaud" iconId="fr-icon-temp-cold-fill" />
-      <Input name="augmenter la température de chauffe" label="augmenter la température de chauffe" iconId="fr-icon-temp-cold-fill" />
-      <Select name="choix du réseau de chaleur" label="choix du réseau de chaleur" />
-      <Select name="choix du réseau de froid" label="choix du réseau de froid" />
-      <Input
-        name="nombre de logement dans l'immeuble concerné"
-        label="nombre de logement dans l'immeuble concerné"
-        nativeInputProps={{
-          inputMode: 'numeric',
-          maxLength: 5, // a l'air de ne pas fonctionner
-          type: 'number',
-        }}
-      />
-      <Input
-        name="surface logement type tertiaire"
-        label="surface logement type tertiaire"
-        nativeInputProps={{
-          inputMode: 'numeric',
-          maxLength: 6, // a l'air de ne pas fonctionner
-          type: 'number',
-        }}
-      />
-      <Input
-        name="Nombre d'habitants moyen par appartement"
-        label="Nombre d'habitants moyen par appartement"
-        nativeInputProps={{
-          inputMode: 'numeric',
-          maxLength: 2, // a l'air de ne pas fonctionner
-          type: 'number',
-        }}
-      />
-      <RadioInput name="Production eau chaude sanitaire" label="Production eau chaude sanitaire" small orientation="horizontal" />
-      <Select name="type de production ECS" label="type de production ECS" />
-      <Input
-        name="Part de la surface à climatiser"
-        label="Part de la surface à climatiser"
-        nativeInputProps={{
-          inputMode: 'numeric',
-          maxLength: 3,
-          type: 'number',
-          min: 0,
-          max: 100,
-          step: 1,
-        }}
-      />
-      <Select name="Température émetteurs" label="Température émetteurs" />
-      <Select
-        name="méthode de calcul pour les besoins en chauffage et refroidissement"
-        label="méthode de calcul pour les besoins en chauffage et refroidissement"
-        hintText="ne fonctionne pas pour le moment"
-      />
-      {engine.getField('type de bâtiment') === 'résidentiel' && <Select name="méthode résidentiel" label="méthode résidentiel" />}
-      {engine.getField('type de bâtiment') === 'tertiaire' && <Select name="méthode tertiaire" label="méthode tertiaire" />}
-      {engine.getField('méthode de calcul pour les besoins en chauffage et refroidissement') === 'DPE' && <Select name="DPE" label="DPE" />}
-      {engine.getField('méthode de calcul pour les besoins en chauffage et refroidissement') === 'Normes thermiques et âge du bâtiment' && (
-        <Select name="normes thermiques et âge du bâtiment" label="normes thermiques et âge du bâtiment" />
-      )}
+            engine.setStringField('code département', address.properties.context.split(', ')[0]);
+          }}
+        />
+        <Input name="degré jours unifié spécifique chaud" label="degré jours unifié spécifique chaud" iconId="fr-icon-temp-cold-fill" />
+        <Input name="degré jours unifié spécifique froid" label="degré jours unifié spécifique froid" iconId="fr-icon-temp-cold-fill" />
+        <Input name="température de référence chaud" label="température de référence chaud" iconId="fr-icon-temp-cold-fill" />
+        <Input name="augmenter la température de chauffe" label="augmenter la température de chauffe" iconId="fr-icon-temp-cold-fill" />
+      </Accordion>
+      <Accordion label="Réseaux de chaleur et de froid">
+        <Select name="choix du réseau de chaleur" label="choix du réseau de chaleur" />
+        <Select name="choix du réseau de froid" label="choix du réseau de froid" />
+      </Accordion>
+      <Accordion label="Besoins et choix du bâtiment">
+        <Accordion label="Choix du bâtiment">
+          <RadioInput name="type de bâtiment" small orientation="horizontal" />
 
-      {/* <Input
-        name="taille"
-        label="Quelle est votre taille (en cm) ?"
-        nativeInputProps={{
-          placeholder: `${engine.getField('taille') ?? ''}`,
-        }}
-      />
-      <Input
-        name="poids"
-        label="Quel est votre poids (en kg) ?"
-        nativeInputProps={{
-          placeholder: `${engine.getField('poids') ?? ''}`,
-        }}
-      />
-      <div>IMC = {engine.getField('résultat')}</div>
-      <div>Interprêtation = {engine.getField('résultat . interpretation')}</div> */}
+          <Input
+            name="nombre de logement dans l'immeuble concerné"
+            label="nombre de logement dans l'immeuble concerné"
+            nativeInputProps={{
+              inputMode: 'numeric',
+              maxLength: 5, // a l'air de ne pas fonctionner
+              type: 'number',
+            }}
+          />
+          <Input
+            name="surface logement type tertiaire"
+            label="surface logement type tertiaire"
+            nativeInputProps={{
+              inputMode: 'numeric',
+              maxLength: 6, // a l'air de ne pas fonctionner
+              type: 'number',
+            }}
+          />
+          <Input
+            name="Nombre d'habitants moyen par appartement"
+            label="Nombre d'habitants moyen par appartement"
+            nativeInputProps={{
+              inputMode: 'numeric',
+              maxLength: 2, // a l'air de ne pas fonctionner
+              type: 'number',
+            }}
+          />
+          <RadioInput name="Production eau chaude sanitaire" label="Production eau chaude sanitaire" small orientation="horizontal" />
+          <Select name="type de production ECS" label="type de production ECS" />
+          <Input
+            name="Part de la surface à climatiser"
+            label="Part de la surface à climatiser"
+            nativeInputProps={{
+              inputMode: 'numeric',
+              maxLength: 3,
+              type: 'number',
+              min: 0,
+              max: 100,
+              step: 1,
+            }}
+          />
+          <Select name="Température émetteurs" label="Température émetteurs" />
+        </Accordion>
+        <Accordion label="Besoins calculés">
+          <Select
+            name="méthode de calcul pour les besoins en chauffage et refroidissement"
+            label="méthode de calcul pour les besoins en chauffage et refroidissement"
+            hintText="ne fonctionne pas pour le moment"
+          />
+          {engine.getField('type de bâtiment') === 'résidentiel' && (
+            <Select
+              name="méthode résidentiel"
+              label="méthode de calcul pour les besoins en chauffage et refroidissement"
+              hintText="méthode résidentiel"
+            />
+          )}
+          {engine.getField('type de bâtiment') === 'tertiaire' && (
+            <Select
+              name="méthode tertiaire"
+              label="méthode de calcul pour les besoins en chauffage et refroidissement"
+              hintText="méthode tertiaire"
+            />
+          )}
+          {engine.getField('méthode de calcul pour les besoins en chauffage et refroidissement') === 'DPE' && (
+            <Select name="DPE" label="DPE" />
+          )}
+          {engine.getField('méthode de calcul pour les besoins en chauffage et refroidissement') ===
+            'Normes thermiques et âge du bâtiment' && (
+            <Select name="normes thermiques et âge du bâtiment" label="normes thermiques et âge du bâtiment" />
+          )}
+        </Accordion>
+      </Accordion>
     </div>
   );
 };
