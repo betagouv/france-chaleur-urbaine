690847fc971614ae6cedc403576367720ef90086
Martin Ratinaud <martinratinaud@gmail.com>
2024-08-26 15:22:02 +0400

    Fixe le blink au chargement de la page simulateur en prenant les infos de l'url server side

--- src/pages/simulateur.tsx
+++ src/pages/simulateur.tsx
@@ -1,12 +1,19 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import React from 'react';
+
 import SimplePage from '@components/shared/page/SimplePage';
 import PublicodesSimulator from '@components/SimulatorPublicodes';
 
-function SimulateurPage() {
+const SimulateurPage: React.FC<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ query }) => {
   return (
     <SimplePage title="Simulateur : France Chaleur Urbaine">
-      <PublicodesSimulator />
+      <PublicodesSimulator tabId={query.tabId} displayMode={query.displayMode} />
     </SimplePage>
   );
-}
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return { props: { query: context.query } };
+};
 
 export default SimulateurPage;
