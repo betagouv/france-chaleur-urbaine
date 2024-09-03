1dd86f62dded58d2edc346569f58ed2821d408fe
Maxime Dréau <maxime@dreau.fr>
2024-09-03 09:10:25 +0200

    Revert debug code

--- src/pages/simulateur.tsx
+++ src/pages/simulateur.tsx
@@ -7,8 +7,7 @@ import PublicodesSimulator from '@components/SimulatorPublicodes';
 const SimulateurPage: React.FC<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ query }) => {
   return (
     <SimplePage title="Simulateur : France Chaleur Urbaine">
-      {/* don't load all publicodes on the server */}
-      {typeof window !== 'undefined' ? <PublicodesSimulator tabId={query.tabId} displayMode={query.displayMode} /> : <></>}
+      <PublicodesSimulator tabId={query.tabId} displayMode={query.displayMode} />
     </SimplePage>
   );
 };
