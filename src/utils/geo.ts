40eb870f58273532188ce7f65cfe4aaac0a0f946
Maxime Dréau <maxime@dreau.fr>
2024-09-18 13:55:22 +0200

    toLocaleString des distances affichées

--- src/utils/geo.ts
+++ src/utils/geo.ts
@@ -4,8 +4,13 @@ import { round } from '@turf/helpers';
  * Pretty format a distance in meters or kilometers.
  */
 export function formatDistance(distance: number): string {
-  if (distance < 2000) {
-    return `${round(distance)} m`;
+  if (distance < 0) {
+    throw new Error('Distance cannot be negative');
   }
-  return `${round(distance / 1000, 2)} km`;
+
+  if (distance < 2000) {
+    return `${round(distance).toLocaleString()} m`;
+  }
+
+  return `${round(distance / 1000, 2).toLocaleString()} km`;
 }
