19f0813525318110f837149220f4e43b7363f33e
Martin Ratinaud <martinratinaud@gmail.com>
2024-09-18 08:43:39 +0400

    Fixe l'affichage des icones avec svgr

--- next.config.js
+++ next.config.js
@@ -106,6 +106,34 @@ const securityHeadersIFramable = [
   },
 ];
 
+// Function to add SVG loading rules, preventing DSFR icons from not showing
+// See https://github.com/gregberge/svgr/issues/860#issuecomment-1531904399
+const addSvgLoadingToConfig = (config) => {
+  const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+  // Exclude *.svg from the existing file loader rule
+  fileLoaderRule.exclude = /\.svg$/i;
+
+  // Reapply the existing rule for *.svg?url
+  config.module.rules.push({
+    ...fileLoaderRule,
+    test: /\.svg$/i,
+    resourceQuery: /url/, // *.svg?url
+  });
+
+  // Convert all other *.svg imports to React components
+  config.module.rules.push({
+    test: /\.svg$/i,
+    issuer: { not: /\.(css|scss|sass)$/ },
+    resourceQuery: { not: /url/ }, // exclude if *.svg?url
+    loader: '@svgr/webpack',
+    options: {
+      dimensions: false,
+      titleProp: true,
+    },
+  });
+};
+
 module.exports = withBundleAnalyzer(
   withSentryConfig(
     {
@@ -201,10 +229,7 @@ module.exports = withBundleAnalyzer(
           test: /\.woff2$/,
           type: 'asset/resource',
         });
-        config.module.rules.push({
-          test: /\.svg$/,
-          use: ['@svgr/webpack'],
-        });
+        addSvgLoadingToConfig(config);
         return config;
       },
       transpilePackages: ['@codegouvfr/react-dsfr'],
