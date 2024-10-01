090d98bf29ead61f14325a36faa699bbc0cf8785
Clemence <technique@clem-dev.com>
2024-10-01 17:46:58 +0200

    Stats at 18:20

--- src/cron_jobs/cron.ts
+++ src/cron_jobs/cron.ts
@@ -32,7 +32,7 @@ new cron.CronJob({
 });
 
 new cron.CronJob({
-  cronTime: '30 17 1 * *', // le 1er du mois à 08:15 - TEMP to change
+  cronTime: '20 18 1 * *', // le 1er du mois à 08:15 - TEMP to change
   onTick: () => saveStatsInDB(),
   start: true,
   timeZone: 'Europe/Paris',
