d3a7c41bf5c12f0f2f95c89a72ea4975ea94b6b0
Maxime Dréau <maxime@dreau.fr>
2024-10-07 10:45:17 +0200

    Supprime le chargement spécial des svg avec svgr
    Car provoque une requête /undefined au chargement des pages

--- next.config.js
+++ next.config.js
@@ -106,34 +106,6 @@ const securityHeadersIFramable = [
   },
 ];
 
-// Function to add SVG loading rules, preventing DSFR icons from not showing
-// See https://github.com/gregberge/svgr/issues/860#issuecomment-1531904399
-const addSvgLoadingToConfig = (config) => {
-  const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
-
-  // Exclude *.svg from the existing file loader rule
-  fileLoaderRule.exclude = /\.svg$/i;
-
-  // Reapply the existing rule for *.svg?url
-  config.module.rules.push({
-    ...fileLoaderRule,
-    test: /\.svg$/i,
-    resourceQuery: /url/, // *.svg?url
-  });
-
-  // Convert all other *.svg imports to React components
-  config.module.rules.push({
-    test: /\.svg$/i,
-    issuer: { not: /\.(css|scss|sass)$/ },
-    resourceQuery: { not: /url/ }, // exclude if *.svg?url
-    loader: '@svgr/webpack',
-    options: {
-      dimensions: false,
-      titleProp: true,
-    },
-  });
-};
-
 module.exports = withBundleAnalyzer(
   withSentryConfig(
     {
@@ -230,7 +202,7 @@ module.exports = withBundleAnalyzer(
           test: /\.woff2$/,
           type: 'asset/resource',
         });
-        addSvgLoadingToConfig(config);
+        // addSvgLoadingToConfig(config);
         return config;
       },
       transpilePackages: ['@codegouvfr/react-dsfr'],
