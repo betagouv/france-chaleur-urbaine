63cf12a304bc4b0c549f4c4723cff3aa604bd2f6
Martin Ratinaud <martinratinaud@gmail.com>
2024-10-15 10:04:10 +0400

    Empeche le RangeFilter de lancer des re-render à chaque changement d'autre composant

--- src/components/Map/components/RangeFilter.tsx
+++ src/components/Map/components/RangeFilter.tsx
@@ -1,5 +1,5 @@
 import { Range } from '@codegouvfr/react-dsfr/Range';
-import { ReactNode, useCallback, useEffect, useRef } from 'react';
+import { memo, ReactNode, useCallback, useEffect, useRef } from 'react';
 
 import Box from '@components/ui/Box';
 import Tooltip from '@components/ui/Tooltip';
@@ -21,108 +21,121 @@ interface RangeFilterProps {
   formatNumber?: (value: number) => string;
 }
 
-const RangeFilter = ({
-  label,
-  value: values,
-  domain,
-  onChange,
-  unit = '',
-  tooltip,
-  domainTransform,
-  formatNumber = (v) => `${roundNumberProgressively(v)}`,
-  ...props
-}: RangeFilterProps) => {
-  const valueMin = domainTransform ? domainTransform.valueToPercent(values[0]) : values[0];
-  const valueMax = domainTransform ? domainTransform.valueToPercent(values[1]) : values[1];
+const areIntervalsEqual = (a: Interval, b: Interval) => a[0] === b[0] && a[1] === b[1];
 
-  const [min, max] = domainTransform ? [0, 100] : domain;
-  const ref = useRef<HTMLDivElement>(null);
+const RangeFilter = memo(
+  ({
+    label,
+    value: values,
+    domain,
+    onChange,
+    unit = '',
+    tooltip,
+    domainTransform,
+    formatNumber = (v) => `${roundNumberProgressively(v)}`,
+    ...props
+  }: RangeFilterProps) => {
+    const valueMin = domainTransform ? domainTransform.valueToPercent(values[0]) : values[0];
+    const valueMax = domainTransform ? domainTransform.valueToPercent(values[1]) : values[1];
 
-  useEffect(() => {
-    if (domainTransform) {
-      updateRangeOutput(domain[0], domain[1]);
-    }
-  }, [domain, domainTransform]);
+    const [min, max] = domainTransform ? [0, 100] : domain;
+    const ref = useRef<HTMLDivElement>(null);
 
-  const updateRangeOutput = useCallback(
-    (min: number, max: number) => {
-      const updateRangeText = () => {
-        const textToUpdate = ref.current?.querySelector('.fr-range__output');
-        if (textToUpdate && textToUpdate.textContent !== '') {
-          textToUpdate.textContent = `${formatNumber(min)}${unit} - ${formatNumber(max)}${unit}`;
-          return;
-        }
-        // Retry after 50ms if the element is not found
-        setTimeout(updateRangeText, 50);
-      };
+    useEffect(() => {
+      if (domainTransform) {
+        updateRangeOutput(domain[0], domain[1]);
+      }
+    }, [domain, domainTransform]);
 
-      updateRangeText();
-    },
-    [formatNumber, unit]
-  );
-  const handleChangeMin = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      const value = +e.target.value;
-      const newValueMin = domainTransform ? domainTransform.percentToValue(value) : value;
-      onChange([newValueMin, values[1]]);
-      updateRangeOutput(newValueMin, values[1]);
-    },
-    [domainTransform, onChange, updateRangeOutput, values]
-  );
+    const updateRangeOutput = useCallback(
+      (min: number, max: number) => {
+        const updateRangeText = () => {
+          const textToUpdate = ref.current?.querySelector('.fr-range__output');
+          if (textToUpdate && textToUpdate.textContent !== '') {
+            textToUpdate.textContent = `${formatNumber(min)}${unit} - ${formatNumber(max)}${unit}`;
+            return;
+          }
+          // Retry after 50ms if the element is not found
+          setTimeout(updateRangeText, 50);
+        };
 
-  const handleChangeMax = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      const value = +e.target.value;
-      const newValueMax = domainTransform ? domainTransform.percentToValue(value) : value;
-      onChange([values[0], newValueMax]);
-      updateRangeOutput(values[0], newValueMax);
-    },
-    [domainTransform, onChange, updateRangeOutput, values]
-  );
+        updateRangeText();
+      },
+      [formatNumber, unit]
+    );
+    const handleChangeMin = useCallback(
+      (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = +e.target.value;
+        const newValueMin = domainTransform ? domainTransform.percentToValue(value) : value;
+        onChange([newValueMin, values[1]]);
+        updateRangeOutput(newValueMin, values[1]);
+      },
+      [domainTransform, onChange, updateRangeOutput, values]
+    );
 
-  return (
-    <>
-      <Range
-        label={
-          <Box display="flex" alignItems="center" justifyContent="space-between">
-            {label}
-            {tooltip && <Tooltip title={tooltip} />}
+    const handleChangeMax = useCallback(
+      (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = +e.target.value;
+        const newValueMax = domainTransform ? domainTransform.percentToValue(value) : value;
+        onChange([values[0], newValueMax]);
+        updateRangeOutput(values[0], newValueMax);
+      },
+      [domainTransform, onChange, updateRangeOutput, values]
+    );
+
+    return (
+      <>
+        <Range
+          label={
+            <Box display="flex" alignItems="center" justifyContent="space-between">
+              {label}
+              {tooltip && <Tooltip title={tooltip} />}
+            </Box>
+          }
+          ref={ref}
+          small
+          double
+          max={max}
+          min={min}
+          hideMinMax={!!domainTransform}
+          nativeInputProps={[
+            {
+              value: valueMin,
+              onChange: handleChangeMin,
+            },
+            {
+              value: valueMax,
+              onChange: handleChangeMax,
+            },
+          ]}
+          suffix={unit}
+          {...props}
+        />
+
+        {!!domainTransform && (
+          <Box display="flex" justifyContent="space-between">
+            <div className="fr-range__min">
+              {formatNumber(domain[0])}
+              {unit}
+            </div>
+            <div className="fr-range__max">
+              {formatNumber(domain[1])}
+              {unit}
+            </div>
           </Box>
-        }
-        ref={ref}
-        small
-        double
-        max={max}
-        min={min}
-        hideMinMax={!!domainTransform}
-        nativeInputProps={[
-          {
-            value: valueMin,
-            onChange: handleChangeMin,
-          },
-          {
-            value: valueMax,
-            onChange: handleChangeMax,
-          },
-        ]}
-        suffix={unit}
-        {...props}
-      />
+        )}
+      </>
+    );
+  },
+  (prevProps, nextProps) =>
+    areIntervalsEqual(prevProps.value, nextProps.value) &&
+    areIntervalsEqual(prevProps.domain, nextProps.domain) &&
+    prevProps.tooltip === nextProps.tooltip &&
+    prevProps.unit === nextProps.unit &&
+    prevProps.label === nextProps.label
+);
 
-      {!!domainTransform && (
-        <Box display="flex" justifyContent="space-between">
-          <div className="fr-range__min">
-            {formatNumber(domain[0])}
-            {unit}
-          </div>
-          <div className="fr-range__max">
-            {formatNumber(domain[1])}
-            {unit}
-          </div>
-        </Box>
-      )}
-    </>
-  );
-};
+// Set display name for debugging purposes
+RangeFilter.displayName = 'RangeFilter';
 
 export default RangeFilter;
