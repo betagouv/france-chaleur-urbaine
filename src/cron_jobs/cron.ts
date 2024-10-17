056b378d3ad658b9048518d7582717688b76bbd4
Clemence <technique@clem-dev.com>
2024-10-17 17:15:42 +0200

    Revert other cron comments + stats cron each first day of the month at 8:15

--- src/cron_jobs/cron.ts
+++ src/cron_jobs/cron.ts
@@ -1,38 +1,38 @@
 import cron from 'cron';
 
-//import { launchJob } from './launch';
+import { launchJob } from './launch';
 import { saveStatsInDB } from './saveStatsInDB';
 
-// new cron.CronJob({
-//   cronTime: '00 10 * * 1-5', // du lundi au vendredi à 10:00
-//   onTick: () => launchJob('dailyNewManagerMail'),
-//   start: true,
-//   timeZone: 'Europe/Paris',
-// });
-
-// new cron.CronJob({
-//   cronTime: '55 9 * * 2', // le mardi à 09:55
-//   onTick: () => launchJob('weeklyOldManagerMail'),
-//   start: true,
-//   timeZone: 'Europe/Paris',
-// });
-
-// new cron.CronJob({
-//   cronTime: '05 10 * * 1', // le lundi à 10:05
-//   onTick: () => launchJob('dailyRelanceMail'),
-//   start: true,
-//   timeZone: 'Europe/Paris',
-// });
-
-// new cron.CronJob({
-//   cronTime: '00 * * * *', // toutes les heures
-//   onTick: () => launchJob('updateUsers'),
-//   start: true,
-//   timeZone: 'Europe/Paris',
-// });
+new cron.CronJob({
+  cronTime: '00 10 * * 1-5', // du lundi au vendredi à 10:00
+  onTick: () => launchJob('dailyNewManagerMail'),
+  start: true,
+  timeZone: 'Europe/Paris',
+});
 
 new cron.CronJob({
-  cronTime: '10 17 17 * *', // le 1er du mois à 08:15 - TEMP to change
+  cronTime: '55 9 * * 2', // le mardi à 09:55
+  onTick: () => launchJob('weeklyOldManagerMail'),
+  start: true,
+  timeZone: 'Europe/Paris',
+});
+
+new cron.CronJob({
+  cronTime: '05 10 * * 1', // le lundi à 10:05
+  onTick: () => launchJob('dailyRelanceMail'),
+  start: true,
+  timeZone: 'Europe/Paris',
+});
+
+new cron.CronJob({
+  cronTime: '00 * * * *', // toutes les heures
+  onTick: () => launchJob('updateUsers'),
+  start: true,
+  timeZone: 'Europe/Paris',
+});
+
+new cron.CronJob({
+  cronTime: '15 08 1 * *', // le 1er du mois à 08:15 - TEMP to change
   onTick: () => saveStatsInDB(),
   start: true,
   timeZone: 'Europe/Paris',
