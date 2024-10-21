2538e860459cadb66651f94c95db689517191d4e
Clemence <technique@clem-dev.com>
2024-10-21 15:44:45 +0200

    Reverse test cron

--- src/cron_jobs/cron.ts
+++ src/cron_jobs/cron.ts
@@ -1,9 +1,9 @@
 import cron from 'cron';
 
-//import { launchJob } from './launch';
+import { launchJob } from './launch';
 import { saveStatsInDB } from './saveStatsInDB';
 
-/*new cron.CronJob({
+new cron.CronJob({
   cronTime: '00 10 * * 1-5', // du lundi au vendredi à 10:00
   onTick: () => launchJob('dailyNewManagerMail'),
   start: true,
@@ -29,10 +29,10 @@ new cron.CronJob({
   onTick: () => launchJob('updateUsers'),
   start: true,
   timeZone: 'Europe/Paris',
-});*/
+});
 
 new cron.CronJob({
-  cronTime: '15 15 21 * *', // le 1er du mois à 08:15 - TEMP to change
+  cronTime: '15 08 1 * *', // le 1er du mois à 08:15 - TEMP to change
   onTick: () => saveStatsInDB(),
   start: true,
   timeZone: 'Europe/Paris',
