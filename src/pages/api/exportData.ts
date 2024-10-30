894fcc49bd5ca39f23f5ed94d0c7d2d2359a8fe3
Martin Ratinaud <martinratinaud@gmail.com>
2024-10-30 16:33:28 +0400

    Renomme variable

--- src/pages/api/exportData.ts
+++ src/pages/api/exportData.ts
@@ -39,10 +39,10 @@ export default handleRouteErrors(async (req: NextApiRequest, res: NextApiRespons
       throw new Error();
   }
 
-  const csv = getSpreadSheet(exportsParams[exportType].columns, data, EXPORT_FORMAT.XLSX);
+  const xlsx = getSpreadSheet(exportsParams[exportType].columns, data, EXPORT_FORMAT.XLSX);
 
   res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
   res.setHeader('Content-Disposition', `attachment; filename=${exportsParams[exportType].filename}.xlsx`);
 
-  return res.status(200).send(csv);
+  return res.status(200).send(xlsx);
 });
