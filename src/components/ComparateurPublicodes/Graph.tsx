be06ac35f67094afae7d2a8d580956d7e2a7d994
Martin Ratinaud <martinratinaud@gmail.com>
2024-11-01 11:56:47 +0400

    Changement des infobulles du graphe

--- src/components/ComparateurPublicodes/Graph.tsx
+++ src/components/ComparateurPublicodes/Graph.tsx
@@ -70,7 +70,7 @@ const getBarStyle = (color: string, { bordered }: { bordered?: boolean } = {}) =
   `color: ${color}; stroke-color: ${color}; stroke-opacity: 1; stroke-width: 1;${bordered ? 'fill-opacity: 0.1;' : ''}`;
 
 interface TooltipProps {
-  title: string;
+  title: React.ReactNode;
   color: string;
   amount: number;
   bordered?: boolean;
@@ -81,7 +81,7 @@ const getTooltip = ({ title, amount, color, bordered, valueFormatter }: TooltipP
   ReactDOMServer.renderToString(
     <GraphTooltip>
       <span style={bordered ? { border: `2px solid ${color}` } : { backgroundColor: color }}></span>
-      <span>{title}</span>
+      <div style={{ maxWidth: '300px', lineHeight: '1.25rem', fontSize: '0.875rem', margin: '2px 0' }}>{title}</div>
       <strong style={{ whiteSpace: 'nowrap' }}>{valueFormatter(amount)}</strong>
     </GraphTooltip>
   );
@@ -95,9 +95,9 @@ const getRow = ({ title, amount, color, bordered, valueFormatter }: TooltipProps
 ];
 
 const emissionsCO2GraphColumnNames = [
-  "Scope 1 : Production directe d'énergie",
-  "Scope 2 : Production indirecte d'énergie",
-  'Scope 3 : Émissions indirectes',
+  'Émissions directes',
+  "Émissions indirectes - production d'énergie",
+  'Émissions indirectes - matériel',
 ];
 const emissionsCO2GraphColumns = emissionsCO2GraphColumnNames.map(getColumn).flat();
 
@@ -262,19 +262,20 @@ const Graph: React.FC<GraphProps> = ({ proMode, engine, className, ...props }) =
     ...modesDeChauffage.flatMap((typeInstallation) => {
       const amounts = [
         ...getRow({
-          title: "Scope 1 : Production directe d'énergie",
+          title:
+            "Émissions liées aux combustibles utilisés pour la production d'énergie, et réalisées directement sur le lieu de la consommation (scope 1)",
           amount: engine.getFieldAsNumber(`env . Installation x ${typeInstallation.emissionsCO2PublicodesKey} . Scope 1`) * nbAppartements,
           color: colorScope1,
           valueFormatter: formatEmissionsCO2,
         }),
         ...getRow({
-          title: "Scope 2 : Production indirecte d'énergie",
+          title: "Émissions liées à l'utilisation d'énergie non produite sur le site de consommation (scope 2)",
           amount: engine.getFieldAsNumber(`env . Installation x ${typeInstallation.emissionsCO2PublicodesKey} . Scope 2`) * nbAppartements,
           color: colorScope2,
           valueFormatter: formatEmissionsCO2,
         }),
         ...getRow({
-          title: 'Scope 3 : Émissions indirectes',
+          title: "Émissions liées à la fabrication des équipements, et non directement à la production d'énergie (scope 3)",
           amount: engine.getFieldAsNumber(`env . Installation x ${typeInstallation.emissionsCO2PublicodesKey} . Scope 3`) * nbAppartements,
           color: colorScope3,
           valueFormatter: formatEmissionsCO2,
