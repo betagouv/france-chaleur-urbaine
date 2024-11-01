3cdf37829f1b20776d472c137c3980e67ae1938b
Martin Ratinaud <martinratinaud@gmail.com>
2024-11-01 11:52:03 +0400

    Change le texte de la description dans la modale

--- src/components/ComparateurPublicodes/Placeholder.tsx
+++ src/components/ComparateurPublicodes/Placeholder.tsx
@@ -127,10 +127,11 @@ export const DescriptionModal = () => {
       </p>
 
       <p>
-        Cet outil prend la suite d'RCE33, outil développé par l'association AMORCE. Cette nouvelle version est financée par le programme
-        européen Heat&Cool LIFE, piloté par la Région Sud, qui vise à développer des outils dans l'objectif de développer les réseaux de
-        chaleur et de froid vertueux. L'association AMORCE pilote le projet, le bureau d’études Elcimaï y a apporté son expertise technique
-        et France Chaleur Urbaine a mis en place la version disponible en ligne.
+        Cet outil prend la suite d'RCE33, outil développé par l'association AMORCE. L'association AMORCE pilote le projet, le bureau
+        d’études Elcimaï y a apporté son expertise technique et France Chaleur Urbaine a mis en place la version disponible en ligne. Cet
+        outil bénéficie d'un financement du programme européen Heat&Cool LIFE, piloté par la Région Sud, qui vise à développer des outils
+        dans l'objectif de développer les réseaux de chaleur et de froid vertueux. La mise en place de l'interface en ligne est financée sur
+        le budget de France Chaleur Urbaine.
       </p>
       <Logos />
     </Modal>
