baa695a1d38c5b4f21acfefac5092c389b844783
Martin Ratinaud <martinratinaud@gmail.com>
2024-11-04 18:19:35 +0400

    Ajoute la possibilité de chercher seulement des villes pour l'adresse

--- src/components/form/AddressAutocomplete.tsx
+++ src/components/form/AddressAutocomplete.tsx
@@ -8,16 +8,36 @@ import Autocomplete from './Autocomplete';
 export type AddressAutocompleteProps = Omit<
   React.ComponentProps<typeof Autocomplete>,
   'children' | 'getOptionValue' | 'fetchFn' | 'onSelect'
-> & { onSelect: (option: SuggestionItem) => void; excludeCities?: boolean };
+> & {
+  onSelect: (option: SuggestionItem) => void;
+  onlyCities?: boolean;
+  excludeCities?: boolean;
+  limit?: number | string;
+};
 
-const AddressAutocomplete = ({ nativeInputProps, onSelect, excludeCities, onClear, ...props }: AddressAutocompleteProps) => {
+const AddressAutocomplete = ({
+  nativeInputProps,
+  onSelect,
+  onClear,
+  onlyCities,
+  excludeCities,
+  limit = 10,
+  ...props
+}: AddressAutocompleteProps) => {
   // const [address, setAddress] = React.useState<Awaited<ReturnType<typeof fetchOptions>>[number]>();
+
   const { suggestionService } = useServices();
   const fetchOptions = async (query: string) => {
-    const suggestions = await suggestionService.fetchSuggestions(query, { limit: '10' });
+    const params: Parameters<typeof suggestionService.fetchSuggestions>[1] = { limit: limit.toString() };
+    if (onlyCities) {
+      params.type = 'municipality';
+    }
+    const suggestions = await suggestionService.fetchSuggestions(query, params);
+
     const features = excludeCities
       ? suggestions.features.filter((feature) => feature.properties.type !== 'municipality')
       : suggestions.features;
+
     return features;
   };
 
@@ -29,7 +49,7 @@ const AddressAutocomplete = ({ nativeInputProps, onSelect, excludeCities, onClea
         // setAddress(address);
         onSelect(address);
       }}
-      getOptionValue={(option) => option.properties.label}
+      getOptionValue={(option) => (onlyCities ? `${option.properties.city}, ${option.properties.postcode}` : option.properties.label)}
       onClear={() => {
         // setAddress(undefined);
         onClear?.();
