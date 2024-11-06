ab5d54ce56bdeea6f49cbd091ae20ec54f2bb765
Martin Ratinaud <martinratinaud@gmail.com>
2024-11-06 12:31:31 +0400

    Bouge le code dans un fragment pour ajouter d'autres composants après

--- src/components/ComparateurPublicodes/ComparateurPublicodes.tsx
+++ src/components/ComparateurPublicodes/ComparateurPublicodes.tsx
@@ -110,220 +110,222 @@ const ComparateurPublicodes: React.FC<ComparateurPublicodesProps> = ({
     isAddressSelected && !!modesDeChauffage ? <Graph engine={engine} proMode={displayMode === 'technicien'} /> : <ResultsNotAvailable />;
 
   return (
-    <div className={cx(fr.cx('fr-container'), className)} {...props}>
-      <FormProvider engine={engine}>
-        <Section>
-          <header>
-            <ComparateurPublicodesTitle />
-            <ToggleSwitch
-              label="Mode&nbsp;avancé"
-              labelPosition="left"
-              inputTitle="Mode Pro"
-              showCheckedHint={false}
-              checked={displayMode === 'technicien'}
-              className={fr.cx('fr-mt-0')}
-              onChange={(checked) => {
-                const newValue = checked ? 'technicien' : 'grand public';
-                setDisplayMode(newValue);
-                engine.setStringField('mode affichage', newValue);
-              }}
-            />
-          </header>
+    <>
+      <div className={cx(fr.cx('fr-container'), className)} {...props}>
+        <FormProvider engine={engine}>
+          <Section>
+            <header>
+              <ComparateurPublicodesTitle />
+              <ToggleSwitch
+                label="Mode&nbsp;avancé"
+                labelPosition="left"
+                inputTitle="Mode Pro"
+                showCheckedHint={false}
+                checked={displayMode === 'technicien'}
+                className={fr.cx('fr-mt-0')}
+                onChange={(checked) => {
+                  const newValue = checked ? 'technicien' : 'grand public';
+                  setDisplayMode(newValue);
+                  engine.setStringField('mode affichage', newValue);
+                }}
+              />
+            </header>
 
-          <Simulator $loading={loading}>
-            <Box display="flex" gap="16px" flexDirection="column">
-              <Accordion
-                expanded={selectedTabId === simulatorTabs[0].tabId}
-                onExpandedChange={(expanded) => (expanded ? setSelectedTabId(simulatorTabs[0].tabId) : setSelectedTabId(null))}
-                bordered
-                label={
-                  <div>
-                    {simulatorTabs[0].label}
-                    {address && selectedTabId !== simulatorTabs[0].tabId && (
-                      <div className={fr.cx('fr-text--xs', 'fr-text--light')}>{address}</div>
-                    )}
-                  </div>
-                }
-              >
-                <AddressAutocomplete
+            <Simulator $loading={loading}>
+              <Box display="flex" gap="16px" flexDirection="column">
+                <Accordion
+                  expanded={selectedTabId === simulatorTabs[0].tabId}
+                  onExpandedChange={(expanded) => (expanded ? setSelectedTabId(simulatorTabs[0].tabId) : setSelectedTabId(null))}
+                  bordered
                   label={
-                    <Label
-                      label="Adresse"
-                      help="Pour le moment, l’adresse est utilisée uniquement pour évaluer la proximité aux réseaux de chaleur et froid et la zone climatique, et non pour récupérer les caractéristiques du bâtiment"
-                    ></Label>
+                    <div>
+                      {simulatorTabs[0].label}
+                      {address && selectedTabId !== simulatorTabs[0].tabId && (
+                        <div className={fr.cx('fr-text--xs', 'fr-text--light')}>{address}</div>
+                      )}
+                    </div>
                   }
-                  state={addressError ? 'error' : undefined}
-                  stateRelatedMessage={
-                    addressError ? 'Désolé, nous n’avons pas trouvé la ville associée à cette adresse, essayez avec une autre' : undefined
-                  }
-                  defaultValue={address || ''}
-                  onClear={() => {
-                    setNearestReseauDeChaleur(undefined);
-                    setNearestReseauDeFroid(undefined);
-                    setAddressError(false);
-                    setAddress(null);
-                    setLngLat(undefined);
-
-                    engine.setSituation(
-                      ObjectEntries(addresseToPublicodesRules).reduce(
-                        (acc, [key]) => ({
-                          ...acc,
-                          [key]: null,
-                        }),
-                        {}
-                      )
-                    );
-                  }}
-                  onSelect={async (selectedAddress) => {
-                    setAddressError(false);
-                    setLngLat(undefined);
-
-                    const [lon, lat] = selectedAddress.geometry.coordinates;
-                    const addressLabel = selectedAddress.properties.label;
-                    if (addressLabel !== address) {
+                >
+                  <AddressAutocomplete
+                    label={
+                      <Label
+                        label="Adresse"
+                        help="Pour le moment, l’adresse est utilisée uniquement pour évaluer la proximité aux réseaux de chaleur et froid et la zone climatique, et non pour récupérer les caractéristiques du bâtiment"
+                      ></Label>
+                    }
+                    state={addressError ? 'error' : undefined}
+                    stateRelatedMessage={
+                      addressError ? 'Désolé, nous n’avons pas trouvé la ville associée à cette adresse, essayez avec une autre' : undefined
+                    }
+                    defaultValue={address || ''}
+                    onClear={() => {
+                      setNearestReseauDeChaleur(undefined);
+                      setNearestReseauDeFroid(undefined);
+                      setAddressError(false);
                       setAddress(null);
-                    }
+                      setLngLat(undefined);
 
-                    const infos: LocationInfoResponse = await postFetchJSON('/api/location-infos', {
-                      lon,
-                      lat,
-                      city: selectedAddress.properties.city,
-                      cityCode: selectedAddress.properties.citycode,
-                    });
-                    setNearestReseauDeChaleur(infos.nearestReseauDeChaleur);
-                    setNearestReseauDeFroid(infos.nearestReseauDeFroid);
+                      engine.setSituation(
+                        ObjectEntries(addresseToPublicodesRules).reduce(
+                          (acc, [key]) => ({
+                            ...acc,
+                            [key]: null,
+                          }),
+                          {}
+                        )
+                      );
+                    }}
+                    onSelect={async (selectedAddress) => {
+                      setAddressError(false);
+                      setLngLat(undefined);
 
-                    if (!infos.infosVille) {
-                      setAddressError(true);
+                      const [lon, lat] = selectedAddress.geometry.coordinates;
+                      const addressLabel = selectedAddress.properties.label;
+                      if (addressLabel !== address) {
+                        setAddress(null);
+                      }
 
-                      return;
-                    }
+                      const infos: LocationInfoResponse = await postFetchJSON('/api/location-infos', {
+                        lon,
+                        lat,
+                        city: selectedAddress.properties.city,
+                        cityCode: selectedAddress.properties.citycode,
+                      });
+                      setNearestReseauDeChaleur(infos.nearestReseauDeChaleur);
+                      setNearestReseauDeFroid(infos.nearestReseauDeFroid);
 
-                    setAddress(addressLabel);
+                      if (!infos.infosVille) {
+                        setAddressError(true);
 
-                    if (infos.nearestReseauDeChaleur || infos.nearestReseauDeFroid) {
-                      setLngLat(selectedAddress.geometry.coordinates);
-                    }
+                        return;
+                      }
 
-                    console.debug('locations-infos', infos);
+                      setAddress(addressLabel);
 
-                    engine.setSituation(
-                      ObjectEntries(addresseToPublicodesRules).reduce(
-                        (acc, [key, infoGetter]) => ({
-                          ...acc,
-                          [key]: infoGetter(infos) ?? null,
-                        }),
-                        {}
-                      )
-                    );
-                  }}
-                />
-                {displayMode === 'grand public' ? (
-                  <ParametresDuBatimentGrandPublic engine={engine} />
-                ) : (
-                  <ParametresDuBatimentTechnicien engine={engine} />
-                )}
-                <Button onClick={() => setSelectedTabId(simulatorTabs[1].tabId)} full disabled={!isAddressSelected} className="fr-mt-2w">
-                  Continuer
-                </Button>
-              </Accordion>
-              <Accordion
-                expanded={selectedTabId === simulatorTabs[1].tabId}
-                onExpandedChange={(expanded) => (expanded ? setSelectedTabId(simulatorTabs[1].tabId) : setSelectedTabId(null))}
-                disabled={!isAddressSelected}
-                bordered
-                label={simulatorTabs[1].label}
-              >
-                <ModesDeChauffageAComparer
-                  engine={engine}
-                  nearestReseauDeChaleur={nearestReseauDeChaleur}
-                  nearestReseauDeFroid={nearestReseauDeFroid}
-                />
-                {displayMode === 'technicien' && (
-                  <Button onClick={() => setSelectedTabId(simulatorTabs[2].tabId)} full disabled={!modesDeChauffage} className="fr-mt-2w">
+                      if (infos.nearestReseauDeChaleur || infos.nearestReseauDeFroid) {
+                        setLngLat(selectedAddress.geometry.coordinates);
+                      }
+
+                      console.debug('locations-infos', infos);
+
+                      engine.setSituation(
+                        ObjectEntries(addresseToPublicodesRules).reduce(
+                          (acc, [key, infoGetter]) => ({
+                            ...acc,
+                            [key]: infoGetter(infos) ?? null,
+                          }),
+                          {}
+                        )
+                      );
+                    }}
+                  />
+                  {displayMode === 'grand public' ? (
+                    <ParametresDuBatimentGrandPublic engine={engine} />
+                  ) : (
+                    <ParametresDuBatimentTechnicien engine={engine} />
+                  )}
+                  <Button onClick={() => setSelectedTabId(simulatorTabs[1].tabId)} full disabled={!isAddressSelected} className="fr-mt-2w">
                     Continuer
                   </Button>
-                )}
-              </Accordion>
-              {displayMode === 'technicien' && (
+                </Accordion>
                 <Accordion
-                  expanded={selectedTabId === simulatorTabs[2].tabId}
-                  onExpandedChange={(expanded) => (expanded ? setSelectedTabId(simulatorTabs[2].tabId) : setSelectedTabId(null))}
+                  expanded={selectedTabId === simulatorTabs[1].tabId}
+                  onExpandedChange={(expanded) => (expanded ? setSelectedTabId(simulatorTabs[1].tabId) : setSelectedTabId(null))}
                   disabled={!isAddressSelected}
                   bordered
-                  label={simulatorTabs[2].label}
+                  label={simulatorTabs[1].label}
                 >
-                  <ParametresDesModesDeChauffage engine={engine} />
+                  <ModesDeChauffageAComparer
+                    engine={engine}
+                    nearestReseauDeChaleur={nearestReseauDeChaleur}
+                    nearestReseauDeFroid={nearestReseauDeFroid}
+                  />
+                  {displayMode === 'technicien' && (
+                    <Button onClick={() => setSelectedTabId(simulatorTabs[2].tabId)} full disabled={!modesDeChauffage} className="fr-mt-2w">
+                      Continuer
+                    </Button>
+                  )}
                 </Accordion>
-              )}
-            </Box>
-            <Results>
-              {nearestReseauDeChaleur && (
-                <Alert
-                  className="fr-mb-2w"
-                  description={
-                    <>
-                      Le réseau de chaleur{' '}
-                      <Link
-                        href={`/reseaux/${nearestReseauDeChaleur['Identifiant reseau']}?address=${encodeURIComponent(address as string)}`}
-                        isExternal
-                      >
-                        <strong>{nearestReseauDeChaleur.nom_reseau}</strong>
-                      </Link>{' '}
-                      est à <strong>{nearestReseauDeChaleur.distance}m</strong> de votre adresse.
-                      {lngLat && (
-                        <div className="fr-text--xs">
-                          <Link isExternal href={`/carte?coord=${lngLat.join(',')}&zoom=17`} className="fr-block">
-                            <strong>Visualiser sur la carte</strong>
-                          </Link>
-                        </div>
-                      )}
-                    </>
-                  }
-                  severity="info"
-                  small
-                />
-              )}
-              {nearestReseauDeFroid && (
-                <Alert
-                  className="fr-mb-2w"
-                  description={
-                    <>
-                      Le réseau de froid{' '}
-                      <Link
-                        href={`/reseaux/${nearestReseauDeFroid['Identifiant reseau']}?address=${encodeURIComponent(address as string)}`}
-                        isExternal
-                      >
-                        <strong>{nearestReseauDeFroid.nom_reseau}</strong>
-                      </Link>{' '}
-                      est à <strong>{nearestReseauDeFroid.distance}m</strong> de votre adresse.
-                      {lngLat && (
-                        <div className="fr-text--xs">
-                          <Link isExternal href={`/carte?coord=${lngLat.join(',')}&zoom=17`} className="fr-block">
-                            <strong>Visualiser sur la carte</strong>
-                          </Link>
-                        </div>
-                      )}
-                    </>
-                  }
-                  severity="info"
-                  small
-                />
-              )}
-              {results}
-            </Results>
-            <FloatingButton onClick={() => setGraphDrawerOpen(true)} iconId="ri-arrow-up-fill">
-              Voir les résultats
-            </FloatingButton>
-            <Drawer open={graphDrawerOpen} onClose={() => setGraphDrawerOpen(false)} anchor="right">
-              <Button onClick={() => setGraphDrawerOpen(false)}>Fermer</Button>
-              <div style={{ maxWidth: '100vw', overflow: 'auto' }}>{results}</div>
-            </Drawer>
-            <DebugDrawer engine={engine} />
-          </Simulator>
-        </Section>
-      </FormProvider>
-    </div>
+                {displayMode === 'technicien' && (
+                  <Accordion
+                    expanded={selectedTabId === simulatorTabs[2].tabId}
+                    onExpandedChange={(expanded) => (expanded ? setSelectedTabId(simulatorTabs[2].tabId) : setSelectedTabId(null))}
+                    disabled={!isAddressSelected}
+                    bordered
+                    label={simulatorTabs[2].label}
+                  >
+                    <ParametresDesModesDeChauffage engine={engine} />
+                  </Accordion>
+                )}
+              </Box>
+              <Results>
+                {nearestReseauDeChaleur && (
+                  <Alert
+                    className="fr-mb-2w"
+                    description={
+                      <>
+                        Le réseau de chaleur{' '}
+                        <Link
+                          href={`/reseaux/${nearestReseauDeChaleur['Identifiant reseau']}?address=${encodeURIComponent(address as string)}`}
+                          isExternal
+                        >
+                          <strong>{nearestReseauDeChaleur.nom_reseau}</strong>
+                        </Link>{' '}
+                        est à <strong>{nearestReseauDeChaleur.distance}m</strong> de votre adresse.
+                        {lngLat && (
+                          <div className="fr-text--xs">
+                            <Link isExternal href={`/carte?coord=${lngLat.join(',')}&zoom=17`} className="fr-block">
+                              <strong>Visualiser sur la carte</strong>
+                            </Link>
+                          </div>
+                        )}
+                      </>
+                    }
+                    severity="info"
+                    small
+                  />
+                )}
+                {nearestReseauDeFroid && (
+                  <Alert
+                    className="fr-mb-2w"
+                    description={
+                      <>
+                        Le réseau de froid{' '}
+                        <Link
+                          href={`/reseaux/${nearestReseauDeFroid['Identifiant reseau']}?address=${encodeURIComponent(address as string)}`}
+                          isExternal
+                        >
+                          <strong>{nearestReseauDeFroid.nom_reseau}</strong>
+                        </Link>{' '}
+                        est à <strong>{nearestReseauDeFroid.distance}m</strong> de votre adresse.
+                        {lngLat && (
+                          <div className="fr-text--xs">
+                            <Link isExternal href={`/carte?coord=${lngLat.join(',')}&zoom=17`} className="fr-block">
+                              <strong>Visualiser sur la carte</strong>
+                            </Link>
+                          </div>
+                        )}
+                      </>
+                    }
+                    severity="info"
+                    small
+                  />
+                )}
+                {results}
+              </Results>
+              <FloatingButton onClick={() => setGraphDrawerOpen(true)} iconId="ri-arrow-up-fill">
+                Voir les résultats
+              </FloatingButton>
+              <Drawer open={graphDrawerOpen} onClose={() => setGraphDrawerOpen(false)} anchor="right">
+                <Button onClick={() => setGraphDrawerOpen(false)}>Fermer</Button>
+                <div style={{ maxWidth: '100vw', overflow: 'auto' }}>{results}</div>
+              </Drawer>
+              <DebugDrawer engine={engine} />
+            </Simulator>
+          </Section>
+        </FormProvider>
+      </div>
+    </>
   );
 };
 
