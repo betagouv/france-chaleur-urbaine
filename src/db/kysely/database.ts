7ae1f71428562a1606fb7297f914ed665470d33d
Martin Ratinaud <martinratinaud@gmail.com>
2024-11-14 15:02:05 +0400

    Supprime les tables non nécessaires dans kysely

--- src/db/kysely/database.ts
+++ src/db/kysely/database.ts
@@ -2809,26 +2809,6 @@ export interface EnrrMobilisablesTiles {
   z: Int8;
 }
 
-export interface GeographyColumns {
-  coord_dimension: number | null;
-  f_geography_column: string | null;
-  f_table_catalog: string | null;
-  f_table_name: string | null;
-  f_table_schema: string | null;
-  srid: number | null;
-  type: string | null;
-}
-
-export interface GeometryColumns {
-  coord_dimension: number | null;
-  f_geometry_column: string | null;
-  f_table_catalog: string | null;
-  f_table_name: string | null;
-  f_table_schema: string | null;
-  srid: number | null;
-  type: string | null;
-}
-
 export interface IgnCommunes {
   geom: string | null;
   id: string | null;
@@ -2972,6 +2952,8 @@ export interface ReseauxDeChaleur {
   communes: string[] | null;
   'contenu CO2': number | null;
   'contenu CO2 ACV': number | null;
+  contenu_CO2_2023_tmp: number | null;
+  contenu_CO2_ACV_2023_tmp: number | null;
   CP_MO: string | null;
   departement: number | null;
   'Dev_reseau%': number | null;
@@ -3017,6 +2999,24 @@ export interface ReseauxDeChaleur {
   prod_MWh_solaire_thermique: number | null;
   prod_MWh_UIOM: number | null;
   production_totale_MWh: number | null;
+  puissance_MW_autre_chaleur_recuperee: number | null;
+  puissance_MW_autres: number | null;
+  puissance_MW_autres_ENR: number | null;
+  puissance_MW_biogaz: number | null;
+  puissance_MW_biomasse_solide: number | null;
+  puissance_MW_chaleur_industiel: number | null;
+  puissance_MW_charbon: number | null;
+  puissance_MW_chaudieres_electriques: number | null;
+  puissance_MW_dechets_internes: number | null;
+  puissance_MW_fioul_domestique: number | null;
+  puissance_MW_fioul_lourd: number | null;
+  puissance_MW_gaz_naturel: number | null;
+  puissance_MW_geothermie: number | null;
+  puissance_MW_GPL: number | null;
+  puissance_MW_PAC: number | null;
+  puissance_MW_solaire_thermique: number | null;
+  puissance_MW_UIOM: number | null;
+  puissance_totale_MW: number | null;
   'PV%': number | null;
   region: string | null;
   'Rend%': number | null;
@@ -3048,6 +3048,8 @@ export interface ReseauxDeFroid {
   communes: string[] | null;
   'contenu CO2': number | null;
   'contenu CO2 ACV': number | null;
+  contenu_CO2_2023_tmp: number | null;
+  contenu_CO2_ACV_2023_tmp: number | null;
   CP_MO: string | null;
   departement: number | null;
   fichiers: Json | null;
@@ -3068,6 +3070,7 @@ export interface ReseauxDeFroid {
   nb_pdl: number | null;
   nom_reseau: string | null;
   production_totale_MWh: number | null;
+  puissance_totale_MW: number | null;
   region: string | null;
   'Rend%': number | null;
   'reseaux classes': boolean | null;
@@ -3081,578 +3084,6 @@ export interface ReseauxDeFroidTiles {
   x: Int8;
   y: Int8;
   z: Int8;
-}
-
-export interface SpatialRefSys {
-  auth_name: string | null;
-  auth_srid: number | null;
-  proj4text: string | null;
-  srid: number;
-  srtext: string | null;
-}
-
-export interface TigerAddr {
-  arid: string | null;
-  fromarmid: number | null;
-  fromhn: string | null;
-  fromtyp: string | null;
-  gid: Generated<number>;
-  mtfcc: string | null;
-  plus4: string | null;
-  side: string | null;
-  statefp: string | null;
-  tlid: Int8 | null;
-  toarmid: number | null;
-  tohn: string | null;
-  totyp: string | null;
-  zip: string | null;
-}
-
-export interface TigerAddrfeat {
-  aridl: string | null;
-  aridr: string | null;
-  edge_mtfcc: string | null;
-  fullname: string | null;
-  gid: Generated<number>;
-  lfromhn: string | null;
-  lfromtyp: string | null;
-  linearid: string | null;
-  ltohn: string | null;
-  ltotyp: string | null;
-  offsetl: string | null;
-  offsetr: string | null;
-  parityl: string | null;
-  parityr: string | null;
-  plus4l: string | null;
-  plus4r: string | null;
-  rfromhn: string | null;
-  rfromtyp: string | null;
-  rtohn: string | null;
-  rtotyp: string | null;
-  statefp: string;
-  the_geom: string | null;
-  tlid: Int8 | null;
-  zipl: string | null;
-  zipr: string | null;
-}
-
-export interface TigerBg {
-  aland: number | null;
-  awater: number | null;
-  bg_id: string;
-  blkgrpce: string | null;
-  countyfp: string | null;
-  funcstat: string | null;
-  gid: Generated<number>;
-  intptlat: string | null;
-  intptlon: string | null;
-  mtfcc: string | null;
-  namelsad: string | null;
-  statefp: string | null;
-  the_geom: string | null;
-  tractce: string | null;
-}
-
-export interface TigerCounty {
-  aland: Int8 | null;
-  awater: number | null;
-  cbsafp: string | null;
-  classfp: string | null;
-  cntyidfp: string;
-  countyfp: string | null;
-  countyns: string | null;
-  csafp: string | null;
-  funcstat: string | null;
-  gid: Generated<number>;
-  intptlat: string | null;
-  intptlon: string | null;
-  lsad: string | null;
-  metdivfp: string | null;
-  mtfcc: string | null;
-  name: string | null;
-  namelsad: string | null;
-  statefp: string | null;
-  the_geom: string | null;
-}
-
-export interface TigerCountyLookup {
-  co_code: number;
-  name: string | null;
-  st_code: number;
-  state: string | null;
-}
-
-export interface TigerCountysubLookup {
-  co_code: number;
-  county: string | null;
-  cs_code: number;
-  name: string | null;
-  st_code: number;
-  state: string | null;
-}
-
-export interface TigerCousub {
-  aland: Numeric | null;
-  awater: Numeric | null;
-  classfp: string | null;
-  cnectafp: string | null;
-  cosbidfp: string;
-  countyfp: string | null;
-  cousubfp: string | null;
-  cousubns: string | null;
-  funcstat: string | null;
-  gid: Generated<number>;
-  intptlat: string | null;
-  intptlon: string | null;
-  lsad: string | null;
-  mtfcc: string | null;
-  name: string | null;
-  namelsad: string | null;
-  nctadvfp: string | null;
-  nectafp: string | null;
-  statefp: string | null;
-  the_geom: string | null;
-}
-
-export interface TigerDirectionLookup {
-  abbrev: string | null;
-  name: string;
-}
-
-export interface TigerEdges {
-  artpath: string | null;
-  countyfp: string | null;
-  deckedroad: string | null;
-  divroad: string | null;
-  exttyp: string | null;
-  featcat: string | null;
-  fullname: string | null;
-  gcseflg: string | null;
-  gid: Generated<number>;
-  hydroflg: string | null;
-  lfromadd: string | null;
-  ltoadd: string | null;
-  mtfcc: string | null;
-  offsetl: string | null;
-  offsetr: string | null;
-  olfflg: string | null;
-  passflg: string | null;
-  persist: string | null;
-  railflg: string | null;
-  rfromadd: string | null;
-  roadflg: string | null;
-  rtoadd: string | null;
-  smid: string | null;
-  statefp: string | null;
-  tfidl: Numeric | null;
-  tfidr: Numeric | null;
-  the_geom: string | null;
-  tlid: Int8 | null;
-  tnidf: Numeric | null;
-  tnidt: Numeric | null;
-  ttyp: string | null;
-  zipl: string | null;
-  zipr: string | null;
-}
-
-export interface TigerFaces {
-  aiannhce: string | null;
-  aiannhce00: string | null;
-  aiannhfp: string | null;
-  aiannhfp00: string | null;
-  anrcfp: string | null;
-  anrcfp00: string | null;
-  atotal: number | null;
-  blkgrpce: string | null;
-  blkgrpce00: string | null;
-  blkgrpce20: string | null;
-  blockce: string | null;
-  blockce00: string | null;
-  blockce20: string | null;
-  cbsafp: string | null;
-  cd108fp: string | null;
-  cd111fp: string | null;
-  cnectafp: string | null;
-  comptyp: string | null;
-  comptyp00: string | null;
-  conctyfp: string | null;
-  conctyfp00: string | null;
-  countyfp: string | null;
-  countyfp00: string | null;
-  countyfp20: string | null;
-  cousubfp: string | null;
-  cousubfp00: string | null;
-  csafp: string | null;
-  elsdlea: string | null;
-  elsdlea00: string | null;
-  gid: Generated<number>;
-  intptlat: string | null;
-  intptlon: string | null;
-  lwflag: string | null;
-  metdivfp: string | null;
-  nctadvfp: string | null;
-  nectafp: string | null;
-  offset: string | null;
-  placefp: string | null;
-  placefp00: string | null;
-  puma5ce: string | null;
-  puma5ce00: string | null;
-  scsdlea: string | null;
-  scsdlea00: string | null;
-  sldlst: string | null;
-  sldlst00: string | null;
-  sldust: string | null;
-  sldust00: string | null;
-  statefp: string | null;
-  statefp00: string | null;
-  statefp20: string | null;
-  submcdfp: string | null;
-  submcdfp00: string | null;
-  tazce: string | null;
-  tazce00: string | null;
-  tblkgpce: string | null;
-  tfid: Numeric | null;
-  the_geom: string | null;
-  tractce: string | null;
-  tractce00: string | null;
-  tractce20: string | null;
-  trsubce: string | null;
-  trsubce00: string | null;
-  trsubfp: string | null;
-  trsubfp00: string | null;
-  ttractce: string | null;
-  uace: string | null;
-  uace00: string | null;
-  ugace: string | null;
-  ugace00: string | null;
-  unsdlea: string | null;
-  unsdlea00: string | null;
-  vtdst: string | null;
-  vtdst00: string | null;
-  zcta5ce: string | null;
-  zcta5ce00: string | null;
-}
-
-export interface TigerFeatnames {
-  fullname: string | null;
-  gid: Generated<number>;
-  linearid: string | null;
-  mtfcc: string | null;
-  name: string | null;
-  paflag: string | null;
-  predir: string | null;
-  predirabrv: string | null;
-  prequal: string | null;
-  prequalabr: string | null;
-  pretyp: string | null;
-  pretypabrv: string | null;
-  statefp: string | null;
-  sufdir: string | null;
-  sufdirabrv: string | null;
-  sufqual: string | null;
-  sufqualabr: string | null;
-  suftyp: string | null;
-  suftypabrv: string | null;
-  tlid: Int8 | null;
-}
-
-export interface TigerGeocodeSettings {
-  category: string | null;
-  name: string;
-  setting: string | null;
-  short_desc: string | null;
-  unit: string | null;
-}
-
-export interface TigerGeocodeSettingsDefault {
-  category: string | null;
-  name: string;
-  setting: string | null;
-  short_desc: string | null;
-  unit: string | null;
-}
-
-export interface TigerLoaderLookuptables {
-  /**
-   * List of columns to exclude as an array. This is excluded from both input table and output table and rest of columns remaining are assumed to be in same order in both tables. gid, geoid,cpi,suffix1ce are excluded if no columns are specified.
-   */
-  columns_exclude: string[] | null;
-  insert_mode: Generated<string>;
-  level_county: Generated<boolean>;
-  /**
-   * These are tables that contain all data for the whole US so there is just a single file
-   */
-  level_nation: Generated<boolean>;
-  level_state: Generated<boolean>;
-  /**
-   * Whether or not to load the table.  For states and zcta5 (you may just want to download states10, zcta510 nationwide file manually) load your own into a single table that inherits from tiger.states, tiger.zcta5.  You'll get improved performance for some geocoding cases.
-   */
-  load: Generated<boolean>;
-  /**
-   * This is the table name to inherit from and suffix of resulting output table -- how the table will be named --  edges here would mean -- ma_edges , pa_edges etc. except in the case of national tables. national level tables have no prefix
-   */
-  lookup_name: string;
-  post_load_process: string | null;
-  pre_load_process: string | null;
-  process_order: Generated<number>;
-  single_geom_mode: Generated<boolean | null>;
-  single_mode: Generated<boolean>;
-  /**
-   * suffix of the tables to load e.g.  edges would load all tables like *edges.dbf(shp)  -- so tl_2010_42129_edges.dbf .
-   */
-  table_name: string | null;
-  /**
-   * Path to use for wget instead of that specified in year table.  Needed currently for zcta where they release that only for 2000 and 2010
-   */
-  website_root_override: string | null;
-}
-
-export interface TigerLoaderPlatform {
-  county_process_command: string | null;
-  declare_sect: string | null;
-  environ_set_command: string | null;
-  loader: string | null;
-  os: string;
-  path_sep: string | null;
-  pgbin: string | null;
-  psql: string | null;
-  unzip_command: string | null;
-  wget: string | null;
-}
-
-export interface TigerLoaderVariables {
-  data_schema: string | null;
-  staging_fold: string | null;
-  staging_schema: string | null;
-  tiger_year: string;
-  website_root: string | null;
-}
-
-export interface TigerPagcGaz {
-  id: Generated<number>;
-  is_custom: Generated<boolean>;
-  seq: number | null;
-  stdword: string | null;
-  token: number | null;
-  word: string | null;
-}
-
-export interface TigerPagcLex {
-  id: Generated<number>;
-  is_custom: Generated<boolean>;
-  seq: number | null;
-  stdword: string | null;
-  token: number | null;
-  word: string | null;
-}
-
-export interface TigerPagcRules {
-  id: Generated<number>;
-  is_custom: Generated<boolean | null>;
-  rule: string | null;
-}
-
-export interface TigerPlace {
-  aland: Int8 | null;
-  awater: Int8 | null;
-  classfp: string | null;
-  cpi: string | null;
-  funcstat: string | null;
-  gid: Generated<number>;
-  intptlat: string | null;
-  intptlon: string | null;
-  lsad: string | null;
-  mtfcc: string | null;
-  name: string | null;
-  namelsad: string | null;
-  pcicbsa: string | null;
-  pcinecta: string | null;
-  placefp: string | null;
-  placens: string | null;
-  plcidfp: string;
-  statefp: string | null;
-  the_geom: string | null;
-}
-
-export interface TigerPlaceLookup {
-  name: string | null;
-  pl_code: number;
-  st_code: number;
-  state: string | null;
-}
-
-export interface TigerSecondaryUnitLookup {
-  abbrev: string | null;
-  name: string;
-}
-
-export interface TigerState {
-  aland: Int8 | null;
-  awater: Int8 | null;
-  division: string | null;
-  funcstat: string | null;
-  gid: Generated<number>;
-  intptlat: string | null;
-  intptlon: string | null;
-  lsad: string | null;
-  mtfcc: string | null;
-  name: string | null;
-  region: string | null;
-  statefp: string;
-  statens: string | null;
-  stusps: string;
-  the_geom: string | null;
-}
-
-export interface TigerStateLookup {
-  abbrev: string | null;
-  name: string | null;
-  st_code: number;
-  statefp: string | null;
-}
-
-export interface TigerStreetTypeLookup {
-  abbrev: string | null;
-  is_hw: Generated<boolean>;
-  name: string;
-}
-
-export interface TigerTabblock {
-  aland: number | null;
-  awater: number | null;
-  blockce: string | null;
-  countyfp: string | null;
-  funcstat: string | null;
-  gid: Generated<number>;
-  intptlat: string | null;
-  intptlon: string | null;
-  mtfcc: string | null;
-  name: string | null;
-  statefp: string | null;
-  tabblock_id: string;
-  the_geom: string | null;
-  tractce: string | null;
-  uace: string | null;
-  ur: string | null;
-}
-
-export interface TigerTabblock20 {
-  aland: number | null;
-  awater: number | null;
-  blockce: string | null;
-  countyfp: string | null;
-  funcstat: string | null;
-  geoid: string;
-  housing: number | null;
-  intptlat: string | null;
-  intptlon: string | null;
-  mtfcc: string | null;
-  name: string | null;
-  pop: number | null;
-  statefp: string | null;
-  the_geom: string | null;
-  tractce: string | null;
-  uace: string | null;
-  uatype: string | null;
-  ur: string | null;
-}
-
-export interface TigerTract {
-  aland: number | null;
-  awater: number | null;
-  countyfp: string | null;
-  funcstat: string | null;
-  gid: Generated<number>;
-  intptlat: string | null;
-  intptlon: string | null;
-  mtfcc: string | null;
-  name: string | null;
-  namelsad: string | null;
-  statefp: string | null;
-  the_geom: string | null;
-  tract_id: string;
-  tractce: string | null;
-}
-
-export interface TigerZcta5 {
-  aland: number | null;
-  awater: number | null;
-  classfp: string | null;
-  funcstat: string | null;
-  gid: Generated<number>;
-  intptlat: string | null;
-  intptlon: string | null;
-  mtfcc: string | null;
-  partflg: string | null;
-  statefp: string;
-  the_geom: string | null;
-  zcta5ce: string;
-}
-
-export interface TigerZipLookup {
-  cnt: number | null;
-  co_code: number | null;
-  county: string | null;
-  cousub: string | null;
-  cs_code: number | null;
-  pl_code: number | null;
-  place: string | null;
-  st_code: number | null;
-  state: string | null;
-  zip: number;
-}
-
-export interface TigerZipLookupAll {
-  cnt: number | null;
-  co_code: number | null;
-  county: string | null;
-  cousub: string | null;
-  cs_code: number | null;
-  pl_code: number | null;
-  place: string | null;
-  st_code: number | null;
-  state: string | null;
-  zip: number | null;
-}
-
-export interface TigerZipLookupBase {
-  city: string | null;
-  county: string | null;
-  state: string | null;
-  statefp: string | null;
-  zip: string;
-}
-
-export interface TigerZipState {
-  statefp: string | null;
-  stusps: string;
-  zip: string;
-}
-
-export interface TigerZipStateLoc {
-  place: string;
-  statefp: string | null;
-  stusps: string;
-  zip: string;
-}
-
-export interface TopologyLayer {
-  child_id: number | null;
-  feature_column: string;
-  feature_type: number;
-  layer_id: number;
-  level: Generated<number>;
-  schema_name: string;
-  table_name: string;
-  topology_id: number;
-}
-
-export interface TopologyTopology {
-  hasz: Generated<boolean>;
-  id: Generated<number>;
-  name: string;
-  precision: number;
-  srid: number;
 }
 
 export interface Users {
@@ -3801,8 +3232,6 @@ export interface DB {
   enrr_mobilisables_friches_tiles: EnrrMobilisablesFrichesTiles;
   enrr_mobilisables_parkings_tiles: EnrrMobilisablesParkingsTiles;
   enrr_mobilisables_tiles: EnrrMobilisablesTiles;
-  geography_columns: GeographyColumns;
-  geometry_columns: GeometryColumns;
   ign_communes: IgnCommunes;
   knex_migrations: KnexMigrations;
   knex_migrations_lock: KnexMigrationsLock;
@@ -3816,43 +3245,6 @@ export interface DB {
   reseaux_de_chaleur_tiles_pr_filtres: ReseauxDeChaleurTilesPrFiltres;
   reseaux_de_froid: ReseauxDeFroid;
   reseaux_de_froid_tiles: ReseauxDeFroidTiles;
-  spatial_ref_sys: SpatialRefSys;
-  'tiger.addr': TigerAddr;
-  'tiger.addrfeat': TigerAddrfeat;
-  'tiger.bg': TigerBg;
-  'tiger.county': TigerCounty;
-  'tiger.county_lookup': TigerCountyLookup;
-  'tiger.countysub_lookup': TigerCountysubLookup;
-  'tiger.cousub': TigerCousub;
-  'tiger.direction_lookup': TigerDirectionLookup;
-  'tiger.edges': TigerEdges;
-  'tiger.faces': TigerFaces;
-  'tiger.featnames': TigerFeatnames;
-  'tiger.geocode_settings': TigerGeocodeSettings;
-  'tiger.geocode_settings_default': TigerGeocodeSettingsDefault;
-  'tiger.loader_lookuptables': TigerLoaderLookuptables;
-  'tiger.loader_platform': TigerLoaderPlatform;
-  'tiger.loader_variables': TigerLoaderVariables;
-  'tiger.pagc_gaz': TigerPagcGaz;
-  'tiger.pagc_lex': TigerPagcLex;
-  'tiger.pagc_rules': TigerPagcRules;
-  'tiger.place': TigerPlace;
-  'tiger.place_lookup': TigerPlaceLookup;
-  'tiger.secondary_unit_lookup': TigerSecondaryUnitLookup;
-  'tiger.state': TigerState;
-  'tiger.state_lookup': TigerStateLookup;
-  'tiger.street_type_lookup': TigerStreetTypeLookup;
-  'tiger.tabblock': TigerTabblock;
-  'tiger.tabblock20': TigerTabblock20;
-  'tiger.tract': TigerTract;
-  'tiger.zcta5': TigerZcta5;
-  'tiger.zip_lookup': TigerZipLookup;
-  'tiger.zip_lookup_all': TigerZipLookupAll;
-  'tiger.zip_lookup_base': TigerZipLookupBase;
-  'tiger.zip_state': TigerZipState;
-  'tiger.zip_state_loc': TigerZipStateLoc;
-  'topology.layer': TopologyLayer;
-  'topology.topology': TopologyTopology;
   users: Users;
   zone_a_potentiel_chaud: ZoneAPotentielChaud;
   zone_a_potentiel_chaud_tiles: ZoneAPotentielChaudTiles;
