416af36aba8990156b677062dd36ff1b6d3a1e89
Martin Ratinaud <martinratinaud@gmail.com>
2024-11-18 14:12:42 +0400

    Utilise une modal dans le StickyForm au lieu de inline

--- src/components/StickyForm/StickyForm.tsx
+++ src/components/StickyForm/StickyForm.tsx
@@ -1,11 +1,16 @@
-import { EligibilityFormAddress, EligibilityFormContact, EligibilityFormMessageConfirmation } from '@/components/EligibilityForm';
-import { FormWarningMessage, Loader, LoaderWrapper, SliceContactFormStyle } from '@/components/HeadSliceForm/HeadSliceForm.style';
-import Slice from '@/components/Slice';
-import Box from '@/components/ui/Box';
-import Icon from '@/components/ui/Icon';
-import useContactFormFCU from '@/hooks/useContactFormFCU';
+import { EligibilityFormAddress, EligibilityFormContact, EligibilityFormMessageConfirmation } from '@components/EligibilityForm';
+import { FormWarningMessage, SliceContactFormStyle } from '@components/HeadSliceForm/HeadSliceForm.style';
+import Slice from '@components/Slice';
+import Loader from '@components/ui/Loader';
+import Modal, { createModal } from '@components/ui/Modal';
+import { useContactFormFCU } from '@hooks';
 
-import { Close, Container, Form, Title } from './StickyForm.styles';
+import { Container, Form, Title } from './StickyForm.styles';
+
+const eligibilityTestModal = createModal({
+  id: 'eligibility-test-modal',
+  isOpenedByDefault: false,
+});
 
 const StickyForm = ({ title }: { title?: string }) => {
   const {
@@ -29,29 +34,26 @@ const StickyForm = ({ title }: { title?: string }) => {
       <EligibilityFormAddress onChange={handleOnChangeAddress} onFetch={handleOnFetchAddress} onSuccess={handleOnSuccessAddress} />
       {showWarning && <FormWarningMessage show>{warningMessage}</FormWarningMessage>}
 
-      {!showWarning && loadingStatus === 'loading' && (
-        <Box className="fr-col-12">
-          <LoaderWrapper show>
-            <Loader color="balck" />
-          </LoaderWrapper>
-        </Box>
-      )}
-
       <SliceContactFormStyle />
-      <Form ref={EligibilityFormContactRef}>
-        {(contactReady || messageReceived) && (
-          <Close onClick={handleResetFormContact}>
-            <Icon name="ri-close-line" size="lg" />
-          </Close>
-        )}
-        <Slice theme="grey" className={`slice-contact-form-wrapper ${contactReady && !messageReceived ? 'active' : ''}`}>
-          <EligibilityFormContact addressData={addressData} onSubmit={handleOnSubmitContact} />
-        </Slice>
+      {loadingStatus === 'loading' && <Loader size="md" />}
+      <Modal
+        modal={eligibilityTestModal}
+        title="Tester une adresse"
+        open={contactReady}
+        size="custom"
+        onClose={handleResetFormContact}
+        loading={loadingStatus === 'loading'}
+      >
+        <Form ref={EligibilityFormContactRef}>
+          <Slice theme="grey" className={`slice-contact-form-wrapper ${contactReady && !messageReceived ? 'active' : ''}`}>
+            <EligibilityFormContact addressData={addressData} onSubmit={handleOnSubmitContact} />
+          </Slice>
 
-        <Slice padding={5} theme="grey" className={`slice-contact-form-wrapper ${messageReceived ? 'active' : ''}`}>
-          <EligibilityFormMessageConfirmation addressData={addressData} />
-        </Slice>
-      </Form>
+          <Slice padding={5} theme="grey" className={`slice-contact-form-wrapper ${messageReceived ? 'active' : ''}`}>
+            <EligibilityFormMessageConfirmation addressData={addressData} />
+          </Slice>
+        </Form>
+      </Modal>
     </Container>
   );
 };
