4402964b491580d547eeec6bb8f60f86bf4ec98b
Martin Ratinaud <martinratinaud@gmail.com>
2024-11-22 16:11:46 +0400

    Affine SEO pour la page decret tertiaire + fixe l'affichage des couleurs

--- src/components/Tertiaire/index.tsx
+++ src/components/Tertiaire/index.tsx
@@ -7,6 +7,7 @@ import Simulator from '@components/SimulatorCO2';
 import { TypeSurf } from '@components/SimulatorCO2/SimulatorCO2.businessRule';
 import Slice from '@components/Slice';
 import SliceForm from '@components/SliceForm';
+import { legacyColors } from '@components/ui/helpers/colors';
 import WrappedBlock from '@components/WrappedBlock';
 import WrappedText from '@components/WrappedText';
 import { comparatifRcu, fcuSolutionForFutur } from '@data/tertiaire';
@@ -70,7 +71,7 @@ Vos locaux sont chauffés au fioul ou au gaz&nbsp;?
           <MarkdownWrapper
             value={`
 :::puce-icon{icon="./icons/picto-warning.svg"}
-**À partir du 1er juillet 2022,** de nouvelles normes environnementales, qui visent à limiter les émissions de gaz à effet de serre, entreront en vigueur et **excluent l'installation de nouvelles chaudières au fioul.**  
+**À partir du 1er juillet 2022,** de nouvelles normes environnementales, qui visent à limiter les émissions de gaz à effet de serre, entreront en vigueur et **excluent l'installation de nouvelles chaudières au fioul.**
 **[Des aides](/ressources/aides#contenu) accompagnent cette transition.**
 :::
               `}
@@ -88,23 +89,23 @@ Vos locaux sont chauffés au fioul ou au gaz&nbsp;?
 
       <Slice theme="grey" padding={7} header={`## Découvrez les dispositifs d’aides`} direction="row" className="aides-rcu">
         <MarkdownWrapper
-          value={`##### Vous souhaitez raccorder vos locaux au chauffage urbain&nbsp;?  
+          value={`##### Vous souhaitez raccorder vos locaux au chauffage urbain&nbsp;?
 
-Le dispositif **[«&nbsp;Coup de pouce chauffage des bâtiments résidentiels collectifs et tertiaires&nbsp;»](/ressources/aides#contenu)** a pour objectif d’inciter financièrement les propriétaires ou gestionnaires de bâtiments tertiaires à remplacer leurs équipements de chauffage au charbon, au fioul ou au gaz au profit d’un raccordement à un réseau de chaleur.  
+Le dispositif **[«&nbsp;Coup de pouce chauffage des bâtiments résidentiels collectifs et tertiaires&nbsp;»](/ressources/aides#contenu)** a pour objectif d’inciter financièrement les propriétaires ou gestionnaires de bâtiments tertiaires à remplacer leurs équipements de chauffage au charbon, au fioul ou au gaz au profit d’un raccordement à un réseau de chaleur.
               `}
           className="aides-rcu-body"
         />
         <MarkdownWrapper
-          value={`##### Un accompagnement technique et financier peut aussi être sollicité&nbsp;:  
+          value={`##### Un accompagnement technique et financier peut aussi être sollicité&nbsp;:
 
-- Auprès de **[France Renov](https://france-renov.gouv.fr/fr/pro/quel-accompagnement-pour-mes-travaux)** pour le petit tertiaire privé (<1000 m²)  
+- Auprès de **[France Renov](https://france-renov.gouv.fr/fr/pro/quel-accompagnement-pour-mes-travaux)** pour le petit tertiaire privé (<1000 m²)
 
-- Dans le cadre du **[programme ACTEE](https://www.programme-cee-actee.fr/)** pour les bâtiments publics des collectivités  
+- Dans le cadre du **[programme ACTEE](https://www.programme-cee-actee.fr/)** pour les bâtiments publics des collectivités
               `}
           className="aides-rcu-body"
         />
       </Slice>
-      <Slice theme="color">
+      <Slice bgColor={legacyColors.lightblue}>
         <Understanding cards={tertiaireCards} />
       </Slice>
       <Slice theme="color-light" padding={8}>
