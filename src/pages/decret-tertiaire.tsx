f58926a896a0fcbdf06b11441f551c42d01b9f97
Martin Ratinaud <martinratinaud@gmail.com>
2024-11-27 11:46:57 +0400

    Suppression d'un commentaire en trop

--- src/pages/decret-tertiaire.tsx
+++ src/pages/decret-tertiaire.tsx
@@ -5,7 +5,6 @@ export default function DecretTertiairePage() {
   return (
     <SimplePage
       title="Décret tertiaire et chauffage urbain: atteignez vos objectifs de performance énergétique"
-      // description="Jusqu’à 23 % de réduction de consommations d’énergie comptabilisée ! En application de l’arrêté du 13 avril 2022 relatif aux obligations d’actions de réduction des consommations d’énergie finale dans des bâtiments à usage tertiaire, un coefficient de 0,77 est appliqué aux calculs des consommations d’énergie des bâtiments raccordés aux réseaux de chaleur."
       description="Jusqu’à 23 % de réduction de consommations d’énergie comptabilisée ! En application de l’arrêté du 13 avril 2022 relatif aux obligations d’actions de réduction des consommations d’énergie finale dans des bâtiments à usage tertiaire."
       currentPage="/professionnels"
     >
