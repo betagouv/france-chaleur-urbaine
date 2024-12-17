c572ee32bed884b36cde73677d5ea74d9e6aa7c3
Maxime Dréau <maxime@dreau.fr>
2024-12-17 11:57:21 +0100

    Désactive le warning taille réponse /api/demands

--- src/pages/api/demands/index.ts
+++ src/pages/api/demands/index.ts
@@ -1,15 +1,19 @@
 import type { NextApiRequest } from 'next';
 
-import { handleRouteErrors, invalidPermissionsError } from '@/server/helpers/server';
+import { handleRouteErrors, requireGetMethod } from '@/server/helpers/server';
 import { getDemands } from '@/server/services/manager';
+
+// disable the warning for this route as the result is big > 4MB
+export const config = {
+  api: {
+    responseLimit: false,
+  },
+};
 
 export default handleRouteErrors(
   async (req: NextApiRequest) => {
-    if (req.method === 'GET') {
-      return await getDemands(req.user);
-    }
-
-    throw invalidPermissionsError;
+    requireGetMethod(req);
+    return await getDemands(req.user);
   },
   {
     requireAuthentication: true,
