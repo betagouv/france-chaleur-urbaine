8767889f3c2e9cbd79c1cdcafdb7a3f76de71c27
Maxime Dréau <maxime@dreau.fr>
2025-01-09 12:54:17 +0100

    Disable file.io test

--- src/server/helpers/fileio.spec.ts
+++ src/server/helpers/fileio.spec.ts
@@ -2,7 +2,8 @@ import { describe, expect, test } from 'vitest';
 
 import { FILEIO_API_URL, FileIOClient } from './fileio';
 
-describe('uploadTempFile()', () => {
+// Désactivé car l'API file.io ne semble plus fonctionner au 09/01/2024
+describe.skip('uploadTempFile()', () => {
   test('returns a link to the file', async () => {
     await expect(
       new FileIOClient(FILEIO_API_URL, 'AAAAAAA.AAAAAAA-AAAAAAA-AAAAAAA-AAAAAAA').uploadTempFile('/etc/hostname', 'hostname.txt')
