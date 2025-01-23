3a38d9de47108a3a5edc3a1be0e153ca2af3c7bc
Martin Ratinaud <martinratinaud@gmail.com>
2025-01-23 11:56:18 +0400

    Supporte une taille pour les Notice

--- src/components/ui/Notice.tsx
+++ src/components/ui/Notice.tsx
@@ -2,13 +2,30 @@ import DSFRNotice, { type NoticeProps as DSFRNoticeProps } from '@codegouvfr/rea
 import React from 'react';
 
 export type NoticeProps = Omit<DSFRNoticeProps, 'isClosable' | 'title'> &
-  ({ title: NonNullable<React.ReactNode>; children?: never } | { children: NonNullable<React.ReactNode>; title?: never });
+  ({ title: NonNullable<React.ReactNode>; children?: never } | { children: NonNullable<React.ReactNode>; title?: never }) & {
+    size?: 'sm';
+  };
 
-const Notice: React.FC<NoticeProps> = ({ children, className, onClose, title, ...props }) => {
+const classNames: { titles: { [key: string]: string }; root: { [key: string]: string } } = {
+  titles: {
+    sm: '!text-xs',
+    md: '',
+  },
+  root: {
+    sm: '!py-0.5 [&_>div]:px-1.5',
+    md: '',
+  },
+};
+
+const Notice: React.FC<NoticeProps> = ({ children, className, onClose, title, size = 'md', ...props }) => {
   return (
     <DSFRNotice
       className={className}
       title={children || title}
+      classes={{
+        title: classNames.titles[size],
+        root: classNames.root[size],
+      }}
       onClose={onClose}
       {...({
         /* Force isCloseable when onClose is defined https://github.com/codegouvfr/react-dsfr/issues/342 */
