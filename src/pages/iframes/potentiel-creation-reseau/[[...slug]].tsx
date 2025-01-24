aa1743d505aaa334ea1b55002189ac0c35262f33
Maxime Dréau <maxime@dreau.fr>
2025-01-24 17:48:47 +0100

    Ajout d'une iframe pour le potentiel de création de réseau

---
+++ src/pages/iframes/potentiel-creation-reseau/[[...slug]].tsx
@@ -0,0 +1,74 @@
+import { type GetStaticPaths } from 'next';
+
+import { createMapConfiguration } from '@/components/Map/map-configuration';
+import { FCUMapContextProvider } from '@/components/Map/MapProvider';
+import {
+  DetailsCommune,
+  franceBounds,
+  Map,
+  type PotentielCreationReseauPageProps,
+  SearchCommune,
+} from '@/pages/collectivites-et-exploitants/potentiel-creation-reseau/[[...slug]]';
+import { getCommunePotentiel } from '@/server/services/communeAPotentiel';
+
+const Page: React.FC<PotentielCreationReseauPageProps> = ({ commune }) => {
+  const bounds = commune?.bounds || franceBounds;
+  return (
+    <div className="fr-grid-row">
+      <div className="fr-col-12 fr-col-lg-4 fr-my-2w fr-px-2v fr-px-lg-0 fr-pr-lg-2w">
+        {commune ? <DetailsCommune commune={commune} /> : <SearchCommune />}
+      </div>
+      <div className="fr-col-12 fr-col-lg-8">
+        <FCUMapContextProvider
+          initialMapConfiguration={createMapConfiguration({
+            reseauxDeChaleur: {
+              show: !!commune,
+            },
+            zonesOpportunite: {
+              show: !!commune,
+            },
+            besoinsEnChaleur: !!commune,
+            reseauxEnConstruction: !!commune,
+            communesFortPotentielPourCreationReseauxChaleur: {
+              show: !commune,
+            },
+          })}
+        >
+          <Map commune={commune} bounds={bounds} />
+        </FCUMapContextProvider>
+      </div>
+    </div>
+  );
+};
+
+export const getStaticProps = async ({ params }: { params: { slug?: string } }) => {
+  const codeInsee = (params?.slug?.[0] as string)?.split('-')[0];
+
+  const commune = codeInsee ? await getCommunePotentiel(codeInsee) : null;
+
+  if (codeInsee && !commune) {
+    return {
+      redirect: {
+        destination: `/iframes/potentiel-creation-reseau?notify=error:${encodeURIComponent(
+          "Désolé, nous n'avons pas trouvé la commune en question. Réessayez avec une autre"
+        )}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      commune,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
+
+export default Page;
