bf89fef29ec5370414fbb0b2efa6075a73e866df
Martin Ratinaud <martinratinaud@gmail.com>
2025-01-24 11:56:33 +0400

    Utilise l'utiliatire downloadFile pour ne pas repeter du code

--- src/hooks/useScreenshot.tsx
+++ src/hooks/useScreenshot.tsx
@@ -2,6 +2,7 @@ import domtoimage from 'dom-to-image';
 import React, { useState } from 'react';
 
 import { notify } from '@/services/notification';
+import { downloadFile } from '@/utils/browser';
 
 const dataUrlToBlob = (dataUrl: string): Blob => {
   const [header, base64] = dataUrl.split(',');
@@ -68,15 +69,8 @@ const useScreenshot = () => {
     if (!file || !filename) {
       return;
     }
-
     const url = URL.createObjectURL(file);
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = filename;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(url);
+    downloadFile(url, filename);
   };
 
   return {
