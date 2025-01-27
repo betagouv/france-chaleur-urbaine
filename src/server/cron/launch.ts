00e9072a392ada599f1b23b7284ddaae4d269c54
Martin Ratinaud <martinratinaud@gmail.com>
2025-01-27 18:57:49 +0400

    Lance la synchro des dernières connexions toutes les heures

--- src/server/cron/launch.ts
+++ src/server/cron/launch.ts
@@ -1,6 +1,6 @@
 import * as Sentry from '@sentry/node';
 
-import { syncGestionnairesWithUsers } from '@/server/services/airtable';
+import { syncGestionnairesWithUsers, syncLastConnectionFromUsers } from '@/server/services/airtable';
 import { dailyNewManagerMail, dailyRelanceMail, weeklyOldManagerMail } from '@/server/services/manager';
 
 import '@root/sentry.node.config';
@@ -10,12 +10,13 @@ export const jobs: Record<string, any> = {
   weeklyOldManagerMail,
   dailyRelanceMail,
   syncGestionnairesWithUsers,
+  syncLastConnectionFromUsers,
 };
 
-export const launchJob = async (job: string) => {
+export const launchJob = async (job: string, ...params: any) => {
   console.log(`CRON JOB START: ${job}`);
   try {
-    await jobs[job]();
+    await jobs[job](...params);
   } catch (e) {
     Sentry.captureException(e);
     console.log(`CRON JOB ERROR: ${job}`, e);
