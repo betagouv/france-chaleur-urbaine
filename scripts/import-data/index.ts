b298b7d4f7d6044ea83cc66e2a97dbbcf237cd93
Martin Ratinaud <martinratinaud@gmail.com>
2025-01-29 13:35:35 +0400

    Ajout d'une CLI pour facilement lancer une commande d'import de données

--- scripts/import-data/index.ts
+++ scripts/import-data/index.ts
@@ -1,66 +1,20 @@
-import { readdirSync } from 'fs';
-import { readdir } from 'fs/promises';
-import { join } from 'path';
-
 import { type ImportAdapter } from './types';
 
-function getAdapterFiles(folderPath: string): string[] {
-  return readdirSync(join(__dirname, folderPath))
-    .filter((file) => file.endsWith('.ts'))
-    .map((file) => file.replace('.ts', ''));
-}
+const adapters = {};
 
-/**
- * Asynchronously loads adapter classes from a given folder.
- * @param folderPath - The folder to scan for adapter files.
- */
-async function loadAdapters(folderPath: string): Promise<Record<string, new () => ImportAdapter>> {
-  const adapters: Record<string, new () => ImportAdapter> = {};
-
-  try {
-    const files = await readdir(folderPath);
-
-    for (const file of files) {
-      if (file.endsWith('.ts')) {
-        const moduleName = file.replace('.ts', '');
-        const rootModule = await import(join(folderPath, moduleName));
-
-        const className =
-          moduleName
-            .split('-')
-            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-            .join('') + 'Adapter';
-
-        if (rootModule[className]) {
-          adapters[moduleName] = rootModule[className];
-        }
-      }
-    }
-  } catch (error) {
-    console.error(`Error loading adapters from ${folderPath}:`, error);
-  }
-
-  return adapters;
-}
+export type AdapterName = keyof typeof adapters;
 
 export default class AdapterFactory {
-  public static adapterNames = getAdapterFiles('adapters');
-  public name: string | undefined;
-  public adapter: ImportAdapter | undefined;
-  public adapters: Record<string, new () => ImportAdapter> | undefined;
+  public name: string;
+  public adapter: ImportAdapter;
 
-  constructor(adapterName: string) {
+  constructor(adapterName: AdapterName) {
     this.name = adapterName;
-  }
+    this.adapter = new adapters[adapterName]();
 
-  async getInstance() {
-    if (!this.adapters) {
-      this.adapters = await loadAdapters(join(__dirname, 'adapters'));
-    }
     if (!this.adapter) {
-      this.adapter = new this.adapters[this.name as string]();
+      throw new Error(`unknown adapter ${adapterName}`);
     }
-    return this;
   }
 
   async importData(filepath?: string) {
