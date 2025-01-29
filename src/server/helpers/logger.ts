a18d663f2ee3a6cff08de101e54fdab6dda8c10d
Martin Ratinaud <martinratinaud@gmail.com>
2025-01-29 15:54:42 +0400

    Ajoute une mise en forme des logs plus joli avec LOGS_PRETTY true

--- src/server/helpers/logger.ts
+++ src/server/helpers/logger.ts
@@ -1,8 +1,61 @@
 import { createLogger, format, transports } from 'winston';
 
+/**
+ * Colorize inside of the message and display objects as stringified JSON.
+ */
+const prettyLogsInDev = format((info) => {
+  const { timestamp, level, message, name, ...rest } = info;
+  const symbols = Object.getOwnPropertySymbols(info);
+  const levelSymbol = symbols.find((sym) => sym.toString() === 'Symbol(level)');
+  const levelValue = info[levelSymbol as any];
+  const [startTag, endTag] = info.level.split(levelValue);
+
+  const additionalProps = Object.getOwnPropertyNames(rest).reduce(
+    (acc, key) => {
+      // get only those which are not a symbol
+      acc[key] = info[key];
+      return acc;
+    },
+    {} as { [key: string]: any }
+  );
+
+  if (typeof info.message === 'object') {
+    info.message = JSON.stringify(info.message, null, 2);
+  }
+
+  if (info.name) {
+    info.message = `${startTag}[${info.name}]${endTag} ${info.message}`;
+  }
+
+  if (process.env.COLORIZE_LOGS === 'true') {
+    info.message = `${startTag}${info.message}${endTag}`;
+  }
+
+  if (Object.keys(additionalProps).length > 0) {
+    info.message = `${info.message} \x1b[90m${JSON.stringify(additionalProps)}\x1b[0m`;
+  }
+  const formattedTimestamp = new Date(timestamp).toLocaleTimeString('en-GB', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+
+  info.message = `\x1b[97m${formattedTimestamp}\x1b[0m ${info.message}`;
+
+  return info;
+});
+
 export const logger = createLogger({
   level: 'info',
-  format: format.combine(format.timestamp(), format((info) => ({ ...info, pid: process.pid }))(), format.json()),
+  format:
+    process.env.LOGS_PRETTY === 'true'
+      ? format.combine(
+          format.timestamp(),
+          format.colorize(), // Add color to log level and timestamp
+          prettyLogsInDev(),
+          format.cli() // Apply CLI colorization to other parts of the log
+        )
+      : format.combine(format.timestamp(), format((info) => ({ ...info, pid: process.pid }))(), format.json()),
   transports: [new transports.Console({})],
 });
 
