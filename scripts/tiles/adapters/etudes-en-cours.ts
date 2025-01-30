0414063dc521ae0be45da2daeaa7578991536ed2
Martin Ratinaud <martinratinaud@gmail.com>
2025-01-30 15:29:03 +0400

    Genere le geojson pour les études en cours

---
+++ scripts/tiles/adapters/etudes-en-cours.ts
@@ -0,0 +1,5 @@
+import { BaseAdapter } from '../base';
+
+export default class EtudesEnCoursAdapter extends BaseAdapter {
+  public databaseName = 'etude_en_cours';
+}
--- scripts/tiles/base.ts
+++ scripts/tiles/base.ts
@@ -1,5 +1,17 @@
+import { rename } from 'fs/promises';
+
+import { runBash } from '../helpers/shell';
+
 export abstract class BaseAdapter {
   abstract readonly databaseName: string;
 
-  abstract generateTilesGeoJSON(filepath?: string): Promise<string>;
+  async generateGeoJSON(filepath?: string) {
+    const filepathToExport = filepath || `${this.databaseName}.geojson`;
+    await runBash(
+      `docker run -it --rm --network host -v /tmp/fcu:/volume -w /volume --user $(id -u):$(id -g) ghcr.io/osgeo/gdal:alpine-normal-latest-arm64 ogr2ogr -f GeoJSON output.geojson PG:"host=localhost user=postgres dbname=postgres password=postgres_fcu" etudes_en_cours -t_srs EPSG:4326`
+    );
+    await rename('/tmp/fcu/output.geojson', filepathToExport);
+
+    return filepathToExport;
+  }
 }
