ba2292ed64bc7e2fa41aa916419cc1f664f5f118
Martin Ratinaud <martinratinaud@gmail.com>
2025-02-05 10:43:39 +0400

    Retravaille les migrations pour qu'elles passent en dev

--- src/server/db/migrations/20250203114811_etudes_en_cours_commune_names.ts
+++ src/server/db/migrations/20250203114811_etudes_en_cours_commune_names.ts
@@ -1,13 +1,9 @@
 import { type Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.table('etudes_en_cours', (table) => {
-    table.string('description');
-  });
+  await knex.raw('ALTER TABLE etudes_en_cours ADD COLUMN IF NOT EXISTS description VARCHAR(255)');
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.table('etudes_en_cours', (table) => {
-    table.dropColumn('description');
-  });
+  await knex.raw('ALTER TABLE etudes_en_cours DROP COLUMN IF EXISTS description');
 }
--- src/server/db/migrations/20250204115311_etudes_en_cours_rename_column.ts
+++ src/server/db/migrations/20250204115311_etudes_en_cours_rename_column.ts
@@ -1,13 +1,9 @@
 import { type Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.table('etudes_en_cours', (table) => {
-    table.renameColumn('description', 'communes');
-  });
+  await knex.raw('ALTER TABLE etudes_en_cours RENAME COLUMN description TO communes');
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.table('etudes_en_cours', (table) => {
-    table.renameColumn('communes', 'description');
-  });
+  await knex.raw('ALTER TABLE etudes_en_cours RENAME COLUMN communes TO description');
 }
