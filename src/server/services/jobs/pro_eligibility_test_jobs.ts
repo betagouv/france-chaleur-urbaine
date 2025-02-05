d39d42e5bd2da58d7e3868075ca9a0985cfca47f
Maxime Dréau <maxime@dreau.fr>
2025-02-05 11:53:04 +0100

    Catch des erreurs pendant le traitement des adresses en // [skip ci]

--- src/server/services/jobs/pro_eligibility_test_jobs.ts
+++ src/server/services/jobs/pro_eligibility_test_jobs.ts
@@ -1,10 +1,10 @@
 import { sql, type Selectable } from 'kysely';
+import { limitFunction } from 'p-limit';
 import { type Logger } from 'winston';
 
 import { kdb, type Jobs } from '@/server/db/kysely';
 import { getEligilityStatus } from '@/server/services/addresseInformation';
 import { getAddressesCoordinates } from '@/server/services/api-adresse';
-import { processInParallel } from '@/types/async';
 
 export type ProEligibilityTestJob = Omit<Selectable<Jobs>, 'data'> & {
   type: 'pro_eligibility_test';
@@ -18,28 +18,34 @@ export async function processProEligibilityTestJob(job: ProEligibilityTestJob, l
   const results = await getAddressesCoordinates(job.data.csvContent);
   logger.info('API BAN', { duration: Date.now() - startTime });
 
+  console.log('BAN results', results);
   {
     const startTime = Date.now();
     await kdb.transaction().execute(async (trx) => {
       // on supprime toutes les adresses si le job a déjà été traité
       await trx.deleteFrom('pro_eligibility_tests_addresses').where('test_id', '=', job.entity_id).execute();
 
-      await processInParallel(results, 20, async (addressItem) => {
-        const eligibilityStatus =
-          addressItem.result_status === 'ok' ? await getEligilityStatus(addressItem.latitude, addressItem.longitude) : null;
-        await trx
-          .insertInto('pro_eligibility_tests_addresses')
-          .values({
-            test_id: job.entity_id,
-            source_address: addressItem.address,
-            ban_valid: addressItem.result_status === 'ok',
-            ban_address: addressItem.result_label ?? '',
-            ban_score: Math.round((addressItem.result_score ?? 0) * 100), // 0.9733 => 97
-            geom: sql`st_transform(st_point(${addressItem.longitude}, ${addressItem.latitude}, 4326), 2154)`,
-            eligibility_status: eligibilityStatus ?? undefined,
-          })
-          .executeTakeFirst();
-      });
+      const processAddress = limitFunction(
+        async (addressItem: (typeof results)[number]) => {
+          const eligibilityStatus =
+            addressItem.result_status === 'ok' ? await getEligilityStatus(addressItem.latitude, addressItem.longitude) : null;
+          await trx
+            .insertInto('pro_eligibility_tests_addresses')
+            .values({
+              test_id: job.entity_id,
+              source_address: addressItem.address,
+              ban_valid: addressItem.result_status === 'ok',
+              ban_address: addressItem.result_label ?? '',
+              ban_score: Math.round((addressItem.result_score ?? 0) * 100), // 0.9733 => 97
+              geom: sql`st_transform(st_point(${addressItem.longitude}, ${addressItem.latitude}, 4326), 2154)`,
+              eligibility_status: eligibilityStatus ?? undefined,
+            })
+            .executeTakeFirst();
+        },
+        { concurrency: 20 }
+      );
+
+      await Promise.all(results.map((addressItem) => processAddress(addressItem)));
     });
     logger.info('test éligibilité', { duration: Date.now() - startTime });
   }
