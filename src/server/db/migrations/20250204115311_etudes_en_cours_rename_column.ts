890b6955f92520d56394922c5008feda465575e0
Martin Ratinaud <martinratinaud@gmail.com>
2025-02-05 13:30:32 +0400

    Dernier essai pour fixer les migrations

--- src/server/db/migrations/20250204115311_etudes_en_cours_rename_column.ts
+++ src/server/db/migrations/20250204115311_etudes_en_cours_rename_column.ts
@@ -1,9 +1,15 @@
 import { type Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.raw('ALTER TABLE etudes_en_cours RENAME COLUMN description TO communes');
+  const columnExists = await knex.schema.hasColumn('etudes_en_cours', 'description');
+  if (columnExists) {
+    await knex.raw('ALTER TABLE etudes_en_cours RENAME COLUMN description TO communes');
+  }
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.raw('ALTER TABLE etudes_en_cours RENAME COLUMN communes TO description');
+  const columnExists = await knex.schema.hasColumn('etudes_en_cours', 'communes');
+  if (columnExists) {
+    await knex.raw('ALTER TABLE etudes_en_cours RENAME COLUMN communes TO description');
+  }
 }
