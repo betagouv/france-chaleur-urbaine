216cb08bd6a9d75c8eaa82135253b6d492950743
Martin Ratinaud <martinratinaud@gmail.com>
2025-02-05 14:17:24 +0400

    Supprime les migrations qui posent probleme au build

--- src/server/db/migrations/20250203114811_etudes_en_cours_commune_names.ts
+++
@@ -1,9 +0,0 @@
-import { type Knex } from 'knex';
-
-export async function up(knex: Knex): Promise<void> {
-  await knex.raw('ALTER TABLE etudes_en_cours ADD COLUMN IF NOT EXISTS description VARCHAR(255)');
-}
-
-export async function down(knex: Knex): Promise<void> {
-  await knex.raw('ALTER TABLE etudes_en_cours DROP COLUMN IF EXISTS description');
-}
