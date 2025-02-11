0e93b04a64600973a89bf837494bbc9001a3087a
Maxime Dréau <maxime@dreau.fr>
2025-02-11 17:32:35 +0100

    Ajout variable d'env pour logger les requêtes sql

--- src/server/db/kysely/index.ts
+++ src/server/db/kysely/index.ts
@@ -4,6 +4,9 @@ export { sql } from 'kysely';
 export * from './database';
 import '@/server/db'; // permet d'importer les variables d'env correctement
 
+import { env } from '@/environment';
+import { parentLogger } from '@/server/helpers/logger';
+
 import { type DB as Database } from './database';
 
 // Automatically convert postgres number fields from string to javascript numbers.
@@ -18,12 +21,26 @@ const dialect = new PostgresDialect({
   pool: new Pool({ connectionString: process.env.DATABASE_URL }),
 });
 
+const logger = parentLogger.child({
+  module: 'database',
+});
+
 // Database interface is passed to Kysely's constructor, and from now on, Kysely
 // knows your database structure.
 // Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
 // to communicate with your database.
 export const db = new Kysely<Database>({
   dialect,
+  log: env.LOG_SQL_QUERIES
+    ? (event) => {
+        logger.debug(`query ${event.level === 'error' ? 'failed' : 'completed'}`, {
+          durationMs: Math.round(event.queryDurationMillis),
+          sql: event.query.sql,
+          params: event.query.parameters,
+          ...(event.level === 'error' ? { error: event.error } : {}),
+        });
+      }
+    : undefined,
 });
 
 /**
