c362ed30e68c4f4e5e7c0f87d41385127cab990f
Martin Ratinaud <martinratinaud@gmail.com>
2025-02-19 11:25:35 +0400

    Modification de l'encart réduire l'impact

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -309,29 +309,36 @@ export default function Home() {
       <Partners />
 
       <Section id="obligation-raccordement" variant="accent">
-        <SectionContent className="mt-0">
+        <SectionContent className="!mt-0">
           <SectionHeading as="h2" size="h5">
             Réduire l'impact écologique et économique de son chauffage
           </SectionHeading>
-          <p className="mt-3w">Le chauffage urbain, une solution pour les copropriétés</p>
           <p className="mt-3w">
-            Le chauffage représente 67 % de la consommation d’énergie des foyers français et près de 20 % des émissions de gaz à effet de
-            serre nationales. L’augmentation des prix de l’énergie pèse sur le budget des ménages : 40 % des logements sont encore chauffés
-            au gaz, dont les prix ont augmenté de 41 % en 10 ans.
+            Les réseaux de chaleur urbains, une solution décarboner le chauffage des copropriétés, immeubles de logement social et bâtiments
+            tertiaires.
           </p>
           <p className="mt-3w">
-            Pour réduire l’impact écologique d’une copropriété et ses factures d’énergie, la rénovation thermique est le premier réflexe à
-            avoir. Le <Link href="/ressources/avantages#contenu">remplacement d’un chauffage collectif au gaz ou fioul</Link>, par un
-            raccordement à un réseau de chaleur permet également d’y contribuer. Alimentés majoritairement par des énergies renouvelables et
-            de récupération locales, les réseaux de chaleur émettent deux fois moins de gaz à effet de serre qu’un chauffage gaz ou fioul et
-            offrent des prix stables et compétitifs.
+            Le chauffage représente 67 % de la consommation d’énergie des foyers français et près de 20 % des émissions de gaz à effet de
+            serre nationales. Près de 40 % des logements sont encore chauffés au gaz, dont les prix ont fortement fluctué ses dernières
+            années.
+          </p>
+          <p className="mt-3w">
+            Pour réduire l’impact écologique du chauffage, la rénovation thermique est le premier réflexe à avoir. Le{' '}
+            <Link href="/ressources/avantages#contenu">remplacement d’un chauffage collectif au gaz ou fioul</Link> par un raccordement à un
+            réseau de chaleur permet également d’y contribuer. Alimentés majoritairement par des énergies renouvelables et de récupération
+            locales, les réseaux de chaleur émettent deux fois moins de gaz à effet de serre qu’un chauffage au gaz et trois fois moins
+            qu'un chauffage au fioul. Ils offrent généralement des prix compétitifs et plus stables que ceux des énergies fossiles.
           </p>
           <p className="mt-6w">
             Des réseaux de chaleur existent dans la plupart des grandes villes, par exemple <Link href="/villes/paris">Paris</Link>,{' '}
             <Link href="/villes/rennes">Rennes</Link>, <Link href="/villes/nantes">Nantes</Link>,{' '}
             <Link href="/villes/bordeaux">Bordeaux</Link>, <Link href="/villes/strasbourg">Strasbourg</Link>,{' '}
             <Link href="/villes/metz">Metz</Link>, <Link href="/villes/grenoble">Grenoble</Link>, <Link href="/villes/lyon">Lyon</Link>,{' '}
-            <Link href="/villes/aix-en-provence">Aix-en-Provence</Link>,...
+            <Link href="/villes/aix-en-provence">Aix-en-Provence</Link>…
+          </p>
+          <p className="mt-6w mb-0">
+            Vous êtes professionnels (bureau d'étude, bailleur social, gestionnaire de bâtiments tertiaires…) ? Rendez-vous sur notre{' '}
+            <Link href="/professionnels">page dédiée</Link>
           </p>
         </SectionContent>
       </Section>
