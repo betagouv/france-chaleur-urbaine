cf5f853d96de7cd7122003a04d3a9051d4a4397b
Martin Ratinaud <martinratinaud@gmail.com>
2025-02-25 15:23:00 +0400

    Utilise radix tooltip et tailwind au lieu de MUI pour pouvoir à terme supprimer MUI

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -1,39 +1,73 @@
-import { styled } from '@mui/material';
-import { default as MUITooltip, type TooltipProps as MUITooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+'use client';
 
-import Icon, { type IconProps } from './Icon';
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+import * as React from 'react';
 
-type TooltipProps = Omit<MUITooltipProps, 'children'> &
-  Partial<Pick<MUITooltipProps, 'children'>> & {
-    iconProps?: Partial<IconProps>;
-  };
+import Icon, { type IconProps } from '@/components/ui/Icon';
+import cx from '@/utils/cx';
 
-const StyledIcon = styled(Icon)`
-  align-self: start;
-  line-height: 1em !important;
-`;
+const TooltipProvider = TooltipPrimitive.Provider;
 
-const DSFRLikeStyledTooltip = styled(({ className, ...props }: MUITooltipProps) => (
-  <MUITooltip {...props} classes={{ popper: className }} />
-))(({ theme }) => ({
-  [`& .${tooltipClasses.tooltip}`]: {
-    backgroundColor: theme.palette.common.white,
-    color: 'rgb(58, 58, 58)',
-    boxShadow: theme.shadows[2],
-    lineHeight: '14px',
-    fontSize: '12px',
-    padding: '8px',
-  },
-  [`& .${tooltipClasses.tooltip} .${tooltipClasses.arrow}`]: {
-    color: theme.palette.common.white,
-  },
-}));
+const TooltipRoot = TooltipPrimitive.Root;
 
-export default function Tooltip({ children, iconProps, placement, ...props }: TooltipProps) {
+export const TooltipTrigger = TooltipPrimitive.Trigger;
+
+export const TooltipContent = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+>(({ className, sideOffset = 4, ...props }, ref) => (
+  <TooltipPrimitive.Portal>
+    <TooltipPrimitive.Content
+      ref={ref}
+      sideOffset={sideOffset}
+      className={cx(
+        'z-50 overflow-hidden shadow-lg rounded-sm bg-white px-3 py-1.5 text-xs text-black max-w-[300px] animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+        className
+      )}
+      {...props}
+    />
+  </TooltipPrimitive.Portal>
+));
+TooltipContent.displayName = TooltipPrimitive.Content.displayName;
+
+export const TooltipWrapper: React.FC<React.ComponentProps<typeof TooltipProvider>> = ({ children, ...props }) => {
   return (
-    // Un composant react-dsfr devrait arriver bientôt https://github.com/codegouvfr/react-dsfr/pull/190)
-    <DSFRLikeStyledTooltip arrow placement={placement ?? 'top'} {...props}>
-      {children ?? <StyledIcon size="sm" name="ri-information-fill" cursor="help" {...iconProps} />}
-    </DSFRLikeStyledTooltip>
+    <TooltipProvider {...props}>
+      <TooltipRoot>{children}</TooltipRoot>
+    </TooltipProvider>
   );
-}
+};
+
+type TooltipProps = {
+  title: React.ReactNode;
+  children?: React.ReactNode;
+  iconProps?: Partial<IconProps>;
+  side?: TooltipPrimitive.TooltipContentProps['side'];
+  sideOffset?: TooltipPrimitive.TooltipContentProps['sideOffset'];
+  delayDuration?: TooltipPrimitive.TooltipProviderProps['delayDuration'];
+  skipDelayDuration?: TooltipPrimitive.TooltipProviderProps['skipDelayDuration'];
+};
+
+const Tooltip = ({
+  children,
+  title,
+  iconProps,
+  side = 'top',
+  sideOffset = 5,
+  delayDuration = 200,
+  skipDelayDuration = 500,
+}: TooltipProps) => {
+  return (
+    <TooltipWrapper delayDuration={delayDuration} skipDelayDuration={skipDelayDuration}>
+      <TooltipTrigger asChild>{children ?? <Icon size="sm" name="ri-information-fill" cursor="help" {...iconProps} />}</TooltipTrigger>
+      <TooltipContent side={side} sideOffset={sideOffset}>
+        <div>
+          {title} {JSON.stringify(iconProps || {})}
+        </div>
+        <TooltipPrimitive.Arrow className="fill-white" />
+      </TooltipContent>
+    </TooltipWrapper>
+  );
+};
+
+export default Tooltip;
