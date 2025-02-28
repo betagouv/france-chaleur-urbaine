62f5dfd5e0d12426f2973c51a2229c24320fd4f9
Maxime Dréau <maxime@dreau.fr>
2025-02-28 11:51:29 +0100

    Changement type geom réseaux futurs

---
+++ src/server/db/migrations/20250225000003_change_futur_reseau_geom.ts
@@ -0,0 +1,9 @@
+import { type Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    alter table zones_et_reseaux_en_construction alter column geom type geometry using geom::geometry(geometry, 2154);
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {} // eslint-disable-line
