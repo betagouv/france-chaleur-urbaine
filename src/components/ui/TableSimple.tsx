3c60b3ad2c006ac0486c0235933aafdfeae3e4b1
Martin Ratinaud <martinratinaud@gmail.com>
2025-03-04 13:21:00 +0400

    Ajoute un caption à TableSimple

--- src/components/ui/TableSimple.tsx
+++ src/components/ui/TableSimple.tsx
@@ -34,6 +34,7 @@ export type TableSimpleProps<T> = {
   initialSortingState?: SortingState;
   columnFilters?: ColumnFiltersState;
   loading?: boolean;
+  caption?: string;
   enableRowSelection?: boolean;
   onSelectionChange?: (selectedRows: T[]) => void;
 };
@@ -44,6 +45,7 @@ const TableSimple = <T extends RowData>({
   initialSortingState,
   columnFilters,
   loading,
+  caption,
   enableRowSelection,
   onSelectionChange,
 }: TableSimpleProps<T>) => {
@@ -167,7 +169,7 @@ const TableSimple = <T extends RowData>({
   const hasAtLeastOneColumnSorting = table.getHeaderGroups()[0].headers.some((header) => header.column.getCanSort());
 
   return (
-    <>
+    <section>
       <Input
         label=""
         nativeInputProps={{
@@ -176,6 +178,7 @@ const TableSimple = <T extends RowData>({
           placeholder: 'Recherche...',
         }}
       />
+      {caption && <div className="text-2xl leading-8 font-bold mb-5">{caption}</div>}
       <div
         className={fr.cx('fr-table', 'fr-table--no-scroll')}
         ref={tableContainerRef}
@@ -300,7 +303,7 @@ const TableSimple = <T extends RowData>({
           </tbody>
         </table>
       </div>
-    </>
+    </section>
   );
 };
 
