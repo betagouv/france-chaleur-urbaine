29d7443be7bfff9fb984b43316ea64f3e8224a34
Martin Ratinaud <martinratinaud@gmail.com>
2025-03-04 17:03:23 +0400

    Ajoute correctement les feature flags

--- src/environment.ts
+++ src/environment.ts
@@ -8,11 +8,12 @@ dotenv.config();
 const envSchema = {
   NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
   NEXT_PUBLIC_FLAG_ENABLE_COMPARATEUR: z.boolean().default(false),
+  NEXT_PUBLIC_FLAG_ENABLE_TEST_ADRESSES: z.boolean().default(false),
+  NEXT_PUBLIC_FLAG_ENABLE_INSCRIPTIONS: z.boolean().default(false),
   IS_REVIEW_APP: z.boolean().default(false),
   GITHUB_CI: z.boolean().default(false),
   LOG_LEVEL: z.enum(['debug', 'info', 'warn', 'error']).default('info'),
   API_ADRESSE_URL: z.string().default('https://api-adresse.data.gouv.fr/'),
-  NEXT_PUBLIC_FLAG_ENABLE_INSCRIPTIONS: z.boolean().default(false),
   CLOCK_CRONS_ENABLE: z.boolean().default(true),
   CLOCK_JOBS_PROCESSOR_ENABLE: z.boolean().default(true),
   LOG_SQL_QUERIES: z.boolean().default(false),
