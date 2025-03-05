553193b1e1f103405992ed62805794e059cec3bd
Martin Ratinaud <martinratinaud@gmail.com>
2025-03-05 13:59:24 +0400

    Ajoute un hook useCookie pour fixer les pb typescript

---
+++ src/hooks/useCookie.ts
@@ -0,0 +1,16 @@
+/**
+ * This hook uses @react-hookz/web package for cookie management.
+ * It needs dependencies and typescript as it does not work out of the box.
+ *
+ *
+ * To install the required dependencies:
+ * ```
+ * yarn add @react-hookz/web js-cookie
+ * yarn add -D @types/js-cookie
+ * ```
+ *
+ * Note: https://react-hookz.github.io/web/?path=/docs/side-effect-usecookievalue--example#importing
+ */
+import { useCookieValue } from '@react-hookz/web/useCookieValue';
+
+export default useCookieValue;
