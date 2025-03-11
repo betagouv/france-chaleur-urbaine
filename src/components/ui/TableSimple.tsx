f10d505dfbb175c45d8deecbc6e9f4977324ae50
Martin Ratinaud <martinratinaud@gmail.com>
2025-03-11 17:24:48 +0400

    Gère la possibilité de passer une width à une colonne de TableSimple

--- src/components/ui/TableSimple.tsx
+++ src/components/ui/TableSimple.tsx
@@ -23,10 +23,9 @@ import TableCell, { type TableCellProps } from './TableCell';
 export type ColumnDef<T, K = any> = ColumnDefOriginal<T, K> & {
   cellType?: TableCellProps<T>['type'];
   align?: 'center' | 'left' | 'right';
-  flex?: number;
   className?: string;
   suffix?: React.ReactNode;
-};
+} & ({ flex?: number } | { width?: 'auto' | string });
 
 export type TableSimpleProps<T> = {
   columns: ColumnDef<T>[];
@@ -164,9 +163,11 @@ const TableSimple = <T extends RowData>({
 
   const gridTemplateColumns = table
     .getHeaderGroups()[0]
-    .headers.map((header, index) =>
-      enableRowSelection && index === 0 ? 'auto' : `${(header.column.columnDef as ColumnDef<T>).flex || 1}fr`
-    )
+    .headers.map((header, index) => {
+      const columnDef = header.column.columnDef as ColumnDef<T>;
+      const sizeValue = (columnDef as any)?.width ?? `${(columnDef as any)?.flex ?? 1}fr`; // TOFIX: remove any but could not find a way to do it
+      return enableRowSelection && index === 0 ? 'auto' : sizeValue;
+    })
     .join(' ');
   const hasAtLeastOneColumnSorting = table.getHeaderGroups()[0].headers.some((header) => header.column.getCanSort());
 
@@ -218,7 +219,12 @@ const TableSimple = <T extends RowData>({
                       {header.isPlaceholder ? null : (
                         <>
                           {/* mt-[5px] to be aligned  */}
-                          <span className={cx('break-all', hasAtLeastOneColumnSorting ? 'mt-[5px]' : '')}>
+                          <span
+                            className={cx('', hasAtLeastOneColumnSorting ? 'mt-[5px]' : '')}
+                            style={{
+                              wordBreak: 'break-word', // does not exist in tailwind
+                            }}
+                          >
                             {flexRender(columnDef.header, header.getContext())}
                           </span>
                           {header.column.getCanSort() && (
