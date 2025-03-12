ce7e0101b6487ac9da17d2de5e232698cec56ec0
Martin Ratinaud <martinratinaud@gmail.com>
2025-03-12 11:24:11 +0400

    Ajoute le handle sur le drawer etant donné qu'il est swipeable

--- src/components/ui/Drawer.tsx
+++ src/components/ui/Drawer.tsx
@@ -31,15 +31,27 @@ const drawerContentVariants = cva('bg-white h-full fixed outline-none z-[1750] f
   },
 });
 
-const Drawer = ({ children, open, onClose, direction = 'right', className, full = false }: DrawerProps) => {
+const Drawer = ({ children, open, onClose, direction = 'right', className, full = false, handleOnly = false, ...props }: DrawerProps) => {
+  const orientation = direction === 'left' || direction === 'right' ? 'horizontal' : 'vertical';
+
   return (
-    <DrawerVaul.Root open={open} onOpenChange={(open) => !open && onClose?.()} direction={direction}>
+    <DrawerVaul.Root open={open} onOpenChange={(open) => !open && onClose?.()} direction={direction} handleOnly={handleOnly} {...props}>
       <DrawerVaul.Portal>
         <DrawerVaul.Overlay className="fixed inset-0 bg-black/40 z-[1750]" />
         <DrawerVaul.Content className={cx(drawerContentVariants({ direction, full }), className)}>
           <div className="text-right px-3 pt-3">
             <Icon name="fr-icon-close-line" size="lg" onClick={onClose} className="cursor-pointer hover:opacity-70 inline-block" />
           </div>
+          {!props.dismissible && (
+            <DrawerVaul.Handle
+              className={cx(
+                '!mx-0 absolute', // override default values which prevent right positioning
+                'flex-shrink-0 rounded-full bg-zinc-300',
+                orientation === 'horizontal' ? '!w-1.5 !h-12 top-1/2 -translate-y-1/2' : '!w-12 !h-1.5 left-1/2 -translate-x-1/2',
+                direction === 'right' ? 'left-1.5' : direction === 'left' ? 'right-1.5' : direction === 'bottom' ? 'top-1.5' : 'bottom-1.5'
+              )}
+            />
+          )}
           <div className="flex-1 px-3 pb-3 overflow-y-auto ">{children}</div>
         </DrawerVaul.Content>
       </DrawerVaul.Portal>
