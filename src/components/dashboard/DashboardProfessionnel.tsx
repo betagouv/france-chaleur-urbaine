5ebc071c026ebe25dc324e74ef327ff0990fd9f0
Maxime Dréau <maxime@dreau.fr>
2025-03-14 15:22:00 +0100

    Lien comparateur en 1er dans la navigation

--- src/components/dashboard/DashboardProfessionnel.tsx
+++ src/components/dashboard/DashboardProfessionnel.tsx
@@ -6,23 +6,23 @@ export default function DashboardProfessionnel() {
   return (
     <>
       <div className="grid gap-8 grid-cols-2 items-center justify-between mb-5">
-        {clientConfig.ENABLE_TEST_ADRESSES && (
-          <Tile
-            title="Tests d'adresses"
-            desc="Testez l'éligibilité d'adresses pour le raccordement à un réseau de chaleur"
-            linkProps={{
-              href: '/pro/tests-adresses',
-            }}
-            orientation="horizontal"
-            enlargeLinkOrButton
-          />
-        )}
         {clientConfig.ENABLE_COMPARATEUR && (
           <Tile
             title="Comparateur de performances"
             desc="Comparez les performances énergétiques et environnementales des différents modes de chauffage et de refroidissement"
             linkProps={{
               href: '/pro/comparateur-couts-performances',
+            }}
+            orientation="horizontal"
+            enlargeLinkOrButton
+          />
+        )}
+        {clientConfig.ENABLE_TEST_ADRESSES && (
+          <Tile
+            title="Tests d'adresses"
+            desc="Testez l'éligibilité d'adresses pour le raccordement à un réseau de chaleur"
+            linkProps={{
+              href: '/pro/tests-adresses',
             }}
             orientation="horizontal"
             enlargeLinkOrButton
