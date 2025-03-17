104fa8fc8a44e1e638aa0c8563194b1a582075e1
Martin Ratinaud <martinratinaud@gmail.com>
2025-03-17 14:23:21 +0400

    Ajout du message dans l'écran de connexion

--- src/pages/inscription.tsx
+++ src/pages/inscription.tsx
@@ -2,6 +2,8 @@ import RegisterForm from '@/components/connexion/RegisterForm';
 import CenterLayout from '@/components/shared/page/CenterLayout';
 import SimplePage from '@/components/shared/page/SimplePage';
 import Heading from '@/components/ui/Heading';
+import Highlight from '@/components/ui/Highlight';
+import Link from '@/components/ui/Link';
 
 function InscriptionPage() {
   return (
@@ -10,6 +12,17 @@ function InscriptionPage() {
         <Heading as="h1" size="h2" color="blue-france">
           Création de compte sur France Chaleur Urbaine
         </Heading>
+        <Highlight variant="blue" size="sm">
+          <span className="block">Connectez-vous pour bénéficier de fonctionnalités avancées :</span>
+          <br />- <strong>Comparez</strong> simplement les coûts et <strong>émissions de CO2</strong> de 16 modes de chauffage,
+          <br />- Accédez au <strong>mode avancé du comparateur</strong> afin de paramétrer vos simulations,
+          <br />- <strong>Testez en masse</strong> la proximité à un réseau de chaleur,
+          <br />- Envoyez des <strong>demandes d'informations groupées</strong>
+        </Highlight>
+        <p className="fr-text--sm">
+          <strong>Vous êtes maître d'ouvrage ou gestionnaire d'un réseau de chaleur ?</strong> Retrouvez l'ensemble des demandes déposées à
+          proximité de votre réseau. Pour paramétrer vos accès, merci de <Link href="/contact">nous contacter</Link>.
+        </p>
 
         <RegisterForm />
       </CenterLayout>
