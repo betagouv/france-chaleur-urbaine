f39a4471b20ef3b299307bbcb2bf1374cba9c08f
Martin Ratinaud <martinratinaud@gmail.com>
2025-03-17 14:22:17 +0400

    Ajoute le composant Highlight

---
+++ src/components/ui/Highlight.tsx
@@ -0,0 +1,35 @@
+import { Highlight as DSFRHighlight, type HighlightProps as DSFRHighlightProps } from '@codegouvfr/react-dsfr/Highlight';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+const highlightVariants = cva('', {
+  variants: {
+    variant: {
+      default: '',
+      blue: '!bg-[image:linear-gradient(0deg,var(--border-action-high-blue-france),var(--border-action-high-blue-france))]',
+      info: '!bg-[image:linear-gradient(0deg,var(--border-action-high-info),var(--border-action-high-info))]',
+      success: '!bg-[image:linear-gradient(0deg,var(--border-action-high-success),var(--border-action-high-success))]',
+      warning: '!bg-[image:linear-gradient(0deg,var(--border-action-high-warning),var(--border-action-high-warning))]',
+      error: '!bg-[image:linear-gradient(0deg,var(--border-action-high-error),var(--border-action-high-error))]',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+
+export type HighlightProps = VariantProps<typeof highlightVariants> & DSFRHighlightProps;
+
+const Highlight = ({ children, className, variant, size }: HighlightProps) => {
+  return (
+    <DSFRHighlight
+      size={size}
+      classes={{
+        root: highlightVariants({ variant, className }),
+      }}
+    >
+      {children}
+    </DSFRHighlight>
+  );
+};
+
+export default Highlight;
