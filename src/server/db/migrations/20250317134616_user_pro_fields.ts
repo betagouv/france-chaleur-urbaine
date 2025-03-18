4bcaed69157f8a796ba276fefb58fca77feaf02b
Martin Ratinaud <martinratinaud@gmail.com>
2025-03-18 14:17:20 +0400

    Affine les migrations pou rqu'elles soient plus résilientes

--- src/server/db/migrations/20250317134616_user_pro_fields.ts
+++ src/server/db/migrations/20250317134616_user_pro_fields.ts
@@ -2,21 +2,21 @@ import { type Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.raw(`
-    ALTER TABLE public.users ADD COLUMN first_name varchar(255);
-    ALTER TABLE public.users ADD COLUMN last_name varchar(255);
-    ALTER TABLE public.users ADD COLUMN structure varchar(255) NULL;
-    ALTER TABLE public.users ADD COLUMN structure_type varchar(100) NULL;
-    ALTER TABLE public.users ADD COLUMN structure_other varchar(255) NULL;
-    ALTER TABLE public.users ADD COLUMN job varchar(255) NULL;
-    ALTER TABLE public.users ADD COLUMN phone varchar(20);
-    ALTER TABLE public.users ADD COLUMN besoins varchar(255)[];
-    ALTER TABLE public.users ADD COLUMN accepted_cgu_at timestamp;
-    ALTER TABLE public.users ADD COLUMN optin_at timestamp;
+    ALTER TABLE public.users ADD COLUMN IF NOT EXISTS first_name varchar(255);
+    ALTER TABLE public.users ADD COLUMN IF NOT EXISTS last_name varchar(255);
+    ALTER TABLE public.users ADD COLUMN IF NOT EXISTS "structure" varchar(255) NULL;
+    ALTER TABLE public.users ADD COLUMN IF NOT EXISTS structure_type varchar(100) NULL;
+    ALTER TABLE public.users ADD COLUMN IF NOT EXISTS structure_other varchar(255) NULL;
+    ALTER TABLE public.users ADD COLUMN IF NOT EXISTS job varchar(255) NULL;
+    ALTER TABLE public.users ADD COLUMN IF NOT EXISTS phone varchar(20);
+    ALTER TABLE public.users ADD COLUMN IF NOT EXISTS besoins varchar(255)[];
+    ALTER TABLE public.users ADD COLUMN IF NOT EXISTS accepted_cgu_at timestamp;
+    ALTER TABLE public.users ADD COLUMN IF NOT EXISTS optin_at timestamp;
 
-    CREATE INDEX idx_users_first_name ON public.users(first_name);
-    CREATE INDEX idx_users_last_name ON public.users(last_name);
-    CREATE INDEX idx_users_structure ON public.users(structure);
-    CREATE INDEX idx_users_optin_at ON public.users(optin_at);
+    CREATE INDEX IF NOT EXISTS idx_users_first_name ON public.users(first_name);
+    CREATE INDEX IF NOT EXISTS idx_users_last_name ON public.users(last_name);
+    CREATE INDEX IF NOT EXISTS idx_users_structure ON public.users("structure");
+    CREATE INDEX IF NOT EXISTS idx_users_optin_at ON public.users(optin_at);
   `);
 }
 
@@ -26,15 +26,15 @@ export async function down(knex: Knex): Promise<void> {
     DROP INDEX IF EXISTS idx_users_last_name;
     DROP INDEX IF EXISTS idx_users_structure;
 
-    ALTER TABLE public.users DROP COLUMN first_name;
-    ALTER TABLE public.users DROP COLUMN last_name;
-    ALTER TABLE public.users DROP COLUMN structure;
-    ALTER TABLE public.users DROP COLUMN structure_type;
-    ALTER TABLE public.users DROP COLUMN structure_other;
-    ALTER TABLE public.users DROP COLUMN job;
-    ALTER TABLE public.users DROP COLUMN phone;
-    ALTER TABLE public.users DROP COLUMN besoins;
-    ALTER TABLE public.users DROP COLUMN accepted_cgu_at;
-    ALTER TABLE public.users DROP COLUMN optin_at;
+    ALTER TABLE public.users DROP COLUMN IF EXISTS first_name;
+    ALTER TABLE public.users DROP COLUMN IF EXISTS last_name;
+    ALTER TABLE public.users DROP COLUMN IF EXISTS "structure";
+    ALTER TABLE public.users DROP COLUMN IF EXISTS structure_type;
+    ALTER TABLE public.users DROP COLUMN IF EXISTS structure_other;
+    ALTER TABLE public.users DROP COLUMN IF EXISTS job;
+    ALTER TABLE public.users DROP COLUMN IF EXISTS phone;
+    ALTER TABLE public.users DROP COLUMN IF EXISTS besoins;
+    ALTER TABLE public.users DROP COLUMN IF EXISTS accepted_cgu_at;
+    ALTER TABLE public.users DROP COLUMN IF EXISTS optin_at;
   `);
 }
