6e3ac629a0508456c0be3be2212072bdc9b6783a
Martin Ratinaud <martinratinaud@gmail.com>
2025-04-07 18:07:51 +0400

    Affiche mieux le code des iframes + ajoute le backlink

--- src/components/IFrame/Form/IFrameLink.tsx
+++ src/components/IFrame/Form/IFrameLink.tsx
@@ -1,24 +1,31 @@
 import { type HTMLAttributes, useState } from 'react';
 
-import Box from '@/components/ui/Box';
-
-import { CopyInfo, IFrameBox } from './IFrameLink.styles';
-
-const IFrameLink = ({ link, ...props }: { link: string } & HTMLAttributes<HTMLElement>) => {
+const IFrameLink = ({ link, withBacklink = true, ...props }: { link: string; withBacklink?: boolean } & HTMLAttributes<HTMLElement>) => {
   const [copied, setCopied] = useState(false);
+
+  const backlink = `<div style="font-size:11px;color:#999;text-align:right;">Fourni par <a href="https://france-chaleur-urbaine.beta.gouv.fr" target="_blank" rel="noopener">France Chaleur Urbaine</a></div>`;
+  const linkWithBacklink = `${link}${withBacklink ? backlink : ''}`;
+
   const onCopy = () => {
     setCopied(true);
     setTimeout(() => setCopied(false), 1000);
-    navigator.clipboard.writeText(link).catch();
+    navigator.clipboard.writeText(linkWithBacklink).catch();
   };
 
   return (
-    <Box position="relative" {...props}>
-      {copied && <CopyInfo>Copié</CopyInfo>}
-      <IFrameBox onClick={onCopy} className="fr-btn--icon-right fr-icon-clipboard-line">
-        {link}
-      </IFrameBox>
-    </Box>
+    <div className="relative" {...props}>
+      {copied && (
+        <div className="inset-0 flex items-center justify-center bg-white/50 absolute">
+          <span className="py-1 px-2 rounded-md text-sm text-white bg-blue">Copié</span>
+        </div>
+      )}
+      <div
+        onClick={onCopy}
+        className="text-left bg-faded-light cursor-pointer font-mono text-xs !border !border-gray-300 rounded-md px-4 py-3 fr-btn--icon-right fr-icon-clipboard-line"
+      >
+        {linkWithBacklink}
+      </div>
+    </div>
   );
 };
 
