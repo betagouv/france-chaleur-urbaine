e265efa313ccd157878942bbbe53fe5bbf4bdb05
Maxime Dréau <maxime@dreau.fr>
2025-04-08 17:56:21 +0200

    Ajout if not exists pour faciliter les migrations avec dump
    en prod et dev

--- src/server/db/migrations/20250327000005_add_networks_date_actualisation.ts
+++ src/server/db/migrations/20250327000005_add_networks_date_actualisation.ts
@@ -3,12 +3,12 @@ import { type Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   // ces champs seront mis à jour dès qu'une géométrie est mise à jour.
   await knex.raw(`
-    ALTER TABLE public.reseaux_de_chaleur ADD COLUMN date_actualisation_trace timestamptz;
-    ALTER TABLE public.reseaux_de_froid ADD COLUMN date_actualisation_trace timestamptz;
-    ALTER TABLE public.zone_de_developpement_prioritaire ADD COLUMN date_actualisation_trace timestamptz;
-    ALTER TABLE public.zones_et_reseaux_en_construction ADD COLUMN date_actualisation_trace timestamptz;
+    ALTER TABLE public.reseaux_de_chaleur ADD COLUMN IF NOT EXISTS date_actualisation_trace timestamptz;
+    ALTER TABLE public.reseaux_de_froid ADD COLUMN IF NOT EXISTS date_actualisation_trace timestamptz;
+    ALTER TABLE public.zone_de_developpement_prioritaire ADD COLUMN IF NOT EXISTS date_actualisation_trace timestamptz;
+    ALTER TABLE public.zones_et_reseaux_en_construction ADD COLUMN IF NOT EXISTS date_actualisation_trace timestamptz;
 
-    ALTER TABLE public.reseaux_de_chaleur ADD COLUMN date_actualisation_pdp timestamptz;
+    ALTER TABLE public.reseaux_de_chaleur ADD COLUMN IF NOT EXISTS date_actualisation_pdp timestamptz;
   `);
 }
 
