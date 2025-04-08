2971cd7ff37d1042626a5d901321b0cc571bb1e7
Maxime Dréau <maxime@dreau.fr>
2025-04-08 11:20:57 +0200

    Ajoute commentaire sur geom_150m des communes

--- src/server/db/migrations/20250327000002_add_ign_communes_geom_150m.ts
+++ src/server/db/migrations/20250327000002_add_ign_communes_geom_150m.ts
@@ -1,6 +1,9 @@
 import { type Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  // On ajoute une colonne custom pour précalculer la géométrie réduite.
+  // Ça permet d'éviter d'avoir à faire ces calculs dans les requêtes donc ⚡
+  // Là ça va juste mettre 5-10min à tourner. (et potentiellement mettre en pause quelques requêtes mais c'est acceptable)
   await knex.raw(`
     ALTER TABLE public.ign_communes ADD COLUMN geom_150m geometry(MultiPolygon,2154);
     UPDATE public.ign_communes SET geom_150m = st_buffer(geom, -150);
