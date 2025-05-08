fa36564dbe5761f87eae54b11bcefa82f40818f9
Martin Ratinaud <martinratinaud@gmail.com>
2025-05-08 15:40:44 +0400

    Ajout de la page spéciale pour Cerema

--- src/pages/carte-reseaux-de-chaleur-et-froid.tsx
+++ src/pages/carte-reseaux-de-chaleur-et-froid.tsx
@@ -1,28 +1,36 @@
 import IframeWrapper from '@/components/IframeWrapper';
 import Map from '@/components/Map/Map';
 import { createMapConfiguration } from '@/components/Map/map-configuration';
+import SEO from '@/components/SEO';
 
 const CeremaMap = () => {
   return (
-    <IframeWrapper>
-      <Map
-        initialMapConfiguration={createMapConfiguration({
-          reseauxDeChaleur: {
-            show: true,
-          },
-          reseauxEnConstruction: true,
-          zonesDeDeveloppementPrioritaire: true,
-        })}
-        withLegend
-        withBorder
-        legendLogoOpt={{
-          src: '/logo-CEREMA.jpg',
-          alt: 'logo CEREMA',
-        }}
-        withFCUAttribution
-        withComptePro={false}
+    <>
+      <SEO
+        noIndex
+        title="Carte des réseaux de chaleur et de froid"
+        description="Découvrez la carte de référence des réseaux de chaleur et de froid, identifiez les opportunités de raccordement pour votre bâtiment."
       />
-    </IframeWrapper>
+      <IframeWrapper>
+        <Map
+          initialMapConfiguration={createMapConfiguration({
+            reseauxDeChaleur: {
+              show: true,
+            },
+            reseauxEnConstruction: true,
+            zonesDeDeveloppementPrioritaire: true,
+          })}
+          withLegend
+          withBorder
+          legendLogoOpt={{
+            src: '/logo-CEREMA.jpg',
+            alt: 'logo CEREMA',
+          }}
+          withFCUAttribution
+          withComptePro={false}
+        />
+      </IframeWrapper>
+    </>
   );
 };
 
