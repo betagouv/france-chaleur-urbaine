ecc59177ebe664ba013ed258a7d7fd6d1457d852
Maxime Dréau <maxime@dreau.fr>
2025-05-15 16:27:19 +0200

    Gestion des valeurs null dans l'export opendata shapefile
    Le dbf ne gère pas les valeurs nulles pour les nombres

--- scripts/opendata/create-opendata-archive.sh
+++ scripts/opendata/create-opendata-archive.sh
@@ -12,6 +12,7 @@ echo "Utilisation du répertoire temporaire $opendata_dir"
 # Création des vues
 # Les vues préparent les champs qu'on veut exporter en GeoJSON/Shapefile et type bien les champs pour que le format Shapefile soit correct.
 # On ne fait pas de requête directement car postgres ne permet pas d'appeler des fonction avec + de 100 paramètres (par exemple pour `json_build_object`).
+# Pour les exports Shapefile des réseaux de chaleur et de froid, on utilise des tables avec des champs texte pour autoriser les valeurs vides dans le fichier dbf
 psql postgres://postgres:postgres_fcu@localhost:5432/postgres <<EOF
   drop schema if exists opendata cascade;
   create schema if not exists opendata;
@@ -92,6 +93,82 @@ psql postgres://postgres:postgres_fcu@localhost:5432/postgres <<EOF
     order by "Identifiant reseau", id_fcu
   );
 
+  drop view if exists opendata.reseaux_de_chaleur_shp;
+  create view opendata.reseaux_de_chaleur_shp as (
+    SELECT
+      "geom",
+
+      -- champs exportés en propriétés
+      "Identifiant reseau",
+      nom_reseau,
+      array_to_string("communes", ',') as "communes",
+      departement,
+      region,
+      "MO",
+      "Gestionnaire",
+      "reseaux classes",
+      annee_creation::integer::text,
+      longueur_reseau::numeric(10, 1)::text,
+      nb_pdl::integer::text,
+      "Taux EnR&R"::numeric(10, 1)::text,
+      "contenu CO2"::numeric(10, 3)::text,
+      "contenu CO2 ACV"::numeric(10, 3)::text,
+      "PM"::numeric(10, 2)::text,
+      "PV%"::integer::text,
+      "PF%"::integer::text,
+      "PM_L"::numeric(10, 2)::text,
+      "PM_T"::numeric(10, 2)::text,
+      "Rend%"::integer::text,
+      "Dev_reseau%"::integer::text,
+      "production_totale_MWh"::integer::text,
+      "prod_MWh_gaz_naturel"::integer::text,
+      "prod_MWh_charbon"::integer::text,
+      "prod_MWh_fioul_domestique"::integer::text,
+      "prod_MWh_fioul_lourd"::integer::text,
+      "prod_MWh_GPL"::integer::text,
+      "prod_MWh_biomasse_solide"::integer::text,
+      "prod_MWh_dechets_internes"::integer::text,
+      "prod_MWh_UIOM"::integer::text,
+      "prod_MWh_biogaz"::integer::text,
+      "prod_MWh_geothermie"::integer::text,
+      "prod_MWh_PAC"::integer::text,
+      "prod_MWh_solaire_thermique"::integer::text,
+      "prod_MWh_autres_ENR"::integer::text,
+      "prod_MWh_chaleur_industiel"::integer::text,
+      "prod_MWh_autre_chaleur_recuperee"::integer::text,
+      "prod_MWh_chaudieres_electriques"::integer::text,
+      "prod_MWh_autres"::integer::text,
+      "puissance_totale_MW"::numeric(10, 2)::text,
+      "puissance_MW_gaz_naturel"::numeric(10, 2)::text,
+      "puissance_MW_charbon"::numeric(10, 2)::text,
+      "puissance_MW_fioul_domestique"::numeric(10, 2)::text,
+      "puissance_MW_fioul_lourd"::numeric(10, 2)::text,
+      "puissance_MW_GPL"::numeric(10, 2)::text,
+      "puissance_MW_biomasse_solide"::numeric(10, 2)::text,
+      "puissance_MW_dechets_internes"::numeric(10, 2)::text,
+      "puissance_MW_UIOM"::numeric(10, 2)::text,
+      "puissance_MW_biogaz"::numeric(10, 2)::text,
+      "puissance_MW_geothermie"::numeric(10, 2)::text,
+      "puissance_MW_PAC"::numeric(10, 2)::text,
+      "puissance_MW_solaire_thermique"::numeric(10, 2)::text,
+      "puissance_MW_autres_ENR"::numeric(10, 2)::text,
+      "puissance_MW_chaleur_industiel"::numeric(10, 2)::text,
+      "puissance_MW_autre_chaleur_recuperee"::numeric(10, 2)::text,
+      "puissance_MW_chaudieres_electriques"::numeric(10, 2)::text,
+      "puissance_MW_autres"::numeric(10, 2)::text,
+      "livraisons_totale_MWh"::integer::text,
+      "livraisons_residentiel_MWh"::integer::text,
+      "livraisons_tertiaire_MWh"::integer::text,
+      "livraisons_industrie_MWh"::integer::text,
+      "livraisons_agriculture_MWh"::integer::text,
+      "livraisons_autre_MWh"::integer::text,
+      eau_chaude,
+      eau_surchauffee,
+      vapeur
+    FROM reseaux_de_chaleur
+    order by "Identifiant reseau", id_fcu
+  );
+
   drop view if exists opendata.reseaux_de_froid;
   create view opendata.reseaux_de_froid as (
     SELECT
@@ -119,6 +196,37 @@ psql postgres://postgres:postgres_fcu@localhost:5432/postgres <<EOF
       "livraisons_industrie_MWh"::numeric(10, 2),
       "livraisons_agriculture_MWh"::numeric(10, 2),
       "livraisons_autre_MWh"::numeric(10, 2)
+    FROM reseaux_de_froid
+    order by "Identifiant reseau", id_fcu
+  );
+
+  drop view if exists opendata.reseaux_de_froid_shp;
+  create view opendata.reseaux_de_froid_shp as (
+    SELECT
+      "geom",
+
+      -- champs exportés en propriétés
+      "Identifiant reseau",
+      nom_reseau,
+      array_to_string("communes", ',') as "communes",
+      departement,
+      region,
+      "MO",
+      "Gestionnaire",
+      annee_creation::integer::text,
+      longueur_reseau::numeric(10, 1)::text,
+      nb_pdl::integer::text,
+      "contenu CO2"::numeric(10, 3)::text,
+      "contenu CO2 ACV"::numeric(10, 3)::text,
+      "Rend%"::integer::text,
+      "production_totale_MWh"::integer::text,
+      "puissance_totale_MW"::numeric(10, 2)::text,
+      "livraisons_totale_MWh"::numeric(10, 2)::text,
+      "livraisons_residentiel_MWh"::numeric(10, 2)::text,
+      "livraisons_tertiaire_MWh"::numeric(10, 2)::text,
+      "livraisons_industrie_MWh"::numeric(10, 2)::text,
+      "livraisons_agriculture_MWh"::numeric(10, 2)::text,
+      "livraisons_autre_MWh"::numeric(10, 2)::text
     FROM reseaux_de_froid
     order by "Identifiant reseau", id_fcu
   );
@@ -187,10 +295,10 @@ dumpGeoJSON zones_et_reseaux_en_construction "$opendata_dir"/zones_et_reseaux_en
 # Création des shapefile
 pgQuery='PG:host=localhost user=postgres dbname=postgres password=postgres_fcu'
 
-ogr2ogr -f "ESRI Shapefile" -lco ENCODING=UTF-8 "$opendata_dir"/reseaux_de_chaleur.shp "$pgQuery" -sql "SELECT * FROM opendata.reseaux_de_chaleur WHERE st_geometrytype(geom) = 'ST_MultiLineString'"
-ogr2ogr -f "ESRI Shapefile" -lco ENCODING=UTF-8 "$opendata_dir"/reseaux_de_chaleur_sans_traces.shp "$pgQuery" -sql "SELECT * FROM opendata.reseaux_de_chaleur WHERE st_geometrytype(geom) = 'ST_Point'"
-ogr2ogr -f "ESRI Shapefile" -lco ENCODING=UTF-8 "$opendata_dir"/reseaux_de_froid.shp "$pgQuery" -sql "SELECT * FROM opendata.reseaux_de_froid WHERE st_geometrytype(geom) = 'ST_MultiLineString'"
-ogr2ogr -f "ESRI Shapefile" -lco ENCODING=UTF-8 "$opendata_dir"/reseaux_de_froid_sans_traces.shp "$pgQuery" -sql "SELECT * FROM opendata.reseaux_de_froid WHERE st_geometrytype(geom) = 'ST_Point'"
+ogr2ogr -f "ESRI Shapefile" -lco ENCODING=UTF-8 "$opendata_dir"/reseaux_de_chaleur.shp "$pgQuery" -sql "SELECT * FROM opendata.reseaux_de_chaleur_shp WHERE st_geometrytype(geom) = 'ST_MultiLineString'"
+ogr2ogr -f "ESRI Shapefile" -lco ENCODING=UTF-8 "$opendata_dir"/reseaux_de_chaleur_sans_traces.shp "$pgQuery" -sql "SELECT * FROM opendata.reseaux_de_chaleur_shp WHERE st_geometrytype(geom) = 'ST_Point'"
+ogr2ogr -f "ESRI Shapefile" -lco ENCODING=UTF-8 "$opendata_dir"/reseaux_de_froid.shp "$pgQuery" -sql "SELECT * FROM opendata.reseaux_de_froid_shp WHERE st_geometrytype(geom) = 'ST_MultiLineString'"
+ogr2ogr -f "ESRI Shapefile" -lco ENCODING=UTF-8 "$opendata_dir"/reseaux_de_froid_sans_traces.shp "$pgQuery" -sql "SELECT * FROM opendata.reseaux_de_froid_shp WHERE st_geometrytype(geom) = 'ST_Point'"
 ogr2ogr -f "ESRI Shapefile" -lco ENCODING=UTF-8 "$opendata_dir"/pdp.shp "$pgQuery" opendata.zone_de_developpement_prioritaire
 ogr2ogr -f "ESRI Shapefile" -lco ENCODING=UTF-8 "$opendata_dir"/reseaux_en_construction_traces.shp "$pgQuery" opendata.zones_et_reseaux_en_construction -sql "SELECT * FROM opendata.zones_et_reseaux_en_construction WHERE st_geometrytype(geom) = 'ST_MultiLineString'"
 ogr2ogr -f "ESRI Shapefile" -lco ENCODING=UTF-8 "$opendata_dir"/reseaux_en_construction_zones.shp "$pgQuery" opendata.zones_et_reseaux_en_construction -sql "SELECT * FROM opendata.zones_et_reseaux_en_construction WHERE st_geometrytype(geom) = 'ST_MultiPolygon'"
