5fb549bdfb7ea0571df1fff7089b04180dae1cd1
Martin Ratinaud <martinratinaud@gmail.com>
2025-05-20 11:43:08 +0400

    Mise à jour statistiques

--- src/data/statistics.ts
+++ src/data/statistics.ts
@@ -1,12 +1,12 @@
 const statistics = {
-  connection: '1 499', // raccordements à l'étude ou en cours
-  logements: '44 970',
-  CO2Tons: '80 422', // Tonnes de CO2 potentiellement économisées par an
-  networks: '868', // réseaux avec tracé - Réseaux recensés représentant X% de la chaleur livrée par les réseaux en France
-  heatPercent: '93', // de la chaleur livrée par les réseaux en France
-  connectionPercent: '24',
+  connection: '1 801', // raccordements à l'étude ou en cours
+  logements: '54 030',
+  CO2Tons: '96 624', // Tonnes de CO2 potentiellement économisées par an
+  networks: '930', // réseaux avec tracé - Réseaux recensés représentant X% de la chaleur livrée par les réseaux en France
+  heatPercent: '95', // de la chaleur livrée par les réseaux en France
+  connectionPercent: '28', // A recalculer connection/Total des demandes de mise en contact avec un gestionnaire
   iFrameIntegration: '60',
-  lastActu: '8 avril 2025',
+  lastActu: '20 mai 2025',
 };
 
 export default statistics;
