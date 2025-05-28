0e87b1bbc85dfcecfc1d7853a13411370dd25ddf
Martin Ratinaud <martinratinaud@gmail.com>
2025-05-28 14:28:51 +0400

    Meilleur script de migration

--- src/server/db/migrations/20250527115141_email_templates_add_address.ts
+++ src/server/db/migrations/20250527115141_email_templates_add_address.ts
@@ -1,11 +1,15 @@
 import { type Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.alterTable('pro_comparateur_configurations', (table) => {
-    table.string('address');
-  });
+  await knex.raw(`
+    ALTER TABLE pro_comparateur_configurations
+    ADD COLUMN IF NOT EXISTS address VARCHAR;
+  `);
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTableIfExists('pro_comparateur_configurations');
+  await knex.raw(`
+    ALTER TABLE pro_comparateur_configurations
+    DROP COLUMN IF EXISTS address;
+  `);
 }
