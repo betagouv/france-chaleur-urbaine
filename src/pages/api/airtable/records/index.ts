abe331fe96779d059f3cfe1fe6cc4a1f4def4561
Maxime Dréau <maxime@dreau.fr>
2025-07-04 17:59:47 +0200

    Débranche le préremplissage des données dans airtable

--- src/pages/api/airtable/records/index.ts
+++ src/pages/api/airtable/records/index.ts
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 import base, { AirtableDB } from '@/server/db/airtable';
 import { logger } from '@/server/helpers/logger';
 import { BadRequestError, handleRouteErrors, requirePostMethod } from '@/server/helpers/server';
-import { closestNetwork, getConso, getDistanceToNetwork, getNbLogement } from '@/server/services/addresseInformation';
+import { getConso, getNbLogement } from '@/server/services/addresseInformation';
 import { getToRelanceDemand } from '@/server/services/manager';
 import { Airtable } from '@/types/enum/Airtable';
 
@@ -41,37 +41,25 @@ export default handleRouteErrors(async function PostRecords(req: NextApiRequest)
       const { id: demandId }: any = await base(Airtable.UTILISATEURS).create(values, {
         typecast: true,
       });
-      const [conso, nbLogement, network] = await Promise.all([
+      const [conso, nbLogement] = await Promise.all([
         getConso(values.Latitude, values.Longitude),
         values.Logement ? values.Logement : getNbLogement(values.Latitude, values.Longitude),
-        networkId ? getDistanceToNetwork(networkId, values.Latitude, values.Longitude) : closestNetwork(values.Latitude, values.Longitude),
       ]);
-
-      // TODO voir si on veut garder l'affectation ici ou plutôt dans l'admin de gestion des demandes (préféré)
-      // const gestionnaires = await getGestionnaires(values, network ? network['Identifiant reseau'] : '');
-
-      const toRelance = network && network.distance < 200 && values['Type de chauffage'] === 'Collectif';
 
       logger.info('create eligibility demand', {
         id: demandId,
         nbLogement,
         conso,
-        network,
-        // gestionnaires,
       });
 
       await AirtableDB(Airtable.UTILISATEURS).update(
         demandId,
         {
-          // Gestionnaires: gestionnaires,
           Gestionnaires: [],
           Conso: conso ? conso.conso_nb : undefined,
           'ID Conso': conso ? conso.rownum : undefined,
           Logement: nbLogement ? nbLogement.nb_logements : undefined,
           'ID BNB': nbLogement ? `${nbLogement.id}` : undefined,
-          'Identifiant réseau': network ? network['Identifiant reseau'] : undefined,
-          'Nom réseau': network ? network.nom_reseau : undefined,
-          'Relance à activer': toRelance,
         },
         { typecast: true }
       );
