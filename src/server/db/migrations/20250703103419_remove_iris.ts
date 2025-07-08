ed9bff31cb2611853b1d97eedf880efde8f790e4
Martin Ratinaud <martinratinaud@gmail.com>
2025-07-08 17:51:51 +0400

    Fixe la migration de drop de iris

--- src/server/db/migrations/20250703103419_remove_iris.ts
+++ src/server/db/migrations/20250703103419_remove_iris.ts
@@ -1,7 +1,7 @@
 import { type Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('network_iris');
+  await knex.schema.dropTableIfExists('network_iris');
 }
 
 export async function down(knex: Knex): Promise<void> {
