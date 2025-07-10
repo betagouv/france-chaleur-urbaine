3fe1de03e11b986b0c07099246c29a14345da364
Martin Ratinaud <martinratinaud@gmail.com>
2025-07-10 13:35:57 +0400

    Ajoute un logger à la cli d'import et de tiles

--- scripts/data-import/base.ts
+++ scripts/data-import/base.ts
@@ -1,3 +1,11 @@
+import { parentLogger } from '@/server/helpers/logger';
+
 export abstract class BaseAdapter {
+  public name: string = 'data-import';
+  public logger: ReturnType<typeof parentLogger.child>;
   abstract importData(filepath?: string): Promise<void>;
+
+  constructor() {
+    this.logger = parentLogger.child({ name: this.name });
+  }
 }
