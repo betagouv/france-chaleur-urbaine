33c4d9bca646304133309b27918c858bed691ae9
Maxime Dréau <maxime@dreau.fr>
2025-07-24 16:11:34 +0200

    Maj des statistiques au 24 juillet 2025

--- src/data/statistics.ts
+++ src/data/statistics.ts
@@ -1,12 +1,12 @@
 const statistics = {
-  connection: '1 801', // raccordements à l'étude ou en cours
-  logements: '54 030',
-  CO2Tons: '96 624', // Tonnes de CO2 potentiellement économisées par an
-  networks: '930', // réseaux avec tracé - Réseaux recensés représentant X% de la chaleur livrée par les réseaux en France
-  heatPercent: '95', // de la chaleur livrée par les réseaux en France
+  connection: '2017', // raccordements à l'étude ou en cours
+  logements: '60 510',
+  CO2Tons: '108 212', // Tonnes de CO2 potentiellement économisées par an
+  networks: '974', // réseaux avec tracé - Réseaux recensés représentant X% de la chaleur livrée par les réseaux en France
+  heatPercent: '97', // de la chaleur livrée par les réseaux en France
   connectionPercent: '28', // A recalculer connection/Total des demandes de mise en contact avec un gestionnaire
   iFrameIntegration: '60',
-  lastActu: '20 mai 2025',
+  lastActu: '24 juillet 2025',
 };
 
 export default statistics;
