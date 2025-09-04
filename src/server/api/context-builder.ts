fcde8764f38338b4ef42e74af9fa3c803cfc2a4b
Martin Ratinaud <martinratinaud@gmail.com>
2025-09-04 13:47:02 +0400

    type roles in context

--- src/server/api/context-builder.ts
+++ src/server/api/context-builder.ts
@@ -1,10 +1,12 @@
 import { type NextApiRequest } from 'next';
 
+import { type UserRole } from '@/types/enum/UserRole';
+
 export type Context = ReturnType<typeof buildContext>;
 
 const buildContext = (req: NextApiRequest) => {
-  const hasRole = (role: string) => {
-    return req.user?.roles.includes(role);
+  const hasRole = (role: UserRole): boolean => {
+    return req.user?.role === role;
   };
 
   return {
