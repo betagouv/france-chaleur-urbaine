34165c82f1b4769bdb4a700e4b30c8a442731654
Maxime Dréau <maxime@dreau.fr>
2025-10-06 16:33:28 +0200

    Lint fichiers de conf avec Biome

--- sentry.node.config.ts
+++ sentry.node.config.ts
@@ -4,11 +4,10 @@
 import * as Sentry from '@sentry/node';
 
 Sentry.init({
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 0.1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
 });
