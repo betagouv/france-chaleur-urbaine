4d106d2ee51fe8ebd7da5b9d6e6bf49d362d2126
Maxime Dréau <maxime@dreau.fr>
2025-12-11 14:59:23 +0100

    Maj statistiques

--- src/data/statistics.ts
+++ src/data/statistics.ts
@@ -1,12 +1,12 @@
 const statistics = {
-  CO2Tons: '121 785', // tonnes de CO2 potentiellement économisées par an
-  connection: '2 270', // nombre de demandes pour lesquelles le statut est étude en cours, réalisé, travaux en cours ou voté en ag.
+  CO2Tons: '126 453', // tonnes de CO2 potentiellement économisées par an
+  connection: '2 357', // nombre de demandes pour lesquelles le statut est étude en cours, réalisé, travaux en cours ou voté en ag.
   connectionPercent: '27', // ratio connection/total des demandes de mise en contact avec un gestionnaire
   heatPercent: '97', // se base uniquement sur les réseaux de chaleur, et est calculé en prenant les livraisons totales pour les réseaux où has_trace est coché / livraisons totales pourr tous les réseaux
-  iFrameIntegration: '64', // prendre le chiffre indiqué sur Pipedrive dans affaires Iframe / intégrés
-  lastActu: '3 novembre 2025',
-  logements: '66 812', // logements concernés par les raccordements
-  networks: '987', // somme des réseaux de chaleur et de froid pour lesquels has_trace est coché.
+  iFrameIntegration: '65', // prendre le chiffre indiqué sur Pipedrive dans affaires Iframe / intégrés
+  lastActu: '11 décembre 2025',
+  logements: '69 427', // logements concernés par les raccordements
+  networks: '1 001', // somme des réseaux de chaleur et de froid pour lesquels has_trace est coché.
 };
 
 export default statistics;
