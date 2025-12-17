a13591bd12daec11ecb91a2ef125cf25288fdb20
Martin Ratinaud <martinratinaud@gmail.com>
2025-12-17 12:12:50 +0400

    fix: use toBeInTheDocument in home test

--- src/tests/home.test.tsx
+++ src/tests/home.test.tsx
@@ -6,5 +6,7 @@ import Home from '../pages/index';
 test('Page renders successfully', () => {
   render(<Home />);
 
-  expect(screen.getByRole('heading', { level: 1, name: /Le chauffage urbain.*une solution écologique à prix maîtrisé/ })).toBeDefined();
+  expect(
+    screen.getByRole('heading', { level: 1, name: /Le chauffage urbain.*une solution écologique à prix maîtrisé/ })
+  ).toBeInTheDocument();
 });
