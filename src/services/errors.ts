a0c51b429ebd56f5e8c65ee179b37d691c1c4a98
Maxime Dréau <maxime@dreau.fr>
2026-01-20 12:11:30 +0100

    Supprime des fichiers obsolètes

--- src/services/errors.ts
+++
@@ -1,6 +0,0 @@
-export class ServiceError extends Error {
-  // biome-ignore lint/complexity/noUselessConstructor: c'est utile pour typescript pour passer un argument
-  constructor(reason: any) {
-    super(reason);
-  }
-}
