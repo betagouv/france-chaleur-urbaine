1330544e8a483c43aa7f29073c102895f31bf4f3
Morgan Merzouk <morgan.merzouk@beta.gouv.fr>
2026-02-23 11:13:56 +0100

    Changement couleur de fond de page + block ur accentuer les contrastes

--- src/pages/chaleur-renouvelable/resultat/index.tsx
+++ src/pages/chaleur-renouvelable/resultat/index.tsx
@@ -28,6 +28,7 @@ export default function ChaleurRenouvelableResultatPage() {
       title="Découvrez le chauffage qui vous convient !"
       currentPage="/chaleur-renouvelable"
       description="Découvrez les modes de chauffage renouvelables adaptés à votre logement"
+      className="bg-[#FEF7DA]"
     >
       <div className="fr-container fr-pt-6w">
         <div className="flex justify-between fr-mb-3w">
@@ -72,7 +73,7 @@ export default function ChaleurRenouvelableResultatPage() {
         </CallOut>
         <h3>Et maintenant ?</h3>
         <AdemeHelp />
-        <div className="fr-my-6w flex justify-center">
+        <div className="fr-py-6w flex justify-center">
           <Link
             title="Je donne mon avis - nouvelle fenêtre"
             isExternal
