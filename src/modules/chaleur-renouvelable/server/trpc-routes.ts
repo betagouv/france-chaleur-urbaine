03bc5ca4b541b254f5d4f5731a8297bf9075461a
Morgan Merzouk <morgan.merzouk@beta.gouv.fr>
2026-02-27 16:12:52 +0100

    Refacto de l'appel à api/location-infos avec trpc

--- src/modules/chaleur-renouvelable/server/trpc-routes.ts
+++ src/modules/chaleur-renouvelable/server/trpc-routes.ts
@@ -1,7 +1,9 @@
-import { getBatEnrBatimentDetails } from '@/modules/chaleur-renouvelable/server/batenr/service';
+import { zLocationInfos } from '@/modules/chaleur-renouvelable/constants';
+import { getBatEnrBatimentDetails, getLocationInfos } from '@/modules/chaleur-renouvelable/server/service';
 import { zGetBdnbConstructionInput } from '@/modules/tiles/constants';
 import { route, router } from '@/modules/trpc/server';
 
 export const batEnrRouter = router({
   getBatEnrBatimentDetails: route.input(zGetBdnbConstructionInput).query(async ({ input }) => await getBatEnrBatimentDetails(input)),
+  getLocationInfos: route.input(zLocationInfos).query(async ({ input }) => await getLocationInfos(input)),
 });
