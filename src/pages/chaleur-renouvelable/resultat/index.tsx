0f8cea3457608024da6ac2ce21fb483c29be7f29
Morgan Merzouk <morgan.merzouk@beta.gouv.fr>
2026-03-05 15:43:24 +0100

    Suppression du bouton Je Donne Mon Avis (remplacé par posthog survey)

--- src/pages/chaleur-renouvelable/resultat/index.tsx
+++ src/pages/chaleur-renouvelable/resultat/index.tsx
@@ -6,7 +6,6 @@ import { useState } from 'react';
 import Share from '@/components/Share';
 import SimplePage from '@/components/shared/page/SimplePage';
 import Button from '@/components/ui/Button';
-import Image from '@/components/ui/Image';
 import Link from '@/components/ui/Link';
 import Modal from '@/components/ui/Modal';
 
@@ -29,9 +28,9 @@ export default function ChaleurRenouvelableResultatPage() {
       title="Découvrez le chauffage qui vous convient !"
       currentPage="/chaleur-renouvelable"
       description="Découvrez les modes de chauffage renouvelables adaptés à votre logement"
-      className="bg-[#FFFCF9]"
+      className="bg-[#FFFCF9] fr-my-6w"
     >
-      <div className="fr-container fr-pt-6w">
+      <div className="fr-container">
         <div className="flex justify-between fr-mb-3w">
           <Link variant="secondary" href={`/chaleur-renouvelable?${searchParams?.toString() ?? ''}`}>
             <span className="fr-icon-arrow-left-line fr-mr-1w" /> Retour
@@ -45,23 +44,7 @@ export default function ChaleurRenouvelableResultatPage() {
           </Modal>
         </div>
         <h2>Résultats : Vos solutions de chauffage écologique</h2>
-
         <ChoixChauffageResults />
-        <div className="fr-py-6w flex justify-center">
-          <Link
-            title="Je donne mon avis - nouvelle fenêtre"
-            isExternal
-            className="after:content-none bg-none"
-            href="https://jedonnemonavis.numerique.gouv.fr/Demarches/3977?button=4400"
-          >
-            <Image
-              src="https://jedonnemonavis.numerique.gouv.fr/static/bouton-bleu-clair.svg"
-              alt="Je donne mon avis"
-              width="200"
-              height="85"
-            />
-          </Link>
-        </div>
       </div>
     </SimplePage>
   );
