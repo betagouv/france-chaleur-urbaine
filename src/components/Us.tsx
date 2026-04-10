c645e36b99b4c342654c4e5794222a4fb2aab2fc
Morgan Merzouk <morgan.merzouk@beta.gouv.fr>
2026-04-10 12:21:07 +0200

    Mise à jour de la page qui sommes nous

--- src/components/Us.tsx
+++ src/components/Us.tsx
@@ -15,10 +15,11 @@ const Us = () => {
         <HeroSubtitle className="flex items-center gap-8">
           <span className="fr-logo inline-block w-[135px]">République Française</span>
           <span>
-            France Chaleur Urbaine est un <b>service gratuit proposé par l’État</b> qui promeut le chauffage urbain, afin de répondre à
-            trois enjeux majeurs : la lutte contre le changement climatique, la maîtrise du tarif des énergies et la sécurité
-            d’approvisionnement. France Chaleur Urbaine agit en <b>tiers de confiance</b> en mettant en relation les copropriétaires et
-            gestionnaires de bâtiments tertiaires avec les opérateurs des réseaux de chaleur.
+            France Chaleur Urbaine est un <b>service gratuit proposé par ADEME</b> qui oriente les bâtiments vers les solutions de chauffage
+            écologique les plus adaptées. France Chaleur Urbaine répond à trois enjeux majeurs : la lutte contre le changement climatique,
+            la maîtrise du tarif des énergies et la sécurité d'approvisionnement. Afin d'accompagner les bâtiments dans la mise en oeuvre
+            concrète de solutions telles que les réseaux de chaleur, France Chaleur Urbaine agit en <strong>tiers de confiance</strong> en
+            mettant en relation les copropriétaires et gestionnaires de bâtiments tertiaires avec les opérateurs des réseaux de chaleur.
           </span>
         </HeroSubtitle>
       </Hero>
@@ -45,20 +46,21 @@ const Us = () => {
         <SectionContent>
           <List>
             <li>
-              <b>DRIEAT</b> : La Direction régionale et interdépartementale de l’environnement, de l’aménagement et des transports (DRIEAT)
-              d’Ile-de-France porte le projet. Florence Lévy, chargée de mission transition énergétique, pilote le projet en tant
-              qu’intrapreneuse.
+              <b>ADEME</b> : l'Agence de la transition écologique porte le projet. Céline LARUELLE, en tant qu'intrapreneuse et Adrien
+              ZEMOUR, en tant qu'entrepreneur d'intérêt général pilotent le projet.
               <br />
               En savoir plus :{' '}
-              <Link href="https://www.drieat.ile-de-france.developpement-durable.gouv.fr/" isExternal>
-                https://www.drieat.ile-de-france.developpement-durable.gouv.fr/
+              <Link href="https://www.ademe.fr" isExternal>
+                https://www.ademe.fr
               </Link>
             </li>
             <li>
-              <b>BETA.GOUV.FR</b> : France Chaleur Urbaine est une start-up d’Etat du programme Beta.gouv.fr de la Direction
-              interministérielle du numérique (DINUM), qui aide les administrations publiques à construire des services numériques utiles,
-              simples et faciles à utiliser. À ce titre, une équipe de freelances (designer, chargés de déploiement, développeurs...)
-              accompagne le projet.
+              <b>BETA.GOUV.FR</b> : France Chaleur Urbaine est un service numérique public issu du programme{` `}
+              <Link href="https://beta.gouv.fr" isExternal>
+                beta.gouv.fr
+              </Link>
+              , animé par la Direction interministérielle du numérique (DINUM), qui aide les administrations publiques à construire des
+              services numériques utiles, simples et faciles à utiliser.
               <br />
               En savoir plus :{' '}
               <Link href="https://beta.gouv.fr" isExternal>
@@ -75,8 +77,8 @@ const Us = () => {
             En tant que service public numérique, nous sommes transparents sur les ressources allouées et la manière dont elles sont
             employées. Les dépenses réalisées dans le cadre de France Chaleur Urbaine sont présentées ci-dessous par source de financement.{' '}
             <strong>Elles servent à plus de 95 % à financer l’équipe de freelances impliquée sur le projet</strong>. La répartition par
-            poste est également précisée ci-dessous. En complément de ces dépenses, un ETP est mis à disposition par la DRIEAT
-            (intrapreneuse).
+            poste est également précisée ci-dessous. En complément de ces dépenses, un ETP est mis à disposition par la Direction
+            interministérielle du numérique (EIG)
           </p>
           <div className="flex flex-wrap justify-center gap-8 mb-8">
             <Image
@@ -94,17 +96,16 @@ const Us = () => {
 - **2022** : 453 k€
 - **2023** : 643 k€
 - **2024** : 864 k€
+- **2025** : 510 k€
 
 **Sources de financement représentées :**
-- **DINUM** (bleu foncé) – contribution principale chaque année
+- **DINUM** (bleu foncé)
 - **ADEME** (vert)
 - **DGEC** (bleu clair)
 - **DRIEAT** (jaune) – uniquement en 2022
 
 **Observations :**
-- La contribution totale augmente chaque année.
-- DINUM reste la principale source de financement.
-- La part de **DGEC** et **ADEME** progresse nettement en 2023 et 2024.
+- La part de **DGEC** et **ADEME** progresse nettement en 2023, 2024 et 2025.
 - **DRIEAT** n’apparaît que brièvement en 2022.`}
             />
           </div>
@@ -112,19 +113,20 @@ const Us = () => {
             Répartition par poste
           </SectionHeading>
           <TableSimple
-            className="mx-auto max-w-[500px]"
+            className="mx-auto max-w-[600px]"
             columns={[
               { accessorKey: 'poste', flex: 1, header: 'Poste' },
               { accessorKey: '2023', cellType: 'Percent', header: '2023', width: '120px' },
               { accessorKey: '2024', cellType: 'Percent', header: '2024', width: '120px' },
+              { accessorKey: '2025', cellType: 'Percent', header: '2025', width: '120px' },
             ]}
             data={[
-              { '2023': 0.25, '2024': 0.4, id: 1, poste: 'Développement web' },
-              { '2023': 0.33, '2024': 0.32, id: 2, poste: 'Déploiement et animation' },
-              { '2023': 0.15, '2024': 0.13, id: 3, poste: 'Coaching et design' },
-              { '2023': 0.13, '2024': 0.08, id: 4, poste: 'Géomatique' },
-              { '2023': 0.05, '2024': 0.04, id: 5, poste: 'Référencement (SEO/SEA)' },
-              { '2023': 0.09, '2024': 0.04, id: 6, poste: 'Frais (publicité, salons…)' },
+              { '2023': 0.25, '2024': 0.4, '2025': 0.3, id: 1, poste: 'Développement web' },
+              { '2023': 0.33, '2024': 0.32, '2025': 0.4, id: 2, poste: 'Déploiement et animation' },
+              { '2023': 0.15, '2024': 0.13, '2025': 0.2, id: 3, poste: 'Coaching et design' },
+              { '2023': 0.13, '2024': 0.08, '2025': 0.1, id: 4, poste: 'Géomatique' },
+              { '2023': 0.05, '2024': 0.04, '2025': 0, id: 5, poste: 'Référencement (SEO/SEA)' },
+              { '2023': 0.09, '2024': 0.04, '2025': 0, id: 6, poste: 'Frais (publicité, salons…)' },
             ]}
           />
         </SectionContent>
