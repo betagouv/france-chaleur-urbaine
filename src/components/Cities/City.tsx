f9a1f36c46368c8cf2f71ed7653e4c1459d5b0c7
Morgan Merzouk <morgan.merzouk@beta.gouv.fr>
2026-04-15 16:15:09 +0200

    Passage à tailwind de la page City

--- src/components/Cities/City.tsx
+++ src/components/Cities/City.tsx
@@ -6,17 +6,14 @@ import Informations from '@/components/Coproprietaire/Informations';
 import InterviewsVideos from '@/components/Coproprietaire/InterviewsVideos';
 import { ArrowItem } from '@/components/MarkdownWrapper/MarkdownWrapper.style';
 import StickyForm from '@/components/StickyForm/StickyForm';
-import Box, { ResponsiveRow } from '@/components/ui/Box';
-import Heading from '@/components/ui/Heading';
 import Hero, { HeroMeta, HeroTitle } from '@/components/ui/Hero';
 import Link from '@/components/ui/Link';
-import Text from '@/components/ui/Text';
 import WrappedText from '@/components/WrappedText';
 import userExperience from '@/data/villes/user-experience';
 import citiesData from '@/data/villes/villes';
 import type { Network } from '@/types/Summary/Network';
 
-import { CityContainer, VideoGuideColumn } from './City.styles';
+import { CityContainer } from './City.styles';
 import ClassedNetworks from './ClassedNetworks';
 import Dispositifs, { type DispositifsData } from './Dispositifs';
 import Networks from './Networks';
@@ -41,53 +38,43 @@ const City = ({ citySlug, network }: { citySlug: keyof typeof citiesData; networ
             </HeroTitle>
           </Hero>
           <StickyForm title={`Votre bâtiment est-il raccordable au réseau de chaleur de ${cityData.name} ?`} />
-          <Box p="4w" className="fr-container">
-            <Box color="blue-france">
-              <Heading as="h2" color="blue-france">
-                {hasUniqueNetwork ? `Votre réseau de chaleur ${cityData.preposition} ` : `Vos réseaux de chaleur ${cityData.preposition} `}
-                <Text fontWeight="bold" legacyColor="lightblue" display="inline">
-                  {cityData.nameNetwork}
-                </Text>
-              </Heading>
-              <Box mb="4w">
-                <Text as="div">{cityData.description}</Text>
-              </Box>
-            </Box>
+          <div className="fr-container p-4">
+            <h2 className="text-(--text-title-blue-france) fr-my-3w">
+              {hasUniqueNetwork ? `Votre réseau de chaleur ` : `Vos réseaux de chaleur `}
+              <span className="text-[#6060ff]">
+                {cityData.preposition} {cityData.nameNetwork}
+              </span>
+            </h2>
+            <div>{cityData.description}</div>
             {cityData.networksData && (
-              <Box p="2w">
+              <div className="fr-p-2w">
                 <Networks networksData={cityData.networksData} network={network} cityCoord={cityData.coord as [number, number]} />
-              </Box>
+              </div>
             )}
-          </Box>
-          <Box p="8w" backgroundColor="#f9f8f6">
-            <Box className="fr-container">
+          </div>
+          <div className="p-8 bg-[#f9f8f6]">
+            <div className="fr-container">
               <Advantages />
-            </Box>
-          </Box>
-          <Box p="4w" backgroundColor="#4550e5">
-            <Box className="fr-container" textColor="white">
+            </div>
+          </div>
+          <div className="p-4 bg-[#4550e5]">
+            <div className="text-white fr-container">
               <Informations />
-            </Box>
-          </Box>
-          <Box p="8w" className="fr-container">
-            <ResponsiveRow>
-              <VideoGuideColumn>
-                <Heading as="h3" color="blue-france" size="h4">
-                  Découvrez des témoignages sur le terrain
-                </Heading>
-                <InterviewsVideos />
-              </VideoGuideColumn>
-              <VideoGuideColumn>
-                <Heading as="h3" color="blue-france" size="h4">
-                  Le guide complet sur le raccordement
-                </Heading>
-                <CoproGuide guideClassName="fr-mb-0" />
-              </VideoGuideColumn>
-            </ResponsiveRow>
-          </Box>
+            </div>
+          </div>
+          <div className="fr-container p-8 flex flex-col md:flex-row gap-8">
+            <div className="flex-1 px-4">
+              <h3 className="h4 text-(--text-title-blue-france)">Découvrez des témoignages sur le terrain</h3>
+              <InterviewsVideos />
+            </div>
+            <div className="flex-1 px-4">
+              <h3 className="h4 text-(--text-title-blue-france)">Le guide complet sur le raccordement</h3>
+              <CoproGuide guideClassName="fr-mb-0" />
+            </div>
+          </div>
           {cityData.networksData?.isClassed && (
-            <Box p="8w" backgroundColor="#f9f8f6">
-              <ResponsiveRow className="fr-container">
+            <div className="bg[#f9f8f6] p-8">
+              <div className="fr-container flex flex-col md:flex-row gap-4">
                 <ClassedNetworks
                   city={citySlug}
                   nameNetwork={cityData.nameNetwork}
@@ -95,91 +82,73 @@ const City = ({ citySlug, network }: { citySlug: keyof typeof citiesData; networ
                   isUniqueNetwork={hasUniqueNetwork}
                   hasDevelopmentPerimeter={cityData.networksData?.hasDevelopmentPerimeter}
                 />
-              </ResponsiveRow>
-            </Box>
+              </div>
+            </div>
           )}
-          <Box backgroundColor="#4550e5" p="8w">
-            <Heading as="h2" legacyColor="white" center>
+          <div className="bg-[#4550e5] p-8">
+            <h2 className="text-white text-center">
               Découvrez les dispositifs et les aides
               <br />
               auxquels vous avez droit sur {cityData.name}
-            </Heading>
+            </h2>
             {citySlug === 'paris' && (
-              <Box p="4w">
+              <div className="fr-p-4w">
                 <Dispositifs
                   city={citySlug}
                   dispositifsTitle={cityData.dispositifsTitle}
                   dispositifs={cityData.dispositifs as DispositifsData[]}
                 />
-              </Box>
+              </div>
             )}
-            <Box p="4w">
-              <Box className="fr-container">
-                <ResponsiveRow>
-                  <Box flex>
-                    <Box>
-                      <ArrowItem color="white">
-                        <Text>
-                          Le coup de pouce <strong>«&nbsp;Chauffage des bâtiments résidentiels collectifs et tertiaires&nbsp;»</strong>{' '}
-                          permet d’obtenir des aides financières conséquentes pour se raccorder.
-                        </Text>
-                      </ArrowItem>
-                      <ArrowItem color="white">
-                        <Text>
-                          <strong style={{ backgroundColor: '#F8D86E', color: '#000091' }}>
-                            Le coût du raccordement peut ainsi être réduit à quelques centaines d’euros par logement
-                          </strong>{' '}
-                          (en fonction de la situation du bâtiment et de ses besoins en chaleur).
-                        </Text>
-                      </ArrowItem>
-                      <ArrowItem color="white">
-                        <Text>
-                          Différentes entreprises signataires de la charte « Chauffage des bâtiments résidentiels collectifs et tertiaires »
-                          offrent cette prime. <br />
-                          <strong>
-                            Le montant de la prime peut significativement varier d’une entreprise à l’autre, il est donc important de
-                            comparer les offres proposées.
-                          </strong>
-                        </Text>
-                      </ArrowItem>
-                    </Box>
-                    <Box ml="4w" mt="1w">
-                      <Link href="/ressources/aides#contenu" variant="primary">
-                        Tout savoir sur cette aide
-                      </Link>
-                    </Box>
-                  </Box>
-                  <Box flex maxWidth="40%">
-                    <Simulator />
-                  </Box>
-                </ResponsiveRow>
-              </Box>
-            </Box>
+            <div className="fr-container flex flex-col md:flex-row gap-8 fr-mt-6w">
+              <div>
+                <h4 className="fr-h4 text-white">Le coup de pouce « Chauffage des bâtiments résidentiels collectifs et tertiaires »</h4>
+                <div>
+                  <ArrowItem color="white">
+                    Le coup de pouce <strong>«&nbsp;Chauffage des bâtiments résidentiels collectifs et tertiaires&nbsp;»</strong> permet
+                    d’obtenir des aides financières conséquentes pour se raccorder.
+                  </ArrowItem>
+                  <ArrowItem color="white">
+                    <strong style={{ backgroundColor: '#F8D86E', color: '#000091' }}>
+                      Le coût du raccordement peut ainsi être réduit à quelques centaines d’euros par logement
+                    </strong>{' '}
+                    (en fonction de la situation du bâtiment et de ses besoins en chaleur).
+                  </ArrowItem>
+                  <ArrowItem color="white">
+                    Différentes entreprises signataires de la charte « Chauffage des bâtiments résidentiels collectifs et tertiaires »
+                    offrent cette prime. <br />
+                    <strong>
+                      Le montant de la prime peut significativement varier d’une entreprise à l’autre, il est donc important de comparer les
+                      offres proposées.
+                    </strong>
+                  </ArrowItem>
+                </div>
+                <div className="ml-8 mt-1">
+                  <Link href="/ressources/aides#contenu" variant="primary">
+                    Tout savoir sur cette aide
+                  </Link>
+                </div>
+              </div>
+              <div>
+                <h4 className="fr-h4 text-white">Estimez le coup de pouce pour votre résidence</h4>
+                <Simulator />
+              </div>
+            </div>
             {citySlug !== 'paris' && cityData.dispositifs && (
-              <Box p="4w">
+              <div className="fr-p-4w">
                 <Dispositifs
                   city={citySlug}
                   dispositifsTitle={cityData.dispositifsTitle}
                   dispositifs={cityData.dispositifs as DispositifsData[]}
                 />
-              </Box>
+              </div>
             )}
-          </Box>
-
-          <Box p="8w" backgroundColor="#f9f8f6">
-            <Heading as="h2" color="blue-france" center>
-              Les différentes étapes en copropriété :
-            </Heading>
-            <Box display="flex" flexDirection="column" className="fr-container">
+          </div>
+          <div className="bg[#f9f8f6] p-8">
+            <h2 className="text-center text-(--background-active-blue-france)">Les différentes étapes en copropriété :</h2>
+            <div className="flex flex-col fr-container">
               {userExperience.map((props) => (
-                <Box
-                  key={`box-user-experience-${props.imgSrc}`}
-                  display="flex"
-                  flexDirection="row"
-                  alignItems="center"
-                  justifyContent="space-between"
-                  position="relative"
-                >
+                <div key={`box-user-experience-${props.imgSrc}`} className="flex flex-row items-center space-between relative">
                   <WrappedText
                     markdown={false}
                     textClassName="user-experience-description"
@@ -187,10 +156,10 @@ const City = ({ citySlug, network }: { citySlug: keyof typeof citiesData; networ
                     center
                     {...props}
                   />
-                </Box>
+                </div>
               ))}
-            </Box>
-          </Box>
+            </div>
+          </div>
         </>
       )}
     </CityContainer>
