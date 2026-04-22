29f5a9ae639613fac0a06d5b74abfc2cb26045ad
Morgan Merzouk <morgan.merzouk@beta.gouv.fr>
2026-04-22 10:20:58 +0200

    Mise à jour statistics Avril

--- src/data/statistics.ts
+++ src/data/statistics.ts
@@ -1,12 +1,12 @@
 const statistics = {
-  CO2Tons: '137 344', // tonnes de CO2 potentiellement économisées par an
-  connection: '2 560', // nombre de demandes pour lesquelles le statut est étude en cours, réalisé, travaux en cours ou voté en ag.
+  CO2Tons: '144 855', // tonnes de CO2 potentiellement économisées par an
+  connection: '2 700', // nombre de demandes pour lesquelles le statut est étude en cours, réalisé, travaux en cours ou voté en ag.
   connectionPercent: '27', // ratio connection/total des demandes de mise en contact avec un gestionnaire
   heatPercent: '96', // se base uniquement sur les réseaux de chaleur, et est calculé en prenant les livraisons totales pour les réseaux où has_trace est coché / livraisons totales pourr tous les réseaux
   iFrameIntegration: '69', // prendre le chiffre indiqué sur Pipedrive dans affaires Iframe / intégrés
-  lastActu: '19 mars 2026',
-  logements: '76 616', // logements concernés par les raccordements
-  networks: '1 044', // somme des réseaux de chaleur et de froid pour lesquels has_trace est coché.
+  lastActu: '22 avril 2026',
+  logements: '81 481', // logements concernés par les raccordements
+  networks: '1 054', // somme des réseaux de chaleur et de froid pour lesquels has_trace est coché.
 };
 
 export default statistics;
