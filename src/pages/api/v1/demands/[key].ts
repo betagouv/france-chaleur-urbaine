c4b179ddea24aab37696a07276d9c8fe388d9bb1
Maxime Dréau <maxime@dreau.fr>
2026-04-23 10:13:31 +0200

    Décommissionne l'API pour récupérer les demandes

--- src/pages/api/v1/demands/[key].ts
+++ src/pages/api/v1/demands/[key].ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import { getDemandsForGestionnairesApi } from '@/modules/demands/server/public-api';
+// import { getDemandsForGestionnairesApi } from '@/modules/demands/server/public-api';
 import { handleRouteErrors, requireGetMethod } from '@/server/helpers/server';
 import { apiUser } from '@/services/api/authentication';
 import { withCors } from '@/services/api/cors';
@@ -13,15 +13,20 @@ const demands = handleRouteErrors(async (req: NextApiRequest, res: NextApiRespon
     return;
   }
 
-  const demands = await getDemandsForGestionnairesApi(account.gestionnaires || []);
-  return demands.map((demand) => ({
-    address: demand.Adresse,
-    buildingType: demand.Structure,
-    date: demand['Date de la demande'],
-    distance: demand['Distance au réseau'],
-    id: demand.id,
-    network: demand['Identifiant réseau'],
-  }));
+  // const demands = await getDemandsForGestionnairesApi(account.gestionnaires || []);
+  // return demands.map((demand) => ({
+  //   address: demand.Adresse,
+  //   buildingType: demand.Structure,
+  //   date: demand['Date de la demande'],
+  //   distance: demand['Distance au réseau'],
+  //   id: demand.id,
+  //   network: demand['Identifiant réseau'],
+  // }));
+  res.status(410).json({
+    error: 'API décommissionnée',
+    message:
+      'Cette API est obsolète depuis avril 2026. Si vous en aviez encore besoin, merci de nous contacter via https://france-chaleur-urbaine.beta.gouv.fr/contact',
+  });
 });
 
 export default withCors(demands);
