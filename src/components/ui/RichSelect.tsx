dfda2663da96f77197b34169a00bb0ff2749136f
Morgan Merzouk <morgan.merzouk@beta.gouv.fr>
2026-05-12 15:53:37 +0200

    Ajout du type de radiateur sur la landing

--- src/components/ui/RichSelect.tsx
+++ src/components/ui/RichSelect.tsx
@@ -1,4 +1,5 @@
 import * as Select from '@radix-ui/react-select';
+import Image from 'next/image';
 import { useId } from 'react';
 
 import FieldWrapper from '@/components/form/dsfr/FieldWrapper';
@@ -7,6 +8,7 @@ import cx from '@/utils/cx';
 export type RichSelectOption<T extends string = string> = {
   value: T;
   label: string;
+  icone?: string;
   description?: string;
 };
 
@@ -17,6 +19,7 @@ type RichSelectProps<T extends string = string> = {
   placeholder?: string;
   className?: string;
   label?: string;
+  disabled?: boolean;
 };
 
 export default function RichSelect<T extends string>({
@@ -26,6 +29,7 @@ export default function RichSelect<T extends string>({
   placeholder = 'Sélectionner une option',
   className,
   label,
+  disabled = false,
 }: RichSelectProps<T>) {
   const id = useId();
 
@@ -35,11 +39,17 @@ export default function RichSelect<T extends string>({
     <FieldWrapper fieldId={id} label={label || ''} className={className}>
       <Select.Root
         value={radixValue}
+        disabled={disabled}
         onValueChange={(v) => {
           if (v) onChange(v as T);
         }}
       >
-        <Select.Trigger className={cx('fr-select w-full', className)}>
+        <Select.Trigger
+          className={cx(
+            'fr-select w-full data-[disabled]:cursor-not-allowed data-[disabled]:bg-gray-100 data-[disabled]:text-gray-500',
+            className
+          )}
+        >
           <span className="block truncate whitespace-nowrap overflow-hidden text-left">
             <Select.Value placeholder={<span className="block truncate whitespace-nowrap overflow-hidden">{placeholder}</span>} />
           </span>
@@ -51,10 +61,13 @@ export default function RichSelect<T extends string>({
                 <Select.Item
                   key={option.value}
                   value={option.value}
-                  className={cx('cursor-pointer select-none px-4 py-3 outline-none', 'data-[highlighted]:bg-blue-50')}
+                  className={cx('cursor-pointer select-none px-4 py-3 outline-none', 'data-highlighted:bg-blue-50')}
                 >
                   <Select.ItemText>
-                    <span>{option.label}</span>
+                    <div className="flex">
+                      {option.icone && <Image src={option.icone} alt="icone" width="16" height="16" className="mr-1" />}
+                      <span dangerouslySetInnerHTML={{ __html: option.label }} />
+                    </div>
                   </Select.ItemText>
                   {option.description && <div className="text-xs text-slate-500">{option.description}</div>}
                 </Select.Item>
