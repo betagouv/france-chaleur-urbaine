5ccd4e120f7ae2771d4db255372b64f0fa063e1f
Morgan Merzouk <morgan.merzouk@beta.gouv.fr>
2026-06-24 18:43:34 +0200

    composant Tooltip pour le Ademe ENR Choix

--- src/modules/chaleur-renouvelable/client/results/ui/Stars.tsx
+++ src/modules/chaleur-renouvelable/client/results/ui/Stars.tsx
@@ -1,16 +1,15 @@
 import Image from '@/components/ui/Image';
-
+import Tooltip from '@/components/ui/Tooltip';
 export function Stars({ value }: { value: number }) {
   return (
     <div className="flex items-center gap-1" aria-label={`${value} étoiles`}>
       {Array.from({ length: value }).map((_, index) => (
         <Image key={index} src="/icons/icon-star.png" alt="" aria-hidden="true" width="20" height="20" />
       ))}
-      <sup
-        className="fr-icon-information-fill text-blue before:[--icon-size:1rem] cursor-pointer"
-        aria-label="Classement Ademe ENR Choix"
-        title="Classement Ademe ENR Choix"
-      />
+
+      <Tooltip title="Classement Ademe ENR Choix">
+        <sup className="fr-icon-information-fill text-blue before:[--icon-size:1rem] cursor-pointer" />
+      </Tooltip>
     </div>
   );
 }
