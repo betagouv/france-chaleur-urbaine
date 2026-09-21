8556eaf073885fae8d9103209138c7554608802c
Maxime Dréau <maxime@dreau.fr>
2026-09-21 10:21:05 +0200

    Maj stats

--- scripts/stats/refresh.ts
+++ scripts/stats/refresh.ts
@@ -7,12 +7,13 @@ import { prettyFormatNumber } from '@/utils/strings';
 
 const ratioTonnesCO2ParRaccordement = 108212 / 2017; // moyenne de 30 logements par raccordement. calcué initialement avec l'ancien simulateur de CO2.
 
+const salesforceIframesCount = 40; // as of 21/09/2026
+
 export async function refreshStatistics() {
-  const [{ totalDemandes, totalRaccordements, totalLogements }, totalReseaux, pourcentageLivraisonsChaleur, nbIframes] = await Promise.all([
+  const [{ totalDemandes, totalRaccordements, totalLogements }, totalReseaux, pourcentageLivraisonsChaleur] = await Promise.all([
     getDemandesEnCoursStats(),
     getTotalReseaux(),
     getPourcentageLivraisonsChaleur(),
-    getIframesIntegrationsCount(),
   ]);
   const lastActu = new Intl.DateTimeFormat('fr-FR', {
     day: 'numeric',
@@ -28,7 +29,7 @@ export async function refreshStatistics() {
   connection: '${prettyFormatNumber(totalRaccordements)}', // nombre de demandes pour lesquelles le statut est étude en cours, réalisé, travaux en cours ou voté en ag.
   connectionPercent: '${Math.round((totalRaccordements / totalDemandes) * 100)}', // ratio connection/total des demandes de mise en contact avec un gestionnaire
   heatPercent: '${pourcentageLivraisonsChaleur}', // se base uniquement sur les réseaux de chaleur, et est calculé en prenant les livraisons totales pour les réseaux où has_trace est coché / livraisons totales pourr tous les réseaux
-  iFrameIntegration: '${nbIframes}', // historique pipedrive (affaires Iframe / intégrés) + nombre de sources de conversion
+  iFrameIntegration: '${salesforceIframesCount}', // nombre d'iframes dans salesforce ADEME, demander à Léa pour le nombre à jour
   lastActu: '${lastActu}',
   logements: '${prettyFormatNumber(totalLogements)}', // logements concernés par les raccordements
   networks: '${prettyFormatNumber(totalReseaux)}', // somme des réseaux de chaleur et de froid pour lesquels has_trace est coché.
@@ -40,17 +41,6 @@ export default statistics;
 
   logger.info(`Le fichier ${statisticsFilePath} a été mis à jour`);
 }
-
-const oldPipeDriveIframesCount = 69; // as of 01/06/2026
-
-const getIframesIntegrationsCount = async () => {
-  const nbIframes = await kdb
-    .selectFrom('conversion_sources')
-    .select(({ fn }) => [fn.countAll().as('count')])
-    .where('archived_at', 'is', null)
-    .executeTakeFirstOrThrow();
-  return Number(nbIframes.count) + oldPipeDriveIframesCount;
-};
 
 const getTotalReseaux = async () => {
   const reseauxDeChaleur = await kdb
--- src/data/statistics.ts
+++ src/data/statistics.ts
@@ -1,12 +1,12 @@
 const statistics = {
-  CO2Tons: '151 668', // tonnes de CO2 potentiellement économisées par an
-  connection: '2 827', // nombre de demandes pour lesquelles le statut est étude en cours, réalisé, travaux en cours ou voté en ag.
-  connectionPercent: '27', // ratio connection/total des demandes de mise en contact avec un gestionnaire
+  CO2Tons: '278 980', // tonnes de CO2 potentiellement économisées par an
+  connection: '5 200', // nombre de demandes pour lesquelles le statut est étude en cours, réalisé, travaux en cours ou voté en ag.
+  connectionPercent: '46', // ratio connection/total des demandes de mise en contact avec un gestionnaire
   heatPercent: '96', // se base uniquement sur les réseaux de chaleur, et est calculé en prenant les livraisons totales pour les réseaux où has_trace est coché / livraisons totales pourr tous les réseaux
-  iFrameIntegration: '69', // prendre le chiffre indiqué sur Pipedrive dans affaires Iframe / intégrés
-  lastActu: '25 mai 2026',
-  logements: '88 252', // logements concernés par les raccordements
-  networks: '1 061', // somme des réseaux de chaleur et de froid pour lesquels has_trace est coché.
+  iFrameIntegration: '40', // nombre d'iframes dans salesforce ADEME, demander à Léa pour le nombre à jour
+  lastActu: '21 septembre 2026',
+  logements: '170 686', // logements concernés par les raccordements
+  networks: '1 073', // somme des réseaux de chaleur et de froid pour lesquels has_trace est coché.
 };
 
 export default statistics;
